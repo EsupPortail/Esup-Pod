1cd99b20b0e986785df1605f3df07cb7fc828b6e
ptitloup <nicolas.can@univ-lille.fr>
2018-03-12 18:31:52 +0100

    pep8 compliant for import_data

--- pod/video/management/commands/import_data.py
+++ pod/video/management/commands/import_data.py
@@ -1,55 +1,56 @@
 from django.utils import translation
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.core import serializers
-import json
 
 
 class Command(BaseCommand):
     args = 'Channel Theme Type User Discipline'
     help = 'Import from V1'
+    valid_args = ['Channel', 'Theme', 'Type', 'User', 'Discipline', 'FlatPage']
 
     def add_arguments(self, parser):
-        parser.add_argument('type_to_import')
+        parser.add_argument('import')
 
     def handle(self, *args, **options):
         # Activate a fixed locale fr
         translation.activate('fr')
-        if options['type_to_import'] and \
-            options['type_to_import'] in \
-            ['Channel', 'Theme', 'Type', 'User', 'Discipline', 'FlatPage']:
-            type_to_import = options['type_to_import']
-            filepath = "/home/pod/django_projects/import/" + \
-                type_to_import + ".json"
-            f = open(filepath, 'r')
-            filedata = f.read()
-            f.close()
-            newdata = ""
-            if type_to_import == 'Channel':
-                newdata = filedata.replace("pods.channel", "video.channel")
-            if type_to_import == 'Theme':
-                newdata = filedata.replace("pods.theme", "video.theme")
-            if type_to_import == 'Type':
-                newdata = filedata.replace("pods.type", "video.type")
-                newdata = newdata.replace("headband", "icon")
-            if type_to_import == 'User':
-                newdata = filedata.replace("", "")
-            if type_to_import == 'FlatPage':
-                newdata = filedata.replace("", "")
-            if type_to_import == 'Discipline':
-                newdata = filedata.replace(
-                    "pods.discipline", "video.discipline")
-                newdata = newdata.replace("headband", "icon")
-            f = open(filepath, 'w')
-            f.write(newdata)
-            f.close()
-
+        if options['import'] and options['import'] in self.valid_args:
+            type_to_import = options['import']
+            filepath = "../../import/" + type_to_import + ".json"
+            self.migrate_to_v2(filepath, type_to_import)
             with open(filepath, "r") as infile:
                 data = serializers.deserialize("json", infile)
                 for obj in data:
                     obj.save()
         else:
             print(
-                "******* Warning: you must give some arguments: %s *******" % self.args)
+                "******* Warning: you must give some arguments: %s *******"
+                % self.args)
+
+    def migrate_to_v2(filepath, type_to_import):
+        f = open(filepath, 'r')
+        filedata = f.read()
+        f.close()
+        newdata = ""
+        if type_to_import == 'Channel':
+            newdata = filedata.replace("pods.channel", "video.channel")
+        if type_to_import == 'Theme':
+            newdata = filedata.replace("pods.theme", "video.theme")
+        if type_to_import == 'Type':
+            newdata = filedata.replace("pods.type", "video.type")
+            newdata = newdata.replace("headband", "icon")
+        if type_to_import == 'User':
+            newdata = filedata.replace("", "")
+        if type_to_import == 'FlatPage':
+            newdata = filedata.replace("", "")
+        if type_to_import == 'Discipline':
+            newdata = filedata.replace(
+                "pods.discipline", "video.discipline")
+            newdata = newdata.replace("headband", "icon")
+        f = open(filepath, 'w')
+        f.write(newdata)
+        f.close()
+
 
 """
 SAVE FROM PODV1
@@ -60,6 +61,7 @@ from pods.models import Type
 from pods.models import User
 from pods.models import Discipline
 from django.core import serializers
+from django.contrib.flatpages.models import FlatPage
 
 jsonserializer = serializers.get_serializer("json")
 json_serailizer = jsonserializer()
@@ -78,5 +80,9 @@ with open("User.json", "w") as out:
 
 with open("Discipline.json", "w") as out:
     json_serailizer.serialize(Discipline.objects.all(), stream=out)
+
+with open("FlatPage.json", "w") as out:
+    json_serailizer.serialize(FlatPage.objects.all(), stream=out)
+
 
 """
