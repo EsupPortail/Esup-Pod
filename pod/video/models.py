af786a7eda7f2226073ba2a0009b4151cb285aad
DrClockwork <joel.obled@univ-lille.fr>
2018-04-19 15:55:42 +0200

    code review

--- pod/video/models.py
+++ pod/video/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db import connection
 from django.conf import settings
-from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import get_language
 from django.template.defaultfilters import slugify
