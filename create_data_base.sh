4427c94460d1004e4063de686252a4b573264984
ptitloup <nicolas.can@univ-lille.fr>
2018-07-03 10:14:50 +0200

    remove some information from pod_nginx and add some commad in create_data_base to remove previous migrations

--- create_data_base.sh
+++ create_data_base.sh
@@ -1,3 +1,5 @@
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata pod/video/fixtures/initial_data.json
