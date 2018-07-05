6e2e427acf9a1bf2d3a541283a88010bba0a1b6a
ptitloup <nicolas.can@univ-lille.fr>
2018-07-05 11:23:20 +0200

    create recorder app and add it to installed apps

---
+++ pod/recorder/models.py
@@ -0,0 +1,87 @@
+import os
+
+from django.db import models
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+RECORDER_TYPE = getattr(
+    settings, 'RECORDER_TYPE',
+    (
+        ('video', _('Video')),
+        ('audiovideocast', _('Audiovideocast')),
+    )
+)
+DEFAULT_RECORDER_PATH = getattr(
+    settings, 'DEFAULT_RECORDER_PATH',
+    "/home/pod/files"
+)
+DEFAULT_RECORDER_USER_ID = getattr(
+    settings, 'DEFAULT_RECORDER_USER_ID',
+    1
+)
+
+
+class Recording(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        limit_choices_to={'is_staff': True}, default=DEFAULT_RECORDER_USER_ID)
+    title = models.CharField(_('title'), max_length=200, unique=True)
+    source_file = models.FilePathField(
+        path=DEFAULT_RECORDER_PATH, recursive=True)  # match="foo.*"
+    type = models.CharField(
+        max_length=50, choices=RECORDER_TYPE, default=RECORDER_TYPE[0][0])
+    commentaire = models.TextField(_('Comment'), blank=True, default="")
+
+    def __str__(self):
+        return "%s" % (self.name)
+
+    class Meta:
+        verbose_name = _("Recording")
+        verbose_name_plural = _("Recordings")
+
+    def save(self, *args, **kwargs):
+        super(Recording, self).save(*args, **kwargs)
+        print("SAVE !")
+
+    def clean(self):
+        msg = list()
+        msg = self.verify_attributs()
+        if len(msg) > 0:
+            raise ValidationError(msg)
+
+    def verify_attributs(self):
+        msg = list()
+        if not self.type:
+            msg.append(_('Please specify a type.'))
+        elif self.type not in dict(RECORDER_TYPE):
+            msg.append(_('Please use the only type in type choices.'))
+        if not self.source_file:
+            msg.append(_('Please specify a source file.'))
+        elif os.path.exists(self.source_file):
+            msg.append(_("Source file doesn't exists"))
+        return msg
+
+
+@receiver(post_save, sender=Recording)
+def process_recording(sender, instance, created, **kwargs):
+    if created:
+        print("Start !")
+
+
+class RecordingFile(models.Model):
+    file = models.FileField(upload_to='uploads/%Y/%m/%d/')
+    type = models.CharField(
+        max_length=50, choices=RECORDER_TYPE, default=RECORDER_TYPE[0][0])
+
+    class Meta:
+        verbose_name = _("Recording file")
+        verbose_name_plural = _("Recording files")
+
+
+@receiver(post_save, sender=RecordingFile)
+def process_recording_file(sender, instance, created, **kwargs):
+    if created:
+        print("move upload file and create recording")
---
+++ pod/recorder/tests/test_models.py
@@ -0,0 +1,56 @@
+import os
+
+from django.test import TestCase
+from django.test import override_settings
+from django.conf import settings
+from django.contrib.auth.models import User
+
+from ..models import Recording, RecordingFile
+
+
+# Create your tests here.
+@override_settings(
+    MEDIA_ROOT=os.path.join(settings.BASE_DIR, 'media'),
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite',
+        }
+    },
+    LANGUAGE_CODE='en'
+)
+class RecordingTestCase(TestCase):
+    fixtures = ['initial_data.json', ]
+
+    def setUp(self):
+        user = User.objects.create(username="pod")
+        source_file = "/home/pod/files/video.mp4"
+        type = "video"
+        recording = Recording.objects.create(user=user, title="media1")
+        recording.type = type
+        recording.source_file = source_file
+        recording.save()
+        print(" --->  SetUp of MediaCoursesTestCase : OK !")
+
+    """
+        test attributs
+    """
+
+    def test_attributs(self):
+        recording = Recording.objects.get(id=1)
+        self.assertEqual(recording.title, "media1")
+        self.assertEqual(recording.type, "video")
+        self.assertEqual(recording.source_file, "/home/pod/files/video.mp4")
+        print(
+            "   --->  test_attributs of RecordingTestCase : OK !")
+
+    """
+        test delete object
+    """
+
+    def test_delete_object(self):
+        Recording.objects.filter(title="media1").delete()
+        self.assertEquals(Recording.objects.all().count(), 0)
+
+        print(
+            "   --->  test_delete_object of RecordingTestCase : OK !")
