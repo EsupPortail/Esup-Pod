7b37fefa76285966a2939170f9aea7df6bd7467f
ptitloup <nicolas.can@univ-lille.fr>
2018-07-20 09:28:42 +0200

    fix name of project

--- pod/main/celery.py
+++ pod/main/celery.py
@@ -3,9 +3,9 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pod.settings')
 
-app = Celery('proj')
+app = Celery('pod')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
