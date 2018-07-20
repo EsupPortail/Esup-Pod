ee773b56b8f33d2827d49896581638bda6e59337
ptitloup <nicolas.can@univ-lille.fr>
2018-07-20 10:02:51 +0200

    remove import django settings in celery

--- pod/main/celery.py
+++ pod/main/celery.py
@@ -2,11 +2,8 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pod.settings')
-
-from django.conf import settings
 
 app = Celery("pod_project")
 
