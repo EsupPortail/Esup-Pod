3b88a3f3f53bf35ba1d8014b7aff8a6af4a7bd3e
ptitloup <nicolas.can@univ-lille.fr>
2018-09-27 12:59:27 +0200

    fixbug with overlap and verify overlap. Fix translation

--- pod/enrichment/models.py
+++ pod/enrichment/models.py
@@ -238,10 +238,9 @@ class Enrichment(models.Model):
                          self.end <= element.start) or
                         (self.start >= element.end and
                          self.end > element.end)):
-                    msg.append(
-                        _('There is an overlap with ' +
-                          'the enrichment {0}, '.format(element.title) +
-                          'please change start and/or end values.'))
+                    msg.append(_("There is an overlap with the enrichment {0},"
+                                 " please change time start and/or "
+                                 "time end values.").format(element.title))
             if len(msg) > 0:
                 return msg
         return list()
