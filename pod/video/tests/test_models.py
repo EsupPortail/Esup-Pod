3a1a4ecc2c6c6a5b04d596d1934184528be5f2c9
pod <pod@debian.univ-lille.fr>
2018-10-23 16:12:27 +0200

    Test last video display is ok and change code in context_processor

--- pod/video/tests/test_models.py
+++ pod/video/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.test import override_settings
-from django.db.models import Count
+from django.db.models import Count, Q
 from django.template.defaultfilters import slugify
 from django.db.models.fields.files import ImageFieldFile
 from django.contrib.auth.models import User
@@ -374,8 +374,26 @@ class VideoTestCase(TestCase):
             thumbnail = CustomImageModel.objects.create(file="blabla.jpg")
 
         video2 = Video.objects.create(
-            type=type, title="Video2",
-            date_added=datetime.today(),
+            type=type, title="Video2", password= None,
+            date_added=datetime.today(), encoding_in_progress = False,
+            owner=user, date_evt=datetime.today(),
+            video=os.path.join(VIDEOS_DIR, user.owner.hashkey,
+                               '%s.%s' % (slugify(fname), extension)),
+            allow_downloading=True, description="fl",
+            thumbnail=thumbnail, is_draft=False, duration=3)
+
+        video3 = Video.objects.create(
+            type=type, title="Video3", password="toto",
+            date_added=datetime.today(), encoding_in_progress = False,
+            owner=user, date_evt=datetime.today(),
+            video=os.path.join(VIDEOS_DIR, user.owner.hashkey,
+                               '%s.%s' % (slugify(fname), extension)),
+            allow_downloading=True, description="fl",
+            thumbnail=thumbnail, is_draft=False, duration=3)
+
+        video4 = Video.objects.create(
+            type=type, title="Video4", password="",
+            date_added=datetime.today(), encoding_in_progress = False,
             owner=user, date_evt=datetime.today(),
             video=os.path.join(VIDEOS_DIR, user.owner.hashkey,
                                '%s.%s' % (slugify(fname), extension)),
@@ -386,6 +404,16 @@ class VideoTestCase(TestCase):
         tomorrow = datetime.today() + timedelta(days=1)
         ViewCount.objects.create(video=video2, date=tomorrow, count=2)
         print(" --->  SetUp of VideoTestCase : OK !")
+    
+    def test_last_Video_display(self):
+
+        filter_encode = Video.objects.filter(encoding_in_progress = False, is_draft = False)
+        filter_pass = filter_encode.filter(Q(password = '') | Q(password = None), is_restricted = False)
+        self.assertEqual(bool(filter_pass.filter(password = 'toto')), False)
+        self.assertEqual(bool(filter_pass.filter(password = '')), True)
+        self.assertEqual(bool(filter_pass.filter(password__isnull = True)), True)
+        print('--->  test_last_Video_display of VideoTestCase: OK')
+
 
     def test_Video_null_attributs(self):
         video = Video.objects.get(id=1)
@@ -440,6 +468,8 @@ class VideoTestCase(TestCase):
     def test_delete_object(self):
         Video.objects.get(id=1).delete()
         Video.objects.get(id=2).delete()
+        Video.objects.get(id=3).delete()
+        Video.objects.get(id=4).delete()
         self.assertEqual(Video.objects.all().count(), 0)
 
         # check delete view count cascade
@@ -552,7 +582,7 @@ class VideoRenditionTestCase(TestCase):
 
         print(
             "   --->  test_delete_object of VideoRenditionTestCase : OK !")
-
+       
 
 """
     test the Video Encoding model
