bdbd9d683465cf8e78df94bc948717a99fc13286
Eliam <eliam.lotonga@grenet.fr>
2018-11-30 08:38:32 +0000

    add custom homepage url

--- pod/custom/views.py
+++ pod/custom/views.py
@@ -1,3 +1,5 @@
-# from django.shortcuts import render
-
+from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
+def homepage(request):
+    return HttpResponse("Hello word")
