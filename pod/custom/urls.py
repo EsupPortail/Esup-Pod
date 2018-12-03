54a469b7d7bb68290cccd67da467b4d076d5099c
Eliam <eliam.lotonga@grenet.fr>
2018-12-03 12:33:15 +0000

    error import url

--- pod/custom/urls.py
+++ pod/custom/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    urls(r'^$', views.VideoListView.as_view(), name="home_pod"),
+    url(r'^$', views.VideoListView.as_view(), name="home_pod"),
 ]
