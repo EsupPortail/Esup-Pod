3b2a218321a30da0ca26fd04488717c2ff10f998
ptitloup <nicolas.can@univ-lille.fr>
2019-02-12 15:30:38 +0100

    flake 8 compliant

--- pod/video/forms.py
+++ pod/video/forms.py
@@ -624,8 +624,8 @@ class NotesForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(NotesForm, self).__init__(*args, **kwargs)
-        #self.fields["user"].widget = forms.HiddenInput()
-        #self.fields["video"].widget = forms.HiddenInput()
+        # self.fields["user"].widget = forms.HiddenInput()
+        # self.fields["video"].widget = forms.HiddenInput()
         # self.fields["note"].widget.attrs["cols"] = 20
         self.fields["note"].widget.attrs["class"] = "form-control"
         self.fields["note"].widget.attrs["rows"] = 5
