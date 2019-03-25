3e647ebe028998441a048c5985276a610ceeb922
Eliam <eliam.lotonga@grenet.fr>
2019-03-25 10:09:49 +0000

    custom footer on login template

--- pod/custom/static/custom/assets/js/custom.js
+++ pod/custom/static/custom/assets/js/custom.js
@@ -1,35 +1,4 @@
-console.log($);
-
 $(function()
 {
-  // Footer position absolute on authentcate view
-  let screenWidth = window.screen.width;
-  let MIN_WIDTH = 767;
-  let pathnameExpected = "/authentication_login_gateway/";
-
-  let fixeFooter = function(){
-    let currentPath = window.location.pathname;
-    if( currentPath.toLowerCase() === pathnameExpected )
-    {
-      if( screenWidth > MIN_WIDTH  )
-      {
-        if( !($( "footer" ).hasClass( "fixebottom" ))  )
-        {
-          $( "footer" ).addClass( "fixebottom" );
-        }
-      }
-      else {
-        $( "footer" ).removeClass( "fixebottom" );
-      }
-    }
-  }
-  fixeFooter( )
-  $( window ).resize( e =>
-  {
-    screenWidth = $( window ).width();
-    fixeFooter( )
-  })
-
-
 
 });
