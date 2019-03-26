a3537dbd46691d69b449e6bb13d420ac87fc7d30
Eliam <eliam.lotonga@grenet.fr>
2019-03-26 09:30:53 +0000

    .width(), Connexion logo

--- pod/custom/static/custom/assets/js/custom.js
+++ pod/custom/static/custom/assets/js/custom.js
@@ -7,4 +7,45 @@ $(function()
   $( "#s" ).blur( e => {
       $("form#nav-search").removeClass("staticposition");
   })
+
+  let screenWidth = window.width;
+  let MIN_WIDTH = 767;
+  let btnConnexion = $(".btn.btn-connexion");
+  let btnConnexionlogo = $(".btn-custom.logo");
+
+  let displayConnexionLogo = function()
+  {
+      if( screenWidth <= MIN_WIDTH  )
+      {
+        if( btnConnexion.is(":visible") )
+        {
+          btnConnexion.hide();
+        }
+        if( btnConnexionlogo.is(":hidden") )
+        {
+          btnConnexionlogo.show();
+        }
+
+      }
+      else
+      {
+        if( btnConnexionlogo.is(":visible") )
+        {
+          btnConnexionlogo.hide();
+        }
+        if( btnConnexion.is(":hidden") )
+        {
+          btnConnexion.show();
+        }
+      }
+  }
+  displayConnexionLogo();
+
+  $( window ).resize( e =>
+  {
+    screenWidth = $( window ).width();
+    displayConnexionLogo( )
+  })
+
+
 });
