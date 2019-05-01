09bc820f12cdebfb896f4a687758c52e75c69431
valentin <valentin.legros.etu@univ-lille.fr>
2019-05-01 07:13:46 +0200

    widget added in Broadcaster admin page

--- pod/live/forms.py
+++ pod/live/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.conf import settings
 
 from pod.live.models import Building
+from pod.live.models import Broadcaster
 
 FILEPICKER = False
 if getattr(settings, 'USE_PODFILE', False):
@@ -9,11 +10,11 @@ if getattr(settings, 'USE_PODFILE', False):
     from pod.podfile.widgets import CustomFileWidget
 
 
-class BuildingForm(forms.ModelForm):
+class BuildingAdminForm(forms.ModelForm):
     required_css_class = 'required'
 
     def __init__(self, *args, **kwargs):
-        super(BuildingForm, self).__init__(*args, **kwargs)
+        super(BuildingAdminForm, self).__init__(*args, **kwargs)
         if FILEPICKER:
             self.fields['headband'].widget = CustomFileWidget(type="image")
 
@@ -22,4 +23,20 @@ class BuildingForm(forms.ModelForm):
 
     class Meta(object):
         model = Building
+        fields = '__all__'
+
+
+class BroadcasterAdminForm(forms.ModelForm):
+    required_css_class = 'required'
+
+    def __init__(self, *args, **kwargs):
+        super(BroadcasterAdminForm, self).__init__(*args, **kwargs)
+        if FILEPICKER:
+            self.fields['poster'].widget = CustomFileWidget(type="image")
+
+    def clean(self):
+        super(BuildingForm, self).clean()
+
+    class Meta(object):
+        model = Broadcaster
         fields = '__all__'
