df380ccbb53dda4adaac74172d9a0478849a5b25
pod <pod@pod-dev.grenet.fr>
2019-05-23 15:09:02 +0200

    Add scripts to fixed footer on bottom in auth and login views

--- pod/custom/static/custom/assets/js/custom.js
+++ pod/custom/static/custom/assets/js/custom.js
@@ -1,5 +1,49 @@
 $(function()
 {
+  $(document).ready(function(){
+    
+	  
+    var pathname = window.location.pathname
+    views = ["/authentication_login_gateway/", "/accounts/login/"]
+    var footer = $("footer.text-muted.bg-dark.pt-2") || $('footer.text-muted') || $('footer')
+    let screenWidth = $( window ).width();
+    let MIN_WIDTH = 767;
+
+    if( views.includes(pathname) )
+    {
+
+      let fixeFooter = function()
+      {
+        if( screenWidth > MIN_WIDTH  )
+        {
+          if( !( footer.hasClass( "fixebottom" ) )  )
+          {
+            footer.addClass( "fixebottom" );
+          }
+        }
+        else
+        {
+          footer.removeClass( "fixebottom" );
+        }
+      }
+      fixeFooter( )
+      $( window ).resize( e =>
+      {
+        screenWidth = $( window ).width();
+        fixeFooter( )
+      })
+
+      if( views.includes(pathname) )
+      {
+        footer.addClass('fixebottom')
+      }
+      else
+      {
+        footer.removeClass('fixebottom')
+      }
+    }	
+  });
+
   $(".navbar-brand strong").remove();
   $( "#s" ).focus( e => {
       $("form#nav-search").addClass("staticposition");
