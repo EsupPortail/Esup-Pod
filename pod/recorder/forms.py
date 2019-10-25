0b53d18d577ac26cbe2ab99944fd410066d29f59
JoshuaBaubry <joshua.baubrylize@etudiant.univ-lr.fr>
2019-10-25 15:00:39 +0200

    Fix translation + add the ability to the recorder manager to choice owner of final video

--- pod/recorder/forms.py
+++ pod/recorder/forms.py
@@ -1,14 +1,27 @@
 from django import forms
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from django.utils.translation import ugettext_lazy as _
 
-from .models import Recording
+from .models import Recording, Recorder
 from pod.main.forms import add_placeholder_and_asterisk
 
 DEFAULT_RECORDER_PATH = getattr(
     settings, 'DEFAULT_RECORDER_PATH',
     "/data/ftp-pod/ftp/"
 )
+
+
+def check_show_user(request):
+    show_user = False
+    if request.GET.get("recorder"):
+        try:
+            recorder = Recorder.objects.get(id=request.GET.get("recorder"))
+            if recorder and (request.user == recorder.user):
+                show_user = True
+        except ObjectDoesNotExist:
+            pass
+    return show_user
 
 
 class RecordingForm(forms.ModelForm):
@@ -28,8 +41,11 @@ class RecordingForm(forms.ModelForm):
         )
         self.fields['source_file'].widget.attrs['class'] = 'form-control'
 
-        if not request.user.is_superuser:
+        print(request.GET.get("recorder"))
+
+        if not(check_show_user(request) or request.user.is_superuser):
             del self.fields['user']
+        if not request.user.is_superuser:
             # del self.fields['source_file']
             self.fields['recorder'].widget = forms.HiddenInput()
             self.fields['source_file'].widget = forms.HiddenInput()
