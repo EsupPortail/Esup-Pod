ad38849c670fd0d56f10834a9cacafa021eed60a
ptitloup <nicolas.can@univ-lille.fr>
2020-01-31 11:50:36 +0100

    check if ES_URL is not none before send data to ES

--- pod/video_search/models.py
+++ pod/video_search/models.py
@@ -6,11 +6,12 @@ from django.db.models.signals import post_save, post_delete
 
 TEST_SETTINGS = getattr(
     settings, 'TEST_SETTINGS', False)
+ES_URL = getattr(settings, 'ES_URL', ['http://127.0.0.1:9200/'])
 
 # do it  with contributor, overlay, chapter etc.
 @receiver(post_save, sender=Video)
 def update_video_index(sender, instance=None, created=False, **kwargs):
-    if TEST_SETTINGS:
+    if TEST_SETTINGS or ES_URL is None:
         return
     if instance.is_draft is False and instance.encoding_in_progress is False:
         index_es(instance)
@@ -20,6 +21,6 @@ def update_video_index(sender, instance=None, created=False, **kwargs):
 
 @receiver(post_delete, sender=Video)
 def delete_video_index(sender, instance=None, created=False, **kwargs):
-    if TEST_SETTINGS:
+    if TEST_SETTINGS or ES_URL is None:
         return
     delete_es(instance)
