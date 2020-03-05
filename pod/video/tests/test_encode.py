0d67bc0ba6b6f91b21b3df90c5b92b435697aaa8
JoshuaBaubry <joshua.baubry@gmail.com>
2020-03-05 14:36:38 +0100

    remove useless log

--- pod/video/tests/test_encode.py
+++ pod/video/tests/test_encode.py
@@ -146,7 +146,6 @@ class EncodeTestCase(TestCase):
         audio.delete()
         self.assertTrue(not os.path.exists(audio_video_path))
         self.assertTrue(not os.path.exists(audio_log_file))
-        print("AUDIOOOOOOOOOOOOOOOO" + audio_log_file)
         self.assertEqual(len(os.listdir(os.path.dirname(audio_log_file))), 0)
 
         print(
