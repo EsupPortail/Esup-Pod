4fd5b8aebf872232f9415801366b9dccf8952fe6
JoshuaBaubry <joshua.baubry@gmail.com>
2020-03-31 13:39:57 +0200

    add option to customize header

--- pod/authentication/shibmiddleware.py
+++ pod/authentication/shibmiddleware.py
@@ -1,5 +1,9 @@
 from shibboleth.middleware import ShibbolethRemoteUserMiddleware
+from django.conf import settings
+
+REMOTE_USER_HEADER = getattr(
+    settings, 'REMOTE_USER_HEADER', "REMOTE_USER")
 
 
 class PodShibbolethRemoteUserMiddleware(ShibbolethRemoteUserMiddleware):
-    header = "HTTP_REMOTE_USER"
+    header = REMOTE_USER_HEADER
