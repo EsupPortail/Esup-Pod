6bdcc51332bbf9c3b9b0b54b11b67d6ff682f191
JoshuaBaubry <joshua.baubry@gmail.com>
2020-04-15 10:18:46 +0200

    fix compat with shibboleth login

--- pod/authentication/backends.py
+++ pod/authentication/backends.py
@@ -1,4 +1,6 @@
 from shibboleth.backends import ShibbolethRemoteUserBackend
+from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
 
 
 class ShibbBackend(ShibbolethRemoteUserBackend):
@@ -8,3 +10,20 @@ class ShibbBackend(ShibbolethRemoteUserBackend):
               ShibbBackend).update_user_params(user, params)
         user.owner.auth_type = "Shibboleth"
         user.owner.save()
+
+    def setup_user(self, request, username, defaults):
+        if self.create_unknown_user:
+            user, created = User.objects.get_or_create(username=username,
+                                                       defaults=defaults,
+                                                       sites=get_current_site(
+                                                           request))
+            if created:
+                user = self.handle_created_user(
+                    request, user, sites=get_current_site(request))
+        else:
+            try:
+                user = User.objects.get(
+                    username=username, sites=get_current_site(request))
+            except User.DoesNotExist:
+                return
+        return user
