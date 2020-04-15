bba9572a083d9089cbba3f90855e4e8766dc699c
JoshuaBaubry <joshua.baubry@gmail.com>
2020-04-15 11:06:35 +0200

    add current site to shibboleth user when login

--- pod/authentication/backends.py
+++ pod/authentication/backends.py
@@ -1,5 +1,4 @@
 from shibboleth.backends import ShibbolethRemoteUserBackend
-from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
 
 
@@ -9,21 +8,6 @@ class ShibbBackend(ShibbolethRemoteUserBackend):
         super(ShibbBackend,
               ShibbBackend).update_user_params(user, params)
         user.owner.auth_type = "Shibboleth"
+        if get_current_site(None) not in user.owner.sites:
+            user.owner.sites.add(get_current_site(None))
         user.owner.save()
-
-    def setup_user(self, request, username, defaults):
-        if self.create_unknown_user:
-            user, created = User.objects.get_or_create(
-                username=username,
-                defaults=defaults,
-                owner__sites=get_current_site(request))
-            if created:
-                user = self.handle_created_user(
-                    request, user, owner__sites=get_current_site(request))
-        else:
-            try:
-                user = User.objects.get(
-                    username=username, owner__sites=get_current_site(request))
-            except User.DoesNotExist:
-                return
-        return user
