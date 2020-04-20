c4fa86fb5d4415193aaaf330ee47e2d9760dcaba
JoshuaBaubry <joshua.baubry@gmail.com>
2020-04-20 13:36:27 +0200

    superuser bypass sites assignation

--- pod/main/auth_backend.py
+++ pod/main/auth_backend.py
@@ -1,19 +1,25 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.sites.models import Site
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 
 class SiteBackend(ModelBackend):
     def authenticate(self, **credentials):
         user_or_none = super(SiteBackend, self).authenticate(**credentials)
         site = Site.objects.get_current()
-        if user_or_none and (site not in user_or_none.owner.sites.all()):
+        if user_or_none and not user_or_none.is_superuser and \
+                (site not in user_or_none.owner.sites.all()):
             user_or_none = None
         return user_or_none
 
     def get_user(self, user_id):
         try:
-            return User.objects.get(
-                pk=user_id, owner__sites=Site.objects.get_current())
+            site = Site.objects.get_current()
+            user = User.objects.get(pk=user_id)
+            if user and not user.is_superuser and \
+                    (site not in user.owner.sites.all()):
+                user = None
+            return user
         except User.DoesNotExist:
             return None
