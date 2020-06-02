c9bca5b554d59fe1dd0a791365793686994d7672
Joshua Baubry <joshua.baubry@gmail.com>
2020-06-02 16:33:53 +0200

    remove direct video link when password in rss

--- pod/video/feeds.py
+++ pod/video/feeds.py
@@ -197,6 +197,8 @@ class RssSiteVideosFeed(Feed):
             item.date_added.strftime('%Y-%m-%d'), '%Y-%m-%d')
 
     def item_enclosure_url(self, item):
+        if item.password is not None:
+            return ""
         if item.get_video_mp4().count() > 0:
             mp4 = sorted(item.get_video_mp4(), key=lambda m: m.height)[0]
             return ''.join([self.author_link, mp4.source_file.url])
