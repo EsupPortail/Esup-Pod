af6b4f4dcf0eb91e201e81eeb874284572c7d7d2
Eliam LOTONGA <eliam.lotonga@univ-grenoble-alpes.fr>
2020-09-29 14:04:26 +0200

    update lang

--- pod/podfile/static/podfile/js/filewidget.js
+++ pod/podfile/static/podfile/js/filewidget.js
@@ -462,12 +462,13 @@ function initFolders(){
 
 var seeMoreElement = function (nextPage, curr_page, tot_page, search=null){
     search = search? `&page=${search}`: '';
+    let seeMore = gettext('See more');
     return `
 	<div class="view-more-container">
 	    <a id="more" href="#" data-next="/podfile/ajax_calls/user_folders?page=${nextPage}${search}">
 		<span class="see-more" id="see-more-icon">
 		    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right" class="svg-inline--fa fa-arrow-right fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path></svg>
-		</span><span class="text">${gettext('See more')} (${curr_page}/${tot_page})</span>
+		</span><span class="text">${seeMore} (${curr_page}/${tot_page})</span>
 		<div class="loader lds-ellipsis"><div></div><div></div><div></div><div></div></div>
 	    </a>
 	</div>
