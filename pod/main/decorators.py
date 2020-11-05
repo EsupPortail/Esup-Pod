eba7d4e79f79469b170ab9d8b5ee34adaf22a15a
Eliam LOTONGA <contact@eliam-lotonga.fr>
2020-11-05 21:42:07 +0100

    Add custom Ajax decorator

---
+++ pod/main/decorators.py
@@ -0,0 +1,12 @@
+from fonctools import wraps
+from django.core.exceptions import PermissionDenied
+
+
+def ajax_required(fct):
+    @wraps(fct)
+    def _wrapped_view(request, *args, **kwargs):
+        if request.is_ajax():
+            return fct(request, *args, **kwargs)
+        else:
+            raise PermissionDenied()
+        return _wrapped_view
