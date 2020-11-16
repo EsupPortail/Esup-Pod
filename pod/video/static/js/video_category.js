7205d2c624cbd2edbab39dbf7a50e62460305841
Eliam LOTONGA <contact@eliam-lotonga.fr>
2020-11-16 22:51:20 +0100

    WIP delete category Ajax

--- pod/video/static/js/video_category.js
+++ pod/video/static/js/video_category.js
@@ -3,23 +3,23 @@ const cat_to_delete = {
     title: undefined,
     slug: undefined
 }
+const BASE_URL = `${window.location.href}category/`;
 
 let modal_title = document.querySelector("#manageCategoryModal #modal_title")
 let cat_input = document.querySelector("#manageCategoryModal #catTitle");
 
 // Add onclick event to edit a category
-let cats_edit = document.querySelectorAll("#my_videos_filter .categories_list_item #edit_category_icon");
+let cats_edit = document.querySelectorAll("#my_videos_filter .categories_list_item #edit_category");
 cats_edit.forEach(c_e =>{
     c_e.addEventListener('click', (e) =>{
-	let c = c_e.parentNode.querySelector('.cat_title');
-	cat_input.value = c.textContent.trim();
+	cat_input.value = c_e.dataset.title.trim();
     	modal_title.innerText = c_e.getAttribute('title').trim(); 
     	window.setTimeout(function(){ cat_input.focus()}, 500)
     });
 });
 
 // Add onclick event to delete a category
-let cats_del = document.querySelectorAll("#my_videos_filter .categories_list_item #delete_category_icon");
+let cats_del = document.querySelectorAll("#my_videos_filter .categories_list_item #remove_category_icon");
 cats_del.forEach(c_d => {
     c_d.addEventListener('click', (e) =>{
 	// Show confirm modal => manage by boostrap
@@ -31,9 +31,21 @@ cats_del.forEach(c_d => {
 // Add onclick event to delete a category
 let del_cat = document.querySelector("#confirm_remove_category_btn");
 del_cat.addEventListener('click', (e) =>{
+	console.log(cat_to_delete)
     if(cat_to_delete.title && cat_to_delete.slug)
     {
 	// Delete category
+	let cat = CATEGORIES_DATA.find(c =>{
+	    return c.title === cat_to_delete.title && c.slug === cat_to_delete.slug;
+	});
+	if(cat)
+	{
+	    let url = `${BASE_URL}delete/${cat.id}`;
+	    console.log("----------------------------------------")
+	    console.log(url)
+ 	    console.log("----------------------------------------")
+	    // Ajax request to delete category
+	}
     }
     else
     { 
