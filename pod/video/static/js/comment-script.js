0fb68f7a25875b73006b7144f0d4c59af99ebd62
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-01-10 11:43:01 +0100

    Add icon to comment answer, change color gradient of answerw

--- pod/video/static/js/comment-script.js
+++ pod/video/static/js/comment-script.js
@@ -5,15 +5,15 @@ let all_comment = null;
 let lang_btn = document.querySelector(".btn-lang.btn-lang-active");
 let  VOTED_USERS = [];
 const COLORS = [
-    "#212529",
-    "#343A40",
-    "#495057",
-    "#6C757D",
-    "#ADB5BD",
-    "#CED4DA",
-    "#DEE2E6",
-    "#E9ECEF",
-    "#F8F9FA",
+    "#5F616D",
+    "#6B6D79",
+    "#767984",
+    "#828590",
+    "#8D929B",
+    "#999EA7",
+    "#A4AAB2",
+    "#B0B6BE",
+    "#BBC2C9",
 ]
 const LANG = lang_btn?lang_btn.textContent.trim() : "fr";
 const ACTION_COMMENT = {
@@ -236,8 +236,14 @@ class Comment extends HTMLElement
         }
         else
         {
+            let comment_icon_svg = `<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="replyd" class="svg-inline--fa fa-replyd fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M320 480H128C57.6 480 0 422.4 0 352V160C0 89.6 57.6 32 128 32h192c70.4 0 128 57.6 128 128v192c0 70.4-57.6 128-128 128zM193.4 273.2c-6.1-2-11.6-3.1-16.4-3.1-7.2 0-13.5 1.9-18.9 5.6-5.4 3.7-9.6 9-12.8 15.8h-1.1l-4.2-18.3h-28v138.9h36.1v-89.7c1.5-5.4 4.4-9.8 8.7-13.2 4.3-3.4 9.8-5.1 16.2-5.1 4.6 0 9.8 1 15.6 3.1l4.8-34zm115.2 103.4c-3.2 2.4-7.7 4.8-13.7 7.1-6 2.3-12.8 3.5-20.4 3.5-12.2 0-21.1-3-26.5-8.9-5.5-5.9-8.5-14.7-9-26.4h83.3c.9-4.8 1.6-9.4 2.1-13.9.5-4.4.7-8.6.7-12.5 0-10.7-1.6-19.7-4.7-26.9-3.2-7.2-7.3-13-12.5-17.2-5.2-4.3-11.1-7.3-17.8-9.2-6.7-1.8-13.5-2.8-20.6-2.8-21.1 0-37.5 6.1-49.2 18.3s-17.5 30.5-17.5 55c0 22.8 5.2 40.7 15.6 53.7 10.4 13.1 26.8 19.6 49.2 19.6 10.7 0 20.9-1.5 30.4-4.6 9.5-3.1 17.1-6.8 22.6-11.2l-12-23.6zm-21.8-70.3c3.8 5.4 5.3 13.1 4.6 23.1h-51.7c.9-9.4 3.7-17 8.2-22.6 4.5-5.6 11.5-8.5 21-8.5 8.2-.1 14.1 2.6 17.9 8zm79.9 2.5c4.1 3.9 9.4 5.8 16.1 5.8 7 0 12.6-1.9 16.7-5.8s6.1-9.1 6.1-15.6-2-11.6-6.1-15.4c-4.1-3.8-9.6-5.7-16.7-5.7-6.7 0-12 1.9-16.1 5.7-4.1 3.8-6.1 8.9-6.1 15.4s2 11.7 6.1 15.6zm0 100.5c4.1 3.9 9.4 5.8 16.1 5.8 7 0 12.6-1.9 16.7-5.8s6.1-9.1 6.1-15.6-2-11.6-6.1-15.4c-4.1-3.8-9.6-5.7-16.7-5.7-6.7 0-12 1.9-16.1 5.7-4.1 3.8-6.1 8.9-6.1 15.4 0 6.6 2 11.7 6.1 15.6z"></path></svg>`;
+            let comment_icon = document.createElement("DIV");
+            comment_icon.innerHTML = comment_icon_svg;
+            comment_icon.setAttribute("class", "icon comments_icon");
+
             // add show children button action
-            comment_container.setAttribute("class", "");
+            comment_container.setAttribute("class", "comment_child_container");
+            comment_container.prepend(comment_icon)
             comment_container.querySelector(".comment_content").classList.add("comment_content_child");
             this.classList.add("comment_child");
             this.appendChild(comment_container);
@@ -464,7 +470,7 @@ function scrollToComment(targetComment)
 
     window.setTimeout(() => {
         htmlTarget.classList.remove('scroll_to');
-    }, 3000);
+    }, 4000);
 
     htmlTarget.classList.add('scroll_to');
 }
@@ -505,24 +511,29 @@ function add_user_tag(comment_value, parent_comment)
 
 /****************  return color index  ****************
  ******************************************************/
-function setBorderLeftColor(comment, parent_element, is_parent=false){
-    if(is_parent) return 0;
+function setBorderLeftColor(comment, parent_element){
     try{
         let index = Number.parseInt(parent_element.dataset.level) + 1;
 	if(index >= COLORS.length){
 	    comment.dataset.level = COLORS.length - 1;
 	    comment.querySelector(".comment_content")
 	        .style.borderLeft = `4px solid ${COLORS[COLORS.length - 1]}`; 
+	    comment.querySelector(".comments_icon")
+	        .style.color = `${COLORS[COLORS.length - 1]}`; 
 	}
 	else{
 	    comment.dataset.level = index;
 	    comment.querySelector(".comment_content")
 	        .style.borderLeft = `4px solid ${COLORS[index]}`; 
+	    comment.querySelector(".comments_icon")
+	        .style.color = `${COLORS[index]}`; 
 	}
     }catch(e){
 	comment.dataset.level = COLORS.length - 1;
 	comment.querySelector(".comment_content")
 	    .style.borderLeft = `4px solid ${COLORS[COLORS.length - 1]}`; 
+	comment.querySelector(".comments_icon")
+	    .style.color = `${COLORS[COLORS.length - 1]}`; 
     }
 }
 /****************  Add parent Comment  ****************
@@ -575,7 +586,7 @@ function add_child_comment(el, container_el, parent_comment)
             id = `comment_${date_added.getTime()}`,
 	    is_parent=false,
 	    is_comment_owner=true);
-	setBorderLeftColor(c, child_direct_parent, is_parent=false)
+	setBorderLeftColor(c, child_direct_parent)
 	//container_el.prepend(c);
 	container_el.appendChild(c);
         hide_or_add_show_children_btn(container_el.parentElement);
@@ -726,7 +737,7 @@ fetch(base_vote_url).then(response=>{
 			    is_parent=false,
 		            is_comment_owner= user_id===child_data.author__id
 	                );
-			setBorderLeftColor(child_c, parent_to_scroll, is_parent=false)
+			setBorderLeftColor(child_c, parent_to_scroll)
 	                //parent_c.querySelector(".comments_children_container").prepend(child_c);
 	                parent_c.querySelector(".comments_children_container").appendChild(child_c);
 	    	        manage_vote_frontend(child_data.id, child_c);
