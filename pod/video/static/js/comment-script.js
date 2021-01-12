0e081d3dfead007b0969b599ca42c41151955aee
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-01-12 16:45:23 +0100

    fix missing vote

--- pod/video/static/js/comment-script.js
+++ pod/video/static/js/comment-script.js
@@ -146,7 +146,7 @@ class Comment extends HTMLElement {
         else
             comment_container.querySelector('.comment_content_body').appendChild(content);
         let svg_icon = [`<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="star" class="unvoted svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path></svg>`, `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" class="voted svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>`];
-        let vote_action = createFooterBtnAction("comment_actions comment_vote_action", "icon comment_vote_icon", gettext("Agree with the comment"), svg_icon, "comment_vote_btn", `${likes} votes`, id);
+        let vote_action = createFooterBtnAction("comment_actions comment_vote_action", "icon comment_vote_icon", gettext("Agree with the comment"), is_authenticated ? svg_icon : '', "comment_vote_btn", `${likes} votes`, id);
         if (is_authenticated) {
             vote_action.addEventListener("click", () => {
                 let comment_id = get_comment_attribute(document.getElementById(id));
@@ -335,7 +335,7 @@ function vote(comment_id, target_html_el) {
                     'user__id': user_id,
                     'comment__id': comment_id,
                 });
-                btn.innerHTML = parseInt(btn.textContent) + 1;
+                btn.innerHTML = parseInt(btn.textContent) + 1 + " votes";
                 if (!target_html_el.classList.contains('voted'))
                     target_html_el.classList.add('voted');
             }
@@ -343,7 +343,7 @@ function vote(comment_id, target_html_el) {
                 VOTED_USERS = VOTED_USERS.filter(obj => {
                     obj.comment__id !== comment_id
                 })
-                btn.innerHTML = parseInt(btn.textContent) - 1;
+                btn.innerHTML = parseInt(btn.textContent) - 1 + " votes";
                 if (target_html_el.classList.contains('voted'))
                     target_html_el.classList.remove('voted');
             }
