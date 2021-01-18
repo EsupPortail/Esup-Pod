b385d6aa73928f0bc8446f8cc72790a616dd6923
Joshua Baubry <joshua.baubry@gmail.com>
2021-01-18 15:04:08 +0100

    save caption maker in video folder instead of home folder

--- pod/completion/views.py
+++ pod/completion/views.py
@@ -34,8 +34,8 @@ CAPTION_MAKER_ACTION = ['save']
 def video_caption_maker(request, slug):
     video = get_object_or_404(Video, slug=slug,
                               sites=get_current_site(request))
-    user_home_folder = get_object_or_404(
-        UserFolder, name="home", owner=request.user)
+    user_folder = get_object_or_404(
+        UserFolder, name=video.slug, owner=request.user)
     action = None
     if request.user != video.owner and not (
         request.user.is_superuser or request.user.has_perm(
@@ -54,7 +54,7 @@ def video_caption_maker(request, slug):
         return render(
             request,
             'video_caption_maker.html',
-            {'home_folder': user_home_folder,
+            {'current_folder': user_folder,
              'form_make_caption': form_caption,
              'video': video})
 
@@ -62,8 +62,8 @@ def video_caption_maker(request, slug):
 @csrf_protect
 @staff_member_required(redirect_field_name='referrer')
 def video_caption_maker_save(request, video):
-    user_home_folder = get_object_or_404(
-        UserFolder, name="home", owner=request.user)
+    user_folder = get_object_or_404(
+        UserFolder, name=video.slug, owner=request.user)
     if (request.method == "POST"):
         cur_folder = get_current_session_folder(request)
         response = file_edit_save(request, cur_folder)
@@ -79,7 +79,7 @@ def video_caption_maker_save(request, video):
     return render(
         request,
         'video_caption_maker.html',
-        {'home_folder': user_home_folder,
+        {'current_folder': user_folder,
          'form_make_caption': form_caption,
          'video': video})
 
