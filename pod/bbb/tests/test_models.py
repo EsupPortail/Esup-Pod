a255d3bc478b17a0a912acb73e8140bac597811f
Loïc Bonavent <loic.bonavent@umontpellier.fr>
2021-03-05 15:24:08 +0100

    Add tests for the new BBB livestream model

--- pod/bbb/tests/test_models.py
+++ pod/bbb/tests/test_models.py
@@ -2,7 +2,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.utils import timezone
-from ..models import Meeting, User as BBBUser
+from ..models import Meeting, Attendee, Livestream
 
 
 class MeetingTestCase(TestCase):
@@ -68,7 +68,7 @@ class MeetingTestCase(TestCase):
             "   --->  test_delete_object of MeetingTestCase : OK !")
 
 
-class BBBUserTestCase(TestCase):
+class AttendeeTestCase(TestCase):
     fixtures = ['initial_data.json', ]
 
     def setUp(self):
@@ -77,56 +77,118 @@ class BBBUserTestCase(TestCase):
                                           meeting_name="Session BBB1",
                                           encoding_step=0)
 
-        BBBUser.objects.create(id=1, full_name="John Doe",
-                               role="MODERATOR",
-                               meeting=meeting1)
+        Attendee.objects.create(id=1, full_name="John Doe",
+                                role="MODERATOR",
+                                meeting=meeting1)
 
         User.objects.create(username="pod")
         userJaneDoe = User.objects.create(username="pod2")
-        BBBUser.objects.create(id=2, full_name="Jane Doe",
-                               role="MODERATOR",
-                               username="pod2",
-                               meeting=meeting1,
-                               user=userJaneDoe)
+        Attendee.objects.create(id=2, full_name="Jane Doe",
+                                role="MODERATOR",
+                                username="pod2",
+                                meeting=meeting1,
+                                user=userJaneDoe)
 
-        print(" --->  SetUp of BBBUserTestCase : OK !")
+        print(" --->  SetUp of AttendeeTestCase : OK !")
 
     # Test attributes
     def test_attributes(self):
-        bbbUser = BBBUser.objects.get(id=1)
+        attendee = Attendee.objects.get(id=1)
         meeting = Meeting.objects.get(id=1)
-        self.assertEqual(bbbUser.full_name, "John Doe")
-        self.assertEqual(bbbUser.role, "MODERATOR")
-        self.assertEqual(bbbUser.meeting, meeting)
+        self.assertEqual(attendee.full_name, "John Doe")
+        self.assertEqual(attendee.role, "MODERATOR")
+        self.assertEqual(attendee.meeting, meeting)
 
         userJaneDoe = User.objects.get(username="pod2")
-        bbbUser2 = BBBUser.objects.get(id=2)
-        self.assertEqual(bbbUser2.full_name, "Jane Doe")
-        self.assertEqual(bbbUser2.role, "MODERATOR")
-        self.assertEqual(bbbUser2.meeting, meeting)
-        self.assertEqual(bbbUser2.username, "pod2")
-        self.assertEqual(bbbUser2.user, userJaneDoe)
+        attendee2 = Attendee.objects.get(id=2)
+        self.assertEqual(attendee2.full_name, "Jane Doe")
+        self.assertEqual(attendee2.role, "MODERATOR")
+        self.assertEqual(attendee2.meeting, meeting)
+        self.assertEqual(attendee2.username, "pod2")
+        self.assertEqual(attendee2.user, userJaneDoe)
         print(
-            "   --->  test_attributs of BBBUserTestCase : OK !")
+            "   --->  test_attributs of AttendeeTestCase : OK !")
 
     # Test update attributes
     def test_update_attributes(self):
-        bbbUser = BBBUser.objects.get(id=1)
+        attendee = Attendee.objects.get(id=1)
         userJohnDoe = User.objects.get(username="pod")
-        bbbUser.username = "pod"
-        bbbUser.user = userJohnDoe
-        bbbUser.save()
-        self.assertEqual(bbbUser.username, "pod")
-        self.assertEqual(bbbUser.user, userJohnDoe)
+        attendee.username = "pod"
+        attendee.user = userJohnDoe
+        attendee.save()
+        self.assertEqual(attendee.username, "pod")
+        self.assertEqual(attendee.user, userJohnDoe)
         print(
-            "   --->  test_update_attributes of BBBUserTestCase : OK !")
+            "   --->  test_update_attributes of AttendeeTestCase : OK !")
 
     # Test delete object
     def test_delete_object(self):
-        BBBUser.objects.filter(id=1).delete()
-        self.assertEquals(BBBUser.objects.all().count(), 1)
-        BBBUser.objects.filter(id=2).delete()
-        self.assertEquals(BBBUser.objects.all().count(), 0)
+        Attendee.objects.filter(id=1).delete()
+        self.assertEquals(Attendee.objects.all().count(), 1)
+        Attendee.objects.filter(id=2).delete()
+        self.assertEquals(Attendee.objects.all().count(), 0)
 
         print(
-            "   --->  test_delete_object of BBBUserTestCase : OK !")
+            "   --->  test_delete_object of AttendeeTestCase : OK !")
+
+
+class LivestreamTestCase(TestCase):
+    fixtures = ['initial_data.json', ]
+
+    def setUp(self):
+        meeting1 = Meeting.objects.create(id=1, meeting_id="id1",
+                                          internal_meeting_id="internalid1",
+                                          meeting_name="Session BBB1",
+                                          encoding_step=0)
+
+        user1 = User.objects.create(username="pod")
+
+        Livestream.objects.create(id=1,
+                                  start_date=timezone.now(),
+                                  meeting=meeting1,
+                                  user=user1)
+
+        print(" --->  SetUp of LivestreamTestCase : OK !")
+
+    # Test attributes
+    def test_attributes(self):
+        meeting = Meeting.objects.get(id=1)
+        user = User.objects.get(username="pod")
+        livestream = Livestream.objects.get(id=1)
+        self.assertEqual(livestream.status, 0)
+        self.assertEqual(livestream.meeting, meeting)
+        self.assertEqual(livestream.user, user)
+        date = timezone.now()
+        self.assertEqual(livestream.start_date.year, date.year)
+        self.assertEqual(livestream.start_date.month, date.month)
+        self.assertEqual(livestream.start_date.day, date.day)
+
+        print(
+            "   --->  test_attributs of LivestreamTestCase : OK !")
+
+    # Test update attributes
+    def test_update_attributes(self):
+        livestream = Livestream.objects.get(id=1)
+        livestream.status = 1
+        livestream.download_meeting = 1
+        livestream.enable_chat = 1
+        livestream.is_restricted = 1
+        livestream.redis_hostname = "localhost"
+        livestream.redis_port = 6379
+        livestream.broadcaster_id = 1
+        livestream.save()
+        self.assertEqual(livestream.status, 1)
+        self.assertEqual(livestream.download_meeting, 1)
+        self.assertEqual(livestream.redis_hostname, "localhost")
+        self.assertEqual(livestream.redis_port, 6379)
+        self.assertEqual(livestream.broadcaster_id, 1)
+        print(
+            "   --->  test_update_attributes of LivestreamTestCase : OK !")
+
+    # Test delete object
+    def test_delete_object(self):
+        Livestream.objects.filter(id=1).delete()
+        self.assertEquals(Livestream.objects.all().count(), 0)
+
+        print(
+            "   --->  test_delete_object of LivestreamTestCase : OK !")
