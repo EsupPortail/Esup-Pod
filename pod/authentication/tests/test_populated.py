9d4110bfec70db5d3359ffe6f3a13fdd2e062d94
ptitloup <nicolas.can@univ-lille.fr>
2021-04-07 15:36:32 +0200

    create test_populated to check create user and group from CAS and LDAP attributes

---
+++ pod/authentication/tests/test_populated.py
@@ -0,0 +1,49 @@
+# test_populated
+
+from django.test import TestCase
+from pod.authentication.models import Owner, AccessGroup
+from django.contrib.auth.models import User
+# from django.conf import settings
+from django.test.utils import override_settings
+
+from xml.etree import ElementTree as ET
+
+
+class PopulatedTestCase(TestCase):
+    # populate_user_from_tree(user, owner, tree)
+    xml_string = '''<?xml version="1.0" ?>
+<ns0:serviceResponse xmlns:ns0="http://www.yale.edu/tp/cas">
+    <ns0:authenticationSuccess>
+        <ns0:user>pod</ns0:user>
+        <ns0:attributes>
+            <ns0:uid>pod</ns0:uid>
+            <ns0:eduPersonAffiliation>member</ns0:eduPersonAffiliation>
+            <ns0:eduPersonAffiliation>staff</ns0:eduPersonAffiliation>
+            <ns0:eduPersonAffiliation>employee</ns0:eduPersonAffiliation>
+            <ns0:mail>pod@univ.fr</ns0:mail>
+            <ns0:eduPersonPrimaryAffiliation>staff</ns0:eduPersonPrimaryAffiliation>
+            <ns0:sn>Pod</ns0:sn>
+            <ns0:givenname>Univ</ns0:givenname>
+            <ns0:memberOf>cn=group1,ou=groups,dc=univ,dc=fr</ns0:memberOf>
+            <ns0:memberOf>cn=group2,ou=groups,dc=univ,dc=fr</ns0:memberOf>
+        </ns0:attributes>
+    </ns0:authenticationSuccess>
+</ns0:serviceResponse>'''
+
+    def setUp(self):
+        """setUp OwnerTestCase create user pod"""
+        User.objects.create(username="pod", password="pod1234pod")
+        AccessGroup.objects.create(code_name="group1", display_name="Group 1")
+
+    @override_settings(DEBUG=True)
+    def test_populate_user_from_tree(self):
+        owner = Owner.objects.get(user__username="pod")
+        user = User.objects.get(username="pod")
+        self.assertEqual(user.owner, owner)
+        from pod.authentication.populatedCASbackend\
+            import populate_user_from_tree
+        tree = ET.fromstring(self.xml_string)
+        populate_user_from_tree(user, owner, tree)
+        self.assertEqual(user.email, "pod@univ.fr")
+        self.assertEqual(user.first_name, "Pod")
+        self.assertEqual(user.last_name, "Univ")
