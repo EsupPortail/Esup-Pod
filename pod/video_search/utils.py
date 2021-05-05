c18fb8817993558208568c78ee8e1c53f5cd43b3
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-05-05 14:41:00 +0200

    No cover, format code

--- pod/video_search/utils.py
+++ pod/video_search/utils.py
@@ -8,65 +8,91 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-DEBUG = getattr(
-    settings, 'DEBUG', False)
+DEBUG = getattr(settings, "DEBUG", False)
 
-ES_URL = getattr(settings, 'ES_URL', ['http://127.0.0.1:9200/'])
-ES_INDEX = getattr(settings, 'ES_INDEX', 'pod')
-ES_TIMEOUT = getattr(settings, 'ES_TIMEOUT', 30)
-ES_MAX_RETRIES = getattr(settings, 'ES_MAX_RETRIES', 10)
-ES_VERSION = getattr(settings, 'ES_VERSION', 6)
+ES_URL = getattr(settings, "ES_URL", ["http://127.0.0.1:9200/"])
+ES_INDEX = getattr(settings, "ES_INDEX", "pod")
+ES_TIMEOUT = getattr(settings, "ES_TIMEOUT", 30)
+ES_MAX_RETRIES = getattr(settings, "ES_MAX_RETRIES", 10)
+ES_VERSION = getattr(settings, "ES_VERSION", 6)
 
 
-def index_es(video):
+def index_es(video):  # pragma: no cover
     translation.activate(settings.LANGUAGE_CODE)
-    es = Elasticsearch(ES_URL, timeout=ES_TIMEOUT, max_retries=ES_MAX_RETRIES,
-                       retry_on_timeout=True)
+    es = Elasticsearch(
+        ES_URL,
+        timeout=ES_TIMEOUT,
+        max_retries=ES_MAX_RETRIES,
+        retry_on_timeout=True,
+    )
     if es.ping():
         try:
             data = video.get_json_to_index()
-            if data != '{}':
+            if data != "{}":
                 if ES_VERSION == 7:
-                    res = es.index(index=ES_INDEX,
-                                   id=video.id,
-                                   body=data, refresh=True)
+                    res = es.index(
+                        index=ES_INDEX, id=video.id, body=data, refresh=True
+                    )
                 else:
-                    res = es.index(index=ES_INDEX,
-                                   id=video.id, doc_type='pod',
-                                   body=data, refresh=True)
+                    res = es.index(
+                        index=ES_INDEX,
+                        id=video.id,
+                        doc_type="pod",
+                        body=data,
+                        refresh=True,
+                    )
                 if DEBUG:
                     logger.info(res)
                 return res
         except TransportError as e:
-            logger.error("An error occured during index creation: %s-%s : %s" %
-                         (e.status_code, e.error, e.info))
+            logger.error(
+                "An error occured during index creation: %s-%s : %s"
+                % (e.status_code, e.error, e.info)
+            )
     translation.deactivate()
 
 
-def delete_es(video):
-    es = Elasticsearch(ES_URL, timeout=ES_TIMEOUT, max_retries=ES_MAX_RETRIES,
-                       retry_on_timeout=True)
+def delete_es(video):  # pragma: no cover
+    es = Elasticsearch(
+        ES_URL,
+        timeout=ES_TIMEOUT,
+        max_retries=ES_MAX_RETRIES,
+        retry_on_timeout=True,
+    )
     if es.ping():
         try:
             if ES_VERSION == 7:
                 delete = es.delete(
-                    index=ES_INDEX, id=video.id,
-                    refresh=True, ignore=[400, 404])
+                    index=ES_INDEX,
+                    id=video.id,
+                    refresh=True,
+                    ignore=[400, 404],
+                )
             else:
                 delete = es.delete(
-                    index=ES_INDEX, doc_type='pod',
-                    id=video.id, refresh=True, ignore=[400, 404])
+                    index=ES_INDEX,
+                    doc_type="pod",
+                    id=video.id,
+                    refresh=True,
+                    ignore=[400, 404],
+                )
             if DEBUG:
                 logger.info(delete)
             return delete
         except TransportError as e:
-            logger.error("An error occured during delete video : %s-%s : %s" %
-                         (e.status_code, e.error, e.info))
+            logger.error(
+                "An error occured during delete video : %s-%s : %s"
+                % (e.status_code, e.error, e.info)
+            )
 
 
-def create_index_es():
-    es = Elasticsearch(ES_URL, timeout=ES_TIMEOUT, max_retries=ES_MAX_RETRIES,
-                       retry_on_timeout=True)
+def create_index_es():  # pragma: no cover
+    es = Elasticsearch(
+        ES_URL,
+        timeout=ES_TIMEOUT,
+        max_retries=ES_MAX_RETRIES,
+        retry_on_timeout=True,
+    )
     if ES_VERSION == 7:
         template_file = "pod/video_search/search_template7.json"
     else:
@@ -75,7 +101,8 @@ def create_index_es():
     es_template = json.load(json_data)
     try:
         create = es.indices.create(
-            index=ES_INDEX, body=es_template)  # ignore=[400, 404]
+            index=ES_INDEX, body=es_template
+        )  # ignore=[400, 404]
         logger.info(create)
         return create
     except TransportError as e:
@@ -83,19 +110,27 @@ def create_index_es():
         if e.status_code == 400:
             logger.error("Pod index already exists: %s" % e.error)
         else:
-            logger.error("An error occured during"
-                         " index creation: %s-%s : %s" %
-                         (e.status_code, e.error, e.info['error']['reason']))
+            logger.error(
+                "An error occured during"
+                " index creation: %s-%s : %s"
+                % (e.status_code, e.error, e.info["error"]["reason"])
+            )
 
 
-def delete_index_es():
-    es = Elasticsearch(ES_URL, timeout=ES_TIMEOUT, max_retries=ES_MAX_RETRIES,
-                       retry_on_timeout=True)
+def delete_index_es():  # pragma: no cover
+    es = Elasticsearch(
+        ES_URL,
+        timeout=ES_TIMEOUT,
+        max_retries=ES_MAX_RETRIES,
+        retry_on_timeout=True,
+    )
     try:
         delete = es.indices.delete(index=ES_INDEX)
         logger.info(delete)
         return delete
     except TransportError as e:
-        logger.error("An error occured during"
-                     " index video deletion: %s-%s : %s" %
-                     (e.status_code, e.error, e.info['error']['reason']))
+        logger.error(
+            "An error occured during"
+            " index video deletion: %s-%s : %s"
+            % (e.status_code, e.error, e.info["error"]["reason"])
+        )
