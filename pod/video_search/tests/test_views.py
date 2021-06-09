e725d7a3de87d8dabcb9f0bf38444071e295a763
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-06-09 15:35:30 +0200

    Add test_get_remove_selected_facet_link

--- pod/video_search/tests/test_views.py
+++ pod/video_search/tests/test_views.py
@@ -1,8 +1,12 @@
 from datetime import datetime, timedelta
 
 from django.test import TestCase
+from django.test.client import RequestFactory
 
-from pod.video_search.views import get_filter_search
+from pod.video_search.views import (
+    get_filter_search,
+    get_remove_selected_facet_link,
+)
 
 
 class VideoSearchTest(TestCase):
@@ -48,4 +52,31 @@ class VideoSearchTest(TestCase):
             *expected,
         ]
         actual = get_filter_search(selected_facets, start_date, end_date)
+        self.assertEqual(actual, expected)
+
+    def test_get_remove_selected_facet_link(self):
+        request_factory = RequestFactory()
+        request = request_factory.get(
+            "/search/?q=video",
+            {
+                "q": "video",
+                "selected_facets": ["term1:value1", "term2:value2"],
+            },
+        )
+        selected_facets = ["term1:value1", "term2:value2"]
+
+        actual = get_remove_selected_facet_link(request, selected_facets)
+
+        link1 = request.get_full_path().replace(
+            "&selected_facets=term1:value1", ""
+        )
+        link2 = request.get_full_path().replace(
+            "&selected_facets=term2:value2", ""
+        )
+        msg_title = "Remove this filter"
+        expected = [
+            '<a href="%s" title="%s">%s</a>' % (link1, msg_title, "value1"),
+            '<a href="%s" title="%s">%s</a>' % (link2, msg_title, "value2"),
+        ]
+
         self.assertEqual(actual, expected)
