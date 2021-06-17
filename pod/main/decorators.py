fadda16c2d10a990f3fb15a5be113e4fd80e44d9
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-06-17 09:12:58 +0200

    Add admin_required decorator

--- pod/main/decorators.py
+++ pod/main/decorators.py
@@ -1,5 +1,7 @@
 from functools import wraps
 from django.core.exceptions import PermissionDenied
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth import REDIRECT_FIELD_NAME
 
 
 def ajax_required(_view):
@@ -12,3 +14,20 @@ def ajax_required(_view):
             raise PermissionDenied()
 
     return _wrapped_view
+
+
+def admin_required(
+    function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None
+):
+    """
+    Decorator for views that checks that the logged in user is a superuser, redirecting
+    to the log-in page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_authenticated and u.is_superuser,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name,
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
