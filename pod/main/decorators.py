bf39938d4e1e9664547da41e2c6fb595d1c7479e
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-06-24 06:34:38 +0200

    Fix flake8

--- pod/main/decorators.py
+++ pod/main/decorators.py
@@ -16,6 +16,7 @@ def admin_passes_test(
     redirecting to the log-in page if necessary. The test should be a callable
     that takes the user object and returns True if the user passes.
     """
+
     def decorator(_view):
         @wraps(_view)
         def _wrapped_view(request, *args, **kwargs):
@@ -27,19 +28,20 @@ def admin_passes_test(
             # use the path as the "next" url.
             login_scheme, login_netloc = urlparse(resolved_login_url)[:2]
             current_scheme, current_netloc = urlparse(path)[:2]
-            if ((not login_scheme or login_scheme == current_scheme) and
-                    (not login_netloc or login_netloc == current_netloc)):
+            if (not login_scheme or login_scheme == current_scheme) and (
+                not login_netloc or login_netloc == current_netloc
+            ):
                 path = request.get_full_path()
 
             if not request.user.is_authenticated:
-                return redirect_to_login(
-                    path, resolved_login_url, redirect_field_name)
+                return redirect_to_login(path, resolved_login_url, redirect_field_name)
             elif not request.user.is_superuser:
                 raise PermissionDenied()
             else:
                 return _view(request, *args, **kwargs)
 
         return _wrapped_view
+
     return decorator
 
 
@@ -48,6 +50,7 @@ def ajax_required(_view):
     Decorator for views that checks that current request is ajax, raise
     PermissionDenied if necessary.
     """
+
     @wraps(_view)
     def _wrapped_view(request, *args, **kwargs):
 
@@ -60,18 +63,14 @@ def ajax_required(_view):
 
 
 def admin_required(
-    function=None,
-    redirect_field_name=REDIRECT_FIELD_NAME,
-    login_url=None
+    function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None
 ):
     """
     Decorator for views that checks that the logged in user is a superuser, redirecting
     to the log-in page if necessary.
     """
     actual_decorator = admin_passes_test(
-        None,
-        redirect_field_name=redirect_field_name,
-        login_url=login_url
+        None, redirect_field_name=redirect_field_name, login_url=login_url
     )
     if function:
         return actual_decorator(function)
