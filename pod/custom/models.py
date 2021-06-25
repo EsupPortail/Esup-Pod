68540695c86a13275eed06bb6f9284a043eb4701
Eliam LOTONGA <eliam.lotonga@univ-grenoble-alpes.fr>
2021-06-25 09:51:42 +0200

    remove change video owner from custom app

--- pod/custom/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class ManageVideoOwner(models.Model):
-    class Meta:
-        verbose_name = 'Change Video Owner'
-        verbose_name_plural = 'Change Videos Owner'
--- pod/custom/urls.py
+++ pod/custom/urls.py
@@ -4,18 +4,4 @@ from . import views
 
 app_name = "custom"
 
-urlpatterns = [
-    url(r'^$', views.index, name="home"),
-    url(r'^manage/videos/put/(?P<user_id>[\d]+)/$',
-        views.update_video_owner,
-        name="update_video_owner"
-    ),
-    url(r'^manage/videos/owners/$',
-        views.get_owners,
-        name="get_owners"
-    ),
-    url(r'^manage/videos/(?P<user_id>[\d]+)/$',
-        views.get_videos,
-        name="get_videos"
-    ),
-]
+urlpatterns = []
