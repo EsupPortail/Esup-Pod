f231158cc7f9df1042b875b676b3244412adcff4
Céline Didier <celine.didier@univ-lorraine.fr>
2021-06-29 18:09:20 +0200

    Add Playlist REST API

---
+++ pod/playlist/rest_views.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers, viewsets
+
+from .models import Playlist
+from .models import PlaylistElement
+
+import json
+
+# Serializers define the API representation.
+
+class PlaylistSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Playlist
+        fields = (
+            "id",
+            "title",
+            "slug",
+            "owner",
+            "description",
+            "visible"
+        )
+
+class PlaylistElementSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = PlaylistElement
+        fields = (
+            "id",
+            "playlist",
+            "video",
+            "position",
+        )
+
+
+
+#############################################################################
+# ViewSets define the view behavior.
+#############################################################################
+
+
+class PlaylistViewSet(viewsets.ModelViewSet):
+    queryset = Playlist.objects.all()
+    serializer_class = PlaylistSerializer
+
+class PlaylistElementViewSet(viewsets.ModelViewSet):
+    queryset = PlaylistElement.objects.all()
+    serializer_class = PlaylistElementSerializer
