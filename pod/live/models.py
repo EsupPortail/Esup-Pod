49753726f95325acc04e8d58d05732fc66a51fc8
Joshua Baubry <joshua.baubry@gmail.com>
2021-07-09 17:03:23 +0200

    add specific height for aside iframe

--- pod/live/models.py
+++ pod/live/models.py
@@ -102,17 +102,23 @@ class Broadcaster(models.Model):
         null=True,
         blank=True,
     )
+    iframe_height = models.IntegerField(
+        _("Embedded Site Height"),
+        null=True,
+        blank=True,
+        help_text=_("Height of the embedded site (in pixels)"),
+    )
     aside_iframe_url = models.URLField(
         _("Embedded aside Site URL"),
         help_text=_("Url of the embedded site to display on aside"),
         null=True,
         blank=True,
     )
-    iframe_height = models.IntegerField(
-        _("Embedded Site Height"),
+    aside_iframe_height = models.IntegerField(
+        _("Embedded aside Site Height"),
         null=True,
         blank=True,
-        help_text=_("Height of the embedded site (in pixels)"),
+        help_text=_("Height of the aside embedded site (in pixels)"),
     )
     status = models.BooleanField(
         default=0,
