67fdcd74ac2f70cf57e7a24cdd0b255587eb6c7b
gcondess <guillaume.condesse@u-bordeaux.fr>
2021-11-06 11:29:28 +0100

    Enabled Shibboleth authentification to map affiliation to staff user status as it is done with CAS authentification

--- pod/authentication/shibmiddleware.py
+++ pod/authentication/shibmiddleware.py
@@ -1,8 +1,57 @@
 from shibboleth.middleware import ShibbolethRemoteUserMiddleware
 from django.conf import settings
 
-REMOTE_USER_HEADER = getattr(settings, "REMOTE_USER_HEADER", "REMOTE_USER")
+REMOTE_USER_HEADER = getattr(
+    settings, 'REMOTE_USER_HEADER', "REMOTE_USER")
 
+AFFILIATION_STAFF = getattr(
+    settings, 'AFFILIATION_STAFF', ('employee', 'faculty','staff'))
+
+SHIBBOLETH_STAFF_ALLOWED_DOMAINS = getattr(
+    settings, 'SHIBBOLETH_STAFF_ALLOWED_DOMAINS', None)
+
+AFFILIATION = getattr(
+    settings, 'AFFILIATION', (
+    ('student', ''),
+    ('faculty', ''),
+    ('staff', ''),
+    ('employee', ''),
+    ('member', ''),
+    ('affiliate', ''),
+    ('alum', ''),
+    ('library-walk-in',''),
+    ('researcher', ''),
+    ('retired', ''),
+    ('emeritus', ''),
+    ('teacher', ''),
+    ('registered-reader', '')))
 
 class ShibbMiddleware(ShibbolethRemoteUserMiddleware):
     header = REMOTE_USER_HEADER
+
+    def make_profile(self, user, shib_meta):
+        if('affiliation' in shib_meta):
+            if(user and user.owner
+            and shib_meta['affiliation'] in [A[0] for A in AFFILIATION]
+            and user.owner.affiliation != shib_meta['affiliation']):
+                user.owner.affiliation = shib_meta['affiliation']
+                user.owner.save()
+
+        isStaffableUser = False
+        if(SHIBBOLETH_STAFF_ALLOWED_DOMAINS is None
+        or len(SHIBBOLETH_STAFF_ALLOWED_DOMAINS) == 0):
+            isStaffableUser = True
+        else:
+            for d in SHIBBOLETH_STAFF_ALLOWED_DOMAINS:
+                if(user.username.endswith('@'+d)):
+                    isStaffableUser = True
+                    break
+
+        if(isStaffableUser and 'affiliations' in shib_meta):
+            for affiliation in shib_meta['affiliations'].split(';'):
+                if affiliation in AFFILIATION_STAFF:
+                    user.is_staff = True
+                    user.save()
+                    break
+
+        return
