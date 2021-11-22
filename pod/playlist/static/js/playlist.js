84372df9a84dd08201ce25acf4578dfa193bd1b9
ptitloup <nicolas.can@univ-lille.fr>
2021-11-22 16:07:55 +0100

    use regex to parse url, remove reload page and fix link if video added to playlist

--- pod/playlist/static/js/playlist.js
+++ pod/playlist/static/js/playlist.js
@@ -161,24 +161,34 @@ $(window).ready(function () {
     }
   });
 
-  $("#info-video").on("click", ".playlist-item", function () {
+  $("#info-video").on("click", ".playlist-item", function (e) {
+    e.preventDefault();
     const url = window.location.href;
-    const regex = new RegExp('(.*)/video/\\d+-(.*)/');
-    const vmslug = regex.test(url);
-    if (!vmslug) {
+    const regex = new RegExp('(.*)/video/(\\d+-(.*))/');
+    const checkslug = regex.test(url);
+    const foundslug = url.match(regex);
+    if (!checkslug) {
       showalert(
         gettext("The video can not be added from this page."),
         "alert-danger"
       );
       return;
     }
-    const slug = $(this).attr("data-slug"),
-      jqxhr = $.ajax({
+    if (!foundslug[2]) {
+      showalert(
+        gettext("The video slug not found."),
+        "alert-danger"
+      );
+      return;
+    }
+    const slug = $(this).attr("data-slug");
+    const link = $(this);
+    const jqxhr = $.ajax({
         method: "POST",
         url: "/playlist/edit/" + slug + "/",
         data: {
           action: "add",
-          video: vmslug[1],
+          video: foundslug[2],
           csrfmiddlewaretoken: $(this)
             .parents(".dropdown-menu")
             .find("input")
@@ -197,7 +207,8 @@ $(window).ready(function () {
       } else {
         if (response.success) {
           showalert(response.success, "alert-success");
-          window.location.reload();
+          //window.location.reload(); //hide link playlist
+          link.addClass("disabled").removeClass("playlist-item").append('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check align-bottom"><polyline points="20 6 9 17 4 12"></polyline></svg>')
         } else {
           showalert(response.fail, "alert-danger");
         }
