3eaaac4a739f1c07accf221ea1927cc0c07ea0d8
Joshua Baubry <joshua.baubry@gmail.com>
2022-01-10 19:50:32 +0100

    test

--- pod/settings.py
+++ pod/settings.py
@@ -279,3 +279,4 @@ if "H5P_ENABLED" in globals() and eval("H5P_ENABLED") is True:
     sys.path.append(os.path.join(BASE_DIR, "../../H5PP"))
     INSTALLED_APPS.append("h5pp")
     INSTALLED_APPS.append("pod.interactive")
+# test
