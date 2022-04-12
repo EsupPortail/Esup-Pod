f4d74e9334dadcdc0033dc2dfb870fdb797f0897
Céline Didier <ceine.didier@univ-lorraine.fr>
2022-04-12 10:27:29 +0200

    Increase AccessGroup code length

--- pod/authentication/models.py
+++ pod/authentication/models.py
@@ -170,7 +170,7 @@ def create_groupsite_profile(sender, instance, created, **kwargs):
 
 class AccessGroup(models.Model):
     display_name = models.CharField(max_length=128, blank=True, default="")
-    code_name = models.CharField(max_length=128, unique=True)
+    code_name = models.CharField(max_length=250, unique=True)
     sites = models.ManyToManyField(Site)
     users = select2_fields.ManyToManyField(
         Owner,
