082bbcf128e9b2daa5678ffa8e5fdca0cb1195cc
Nicolas <nicolas.can@univ-lille.fr>
2022-05-09 16:23:34 +0200

    add update settings for enodage settings

--- pod/custom/tenants/source_enc/tenant_enc_settings.py
+++ pod/custom/tenants/source_enc/tenant_enc_settings.py
@@ -22,4 +22,7 @@ CONTACT_US_EMAIL = ['contact@__DOMAIN_NAME__']  # to complete
 CELERY_TO_ENCODE = True  # Active encode
 CELERY_BROKER_URL = CELERY_BROKER_URL + "-__NAME__"  # Define a broker
 
-update_tenant_settings("__NAME__")
+########################################################## A LAISSER EN DERNIER !!!!!!!!!!!!!
+the_update_settings = update_settings(locals())
+for variable in the_update_settings:
+    locals()[variable] = the_update_settings[variable]
