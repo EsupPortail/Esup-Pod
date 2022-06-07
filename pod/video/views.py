d268e0dd74fe6d5884f2558383e05c3d2be37510
pampletousse <sloppin23@gmail.com>
2022-06-07 11:30:50 +0200

    WIP - fix owner gesture on My_videos screen

--- pod/video/views.py
+++ pod/video/views.py
@@ -535,6 +535,8 @@ def my_videos(request):
     except EmptyPage:
         videos = paginator.page(paginator.num_pages)
 
+    ownersInstances = get_owners_has_instances(request.GET.getlist("owner"))
+
     if request.is_ajax():
         return render(
             request,
@@ -545,7 +547,9 @@ def my_videos(request):
     data_context["videos"] = videos
     data_context["count_videos"] = count_videos
     data_context["full_path"] = full_path
+    data_context["owners"] = request.GET.getlist("owner")
     data_context["page_title"] = _("My videos")
+    data_context["ownersInstances"] = ownersInstances
 
     return render(request, "videos/my_videos.html", data_context)
 
