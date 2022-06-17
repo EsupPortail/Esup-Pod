fc61754a7419f5338080928104fbd1ed99406df0
Ptitloup <nicolas.can@univ-lille.fr>
2022-06-17 12:10:02 +0200

    Update populatedCASbackend.py
    Fix pep8 compliant

--- pod/authentication/populatedCASbackend.py
+++ pod/authentication/populatedCASbackend.py
@@ -220,11 +220,14 @@ def populate_user_from_entry(user, owner, entry):
         else ""
     )
     user.last_name = ""
-    if USER_LDAP_MAPPING_ATTRIBUTES.get("last_name") and entry[USER_LDAP_MAPPING_ATTRIBUTES["last_name"]]:
+    if (
+        USER_LDAP_MAPPING_ATTRIBUTES.get("last_name")
+        and entry[USER_LDAP_MAPPING_ATTRIBUTES["last_name"]]
+    ):
         user.last_name = (
             entry[USER_LDAP_MAPPING_ATTRIBUTES['last_name']].value[0] if (
                 isinstance(entry[USER_LDAP_MAPPING_ATTRIBUTES['last_name']].value, list)
-                ) else entry[USER_LDAP_MAPPING_ATTRIBUTES['last_name']].value
+            ) else entry[USER_LDAP_MAPPING_ATTRIBUTES['last_name']].value
         )
     user.save()
     owner.affiliation = (
