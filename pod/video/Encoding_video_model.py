59a70b7e2828044a49af0d943decce4811fc2e9d
JoshuaBaubry <joshua.baubry@gmail.com>
2022-07-06 21:48:28 +0000

    start encoding video model

--- pod/video/Encoding_video_model.py
+++ pod/video/Encoding_video_model.py
@@ -4,11 +4,19 @@ from .models import EncodingVideo
 from .models import EncodingAudio
 from .models import PlaylistVideo
 from .models import Video
-
+from .utils import create_outputdir, change_encoding_step, fix_video_duration, send_email_encoding
 from .Encoding_video import Encoding_video, FFMPEG_MP4_ENCODE
+import json
+import time
 
 FFMPEG_MP4_ENCODE = getattr(settings, "FFMPEG_MP4_ENCODE", FFMPEG_MP4_ENCODE)
+EMAIL_ON_ENCODING_COMPLETION = getattr(settings, "EMAIL_ON_ENCODING_COMPLETION", True)
+TRANSCRIPT = getattr(settings, "USE_TRANSCRIPTION", False)
 
+if TRANSCRIPT:
+    from . import transcript
+
+    TRANSCRIPT_VIDEO = getattr(settings, "TRANSCRIPT_VIDEO", "start_transcript")
 
 class Encoding_video_model(Encoding_video):
 
@@ -70,3 +78,45 @@ class Encoding_video_model(Encoding_video):
         else:
             msg += "Playlist: Nothing to delete"
         return msg
+
+    def store_encoding_video(self, video_to_store):
+        video = Video.objects.get(id=video_to_store)
+        output_dir = create_outputdir(video_to_store, video.video.path)
+        info_video = {}
+
+        with open(output_dir + "/info_video.json") as json_file:
+            info_video = json.load(json_file)
+
+        video.duration = info_video["duration"]
+        video.encoding_in_progress = True
+        video.save()
+
+        video = Video.objects.get(id=video_to_store)  # refresh given object
+
+        # remote_video_part(video_to_encode, info_video, output_dir)
+        # remote_audio_part(video_to_encode, info_video, output_dir)
+
+        if not info_video["has_stream_video"]:
+            video.is_video = False
+            video.save()
+
+        change_encoding_step(video_to_store, 0, "done")
+
+        video.encoding_in_progress = False
+        video.save()
+
+        fix_video_duration(video.id, output_dir)
+
+        with open(output_dir + "/encoding.log", "a") as f:
+            f.write("\n\nEnd : %s" % time.ctime())
+
+        # envois mail fin encodage
+        if EMAIL_ON_ENCODING_COMPLETION:
+            send_email_encoding(video)
+
+        # Transcript
+        if TRANSCRIPT and video.transcript:
+            transcript_video = getattr(transcript, TRANSCRIPT_VIDEO)
+            transcript_video(video_to_store, False)
+
+        print("ALL is DONE")
