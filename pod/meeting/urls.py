eb65758dafc1b3052dc607948be2fbca64b17e0c
ptitloup <nicolas.can@univ-lille.fr>
2022-07-11 17:28:16 +0200

    work on meeting form

--- pod/meeting/urls.py
+++ pod/meeting/urls.py
@@ -6,6 +6,6 @@ app_name = 'meeting'
 
 urlpatterns = [
     path('', views.my_meetings, name='my_meetings'),
-    path('<slug:meeting_id>/', views.join, name='join'),
     path('add/', views.add, name='add'),
+    path('<slug:meeting_id>/', views.join, name='join'),
 ]
--- pod/meeting/views.py
+++ pod/meeting/views.py
@@ -8,6 +8,9 @@ from django.core.exceptions import SuspiciousOperation
 from django.views.decorators.csrf import csrf_protect
 
 from .models import Meeting
+from .forms import MeetingForm
+
+RESTRICT_EDIT_MEETING_ACCESS_TO_STAFF_ONLY = False
 
 
 def index(request):
@@ -27,11 +30,29 @@ def my_meetings(request):
     )
 
 
+@csrf_protect
 @login_required(redirect_field_name="referrer")
 def add(request):
-    # site = get_current_site(request)
-
-    return HttpResponse("Hello, world. You're at the meeting index.")
+    """
+    if request.user not in channel.owners.all() and not (
+        request.user.is_superuser or request.user.has_perm("video.change_channel")
+    ):
+        messages.add_message(request, messages.ERROR, _("You cannot edit this channel."))
+        raise PermissionDenied
+    """
+    if RESTRICT_EDIT_MEETING_ACCESS_TO_STAFF_ONLY and request.user.is_staff is False:
+        return render(request, "meeting/add.html", {"access_not_allowed": True})
+    form = MeetingForm(
+        is_staff=request.user.is_staff,
+        is_superuser=request.user.is_superuser,
+        current_user=request.user,
+        initial={"owner": request.user},
+    )
+    return render(
+        request,
+        "meeting/add.html",
+        {"form": form, },
+    )
 
 
 @csrf_protect
@@ -40,7 +61,7 @@ def join(request, meeting_id):
         id = int(meeting_id[: meeting_id.find("-")])
     except ValueError:
         raise SuspiciousOperation("Invalid video id")
-    meeting = get_object_or_404(Meeting, id=id, sites=get_current_site(request))
+    meeting = get_object_or_404(Meeting, id=id, site=get_current_site(request))
     return render(
         request,
         "meeting/join.html",
