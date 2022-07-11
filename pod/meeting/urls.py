059cc4caf449420ba4b2940ee96f14d692d6099f
ptitloup <nicolas.can@univ-lille.fr>
2022-07-11 15:44:30 +0200

    add add view and join view

--- pod/meeting/urls.py
+++ pod/meeting/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'meeting'
+
 urlpatterns = [
     path('', views.my_meetings, name='my_meetings'),
+    path('<slug:meeting_id>/', views.join, name='join'),
+    path('add/', views.add, name='add'),
 ]
--- pod/meeting/views.py
+++ pod/meeting/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.utils.translation import ugettext_lazy as _
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import SuspiciousOperation
+from django.views.decorators.csrf import csrf_protect
+
+from .models import Meeting
 
 
 def index(request):
@@ -20,4 +24,25 @@ def my_meetings(request):
         request,
         "meeting/my_meetings.html",
         {"meetings": meetings, "page_title": _("My meetings")},
+    )
+
+
+@login_required(redirect_field_name="referrer")
+def add(request):
+    # site = get_current_site(request)
+
+    return HttpResponse("Hello, world. You're at the meeting index.")
+
+
+@csrf_protect
+def join(request, meeting_id):
+    try:
+        id = int(meeting_id[: meeting_id.find("-")])
+    except ValueError:
+        raise SuspiciousOperation("Invalid video id")
+    meeting = get_object_or_404(Meeting, id=id, sites=get_current_site(request))
+    return render(
+        request,
+        "meeting/join.html",
+        {"meeting": meeting, },
     )
