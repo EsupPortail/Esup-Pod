60eb4258a499bcb42edadb76778e3f7aae7093b8
ptitloup <nicolas.can@univ-lille.fr>
2022-10-12 16:27:27 +0200

    create def to recreate thumbnail for specific video - add command to call it

---
+++ pod/video/management/commands/encode.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from pod.video.encode import encode_video
+
+
+class Command(BaseCommand):
+    help = "encoding video by id"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "id",
+            type=int,
+            help="The id of the video",
+        )
+
+    def handle(self, *args, **options):
+        encode_video(options["id"])
