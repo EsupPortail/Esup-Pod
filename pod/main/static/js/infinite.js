6bdc79dd54332015f0689b4f874b2d865c5aa065
Mateo <mateojvaldes60@gmail.com>
2022-10-14 12:21:07 +0200

    infinite bugs really badly

--- pod/main/static/js/infinite.js
+++ pod/main/static/js/infinite.js
@@ -11,29 +11,46 @@ function isFooterInView() {
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
+/*
+const isElementXPercentInViewport = function () {
+  percentVisible = 95;
+  var footer = document.querySelector(
+    "footer.container-fluid.pod-footer-container"
+  );
+  let rect = footer.getBoundingClientRect(),
+    windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+  return !(
+    Math.floor(100 - ((rect.top >= 0 ? 0 : rect.top) / +-rect.height) * 100) <
+      percentVisible ||
+    Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) <
+      percentVisible
+  );
+};
+
+*/
 
 //chech if user
 
 class InfiniteLoader {
-  constructor(url, onBeforePageLoad, onAfterPageLoad) {
+  constructor(url, callBackBeforeLoad, callBackAfterLoad) {
     this.infinite_loading = document.querySelector(".infinite-loading");
     this.videos_list = document.getElementById("videos_list");
     this.page = 1;
-    this.onBeforePageLoad = onBeforePageLoad;
-    this.onAfterPageLoad = onAfterPageLoad;
+    this.callBackBeforeLoad = callBackBeforeLoad;
+    this.callBackAfterLoad = callBackAfterLoad;
     this.url = url;
-    window.addEventListener("load", (e) => {
-      this.initMore();
-    });
+   
     window.addEventListener("scroll", (e) => {
       this.initMore();
     });
   }
 
   initMore() {
+    console.log(isFooterInView())
     if (isFooterInView()) {
       let url = this.url;
-      this.onBeforePageLoad();
+      this.callBackBeforeLoad();
       this.getData(url, this.page);
       console.log("page", this.page);
       this.page += 1;
@@ -41,6 +58,7 @@ class InfiniteLoader {
   }
 
   getData(url, page) {
+    if (!url) return;
     url = url + page;
     fetch(url, {
       method: "GET",
@@ -53,13 +71,14 @@ class InfiniteLoader {
       .then((response) => response.text())
       .then((data) => {
         // parse data into html
+
         let html = new DOMParser().parseFromString(data, "text/html").body
-          .firstChild;
-        let element = this.videos_list;
+        let element = this.videos_list.parentNode;
         html.childNodes.forEach(function (node) {
+          console.log(node)
           element.appendChild(node);
         });
-        this.onAfterPageLoad();
+        this.callBackAfterLoad();
       })
       .catch((error) => {
         console.log(error);
--- pod/video/static/js/filter_aside_video_list_refresh.js
+++ pod/video/static/js/filter_aside_video_list_refresh.js
@@ -2,90 +2,114 @@ var infinite_waypoint;
 var formCheckedInputs = [];
 var regExGetOnlyChars = /([\D])/g;
 
-function getInfiniteScrollWaypoint() {
-  // Return Waypoint Infinite object to init/refresh the infinite scroll
-  let infinite_loading = document.querySelector(".infinite-loading");
-  return new Waypoint.Infinite({
-    element: document.getElementById("videos_list"),
-    onBeforePageLoad: function () {
-      infinite_loading.style.display = "block";
-    },
-    onAfterPageLoad: function ($items) {
-      infinite_loading.style.display = "none";
-      let footer = document.querySelector("footer.static-pod");
-      footer.classList.add("small");
-      footer.classList.add("fixed-bottom");
-      footer.setAttribute("style", "height:80px; overflow-y:auto");
-      var docHeight = Math.max(
-        body.scrollHeight,
-        body.offsetHeight,
-        html.clientHeight,
-        html.scrollHeight,
-        html.offsetHeight
-      );
+// Return Waypoint Infinite object to init/refresh the infinite scroll
+let infinite_loading = document.querySelector(".infinite-loading");
+
+element = document.getElementById("videos_list");
+onBeforePageLoad = function () {
+  infinite_loading.style.display = "block";
+};
+onAfterPageLoad = function () {
+  infinite_loading.style.display = "none";
+  let footer = document.querySelector("footer.static-pod");
+  if (!footer) return;
+  footer.classList.add("small");
+  footer.classList.add("fixed-bottom");
+  footer.setAttribute("style", "height:80px; overflow-y:auto");
+  var docHeight = Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight,
+    html.offsetHeight
+  );
+  document.querySelector("footer.static-pod .hidden-pod").style.display =
+    "none";
+  window.addEventListener("scroll", function () {
+    if (window.innerHeight + window.scrollTop() === docHeight) {
       document.querySelector("footer.static-pod .hidden-pod").style.display =
-        "none";
-      window.addEventListener("scroll", function () {
-        if (window.innerHeight + window.scrollTop() === docHeight) {
-          document.querySelector(
-            "footer.static-pod .hidden-pod"
-          ).style.display = "block";
-          footer.setAttribute("style", "height:auto;");
-          footer.classList.remove("fixed-bottom");
-        }
-      });
-    },
+        "block";
+      footer.setAttribute("style", "height:auto;");
+      footer.classList.remove("fixed-bottom");
+    }
   });
-}
+};
+url = "";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  var infinite = new InfiniteLoader(url, onBeforePageLoad, onAfterPageLoad);
+});
 
 function replaceCountVideos(newCount) {
   // Replace count videos label (h1) with translation and plural
   var transVideoCount = newCount > 1 ? "videos found" : "video found";
-  $("#video_count")[0].innerHTML = newCount + " " + gettext(transVideoCount);
+  document.getElementById("video_count").innerHTML =
+    newCount + " " + gettext(transVideoCount);
 }
 
 function refreshVideosSearch(formCheckedInputs) {
   // Ajax request to refresh view with filtered video list
+  url = urlVideos;
+  data = formCheckedInputs;
 
-  return $.ajax({
-    type: "GET",
-    url: urlVideos,
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "X-CSRFToken": "{{ csrf_token }}",
+    },
     data: formCheckedInputs,
     dataType: "html",
-    headers: {
-      "X-Requested-With": "XMLHttpRequest",
-    },
-    success: function (html, status) {
-      if (infinite_waypoint) {
-        infinite_waypoint.destroy();
-      }
-      $(".infinite-loading").remove();
-      $(".infinite-more-link").remove();
-      $("#videos_list").replaceWith(html);
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      // parse data into html and replace videos list
+      // destroy waypoint id exists
+
+      let html = new DOMParser().parseFromString(data, "text/html").body
+        .firstChild;
+      //document.querySelector(".infiniteloading").style.display = "none";
+      //document.querySelector(".infinite-more-link").style.display = "none";
+      document.getElementById("videos_list").innerHTML = "";
       replaceCountVideos(countVideos);
       window.history.pushState({}, "", this.url);
-    },
-    error: function (result, status, error) {
-      $("#videos_list").html(gettext("An Error occurred while processing."));
-    },
-  });
+      hideInfiniteloading();
+    })
+    .catch((error) => {
+      console.log(error);
+      document.getElementById("videos_list").innerHTML = gettext(
+        "An Error occurred while processing."
+      );
+    });
 }
+hideInfiniteloading = function () {
+  //document.querySelector(".infiniteloading").style.display = "none";
+  // get waypoint object
+  document
+    .querySelectorAll(".form-check-input input[type=checkbox]")
+    .forEach((checkbox) => {
+      checkbox.removeAttribute("disabled");
+    });
+};
 
-$(".form-check-input").change(function () {
-  // Filter checkboxes change triggered event
-  formCheckedInputs = [];
-  $(".infinite-loading").show();
-  $(".form-check-input input[type=checkbox]").attr("disabled", "true");
-  $("#videos_list").html("");
-  $("input[type=checkbox]:checked").each(function () {
-    formCheckedInputs.push(this);
-  });
-  refreshVideosSearch(formCheckedInputs).done(function () {
-    $(".infinite-loading").hide();
-    infinite_waypoint = getInfiniteScrollWaypoint();
-    $(".form-check-input input[type=checkbox]").removeAttr("disabled");
+document.querySelectorAll(".form-check-input").forEach((checkbox) => {
+  checkbox.addEventListener("change", function () {
+    // Filter checkboxes change triggered event
+    console.log("checkbox changed");
+    formCheckedInputs = [];
+    document.querySelector(".infinite-loading").display = "block";
+    document
+      .querySelectorAll(".form-check-input input[type=checkbox]")
+      .forEach((checkbox) => {
+        checkbox.setAttribute("disable", "true");
+      });
+
+    document.getElementById("videos_list").innerHTML = "";
+    document.querySelectorAll("input[type=checkbox]:checked").forEach((e) => {
+      formCheckedInputs.push(e);
+    });
+    refreshVideosSearch(formCheckedInputs);
   });
 });
 
 // First launch of the infinite scroll
-infinite_waypoint = getInfiniteScrollWaypoint();
+//infinite_waypoint = getInfiniteScrollWaypoint();
