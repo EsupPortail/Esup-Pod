58bc8a2b82b0b562ff65c447f82c008cc9694757
ptitloup <nicolas.can@univ-lille.fr>
2022-10-21 15:45:31 +0200

    add simple test to feed view (to complete later)

---
+++ pod/video/tests/test_feeds.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.test import Client
+from http import HTTPStatus
+from django.urls import reverse
+
+from xml.dom import minidom
+
+
+class FeedTestView(TestCase):
+    fixtures = [
+        "initial_data.json",
+    ]
+
+    def setUp(self):
+        pass
+
+    def test_get_rss_video_from_video(self):
+        self.client = Client()
+        url = reverse("rss-video:rss-video", kwargs={})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        mediaPackage_content = minidom.parseString(response.content)
+        mediapackage = mediaPackage_content.getElementsByTagName("rss")[0]
+        self.assertTrue(mediapackage)
+        print(
+            " -->  test_get_rss_video_from_video of FeedTestView", " : OK !"
+        )
+
+    def test_get_rss_audio_from_video(self):
+        self.client = Client()
+        url = reverse("rss-video:rss-audio", kwargs={})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        mediaPackage_content = minidom.parseString(response.content)
+        mediapackage = mediaPackage_content.getElementsByTagName("rss")[0]
+        self.assertTrue(mediapackage)
+        print(
+            " -->  test_get_rss_audio_from_video of FeedTestView", " : OK !"
+        )
