9e39162be7327cfbfb05afb06f262c3b808d93b1
loanR <loan.rbrt@gmail.com>
2022-10-26 16:28:27 +0200

    Add configurable proxy environment variable through settings

--- pod/wsgi.py
+++ pod/wsgi.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pod.settings")
+proxy_host = getattr(settings, "PROXY_HOST", None)
+proxy_port = getattr(settings, "PROXY_PORT", None)
+if proxy_host and proxy_port:
+    os.environ['http_proxy'] = os.environ['https_proxy'] = f'{proxy_host}:{proxy_port}'
 
 
 application = get_wsgi_application()
