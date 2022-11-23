a0fc8aa79ef5fe0e4df43177107e3fbaf78ae24b
farid.aitkarra <farid.aitkarra@univ-lille.fr>
2022-11-23 15:18:37 +0100

    feature/conteneurisation : - aj de la documentation - 3 - correction d'une django.db.migrations.exceptions.inconsistentmigrationhistory concernant les flatpages dans un context de conteneurisation d'esup-pod

--- dockerfile-dev-with-volumes/reset-local-pod.sh
+++ dockerfile-dev-with-volumes/reset-local-pod.sh
@@ -5,7 +5,7 @@ docker-compose -f ../docker-compose-dev-with-volumes.yml -p esup-pod down -v
 sudo rm -rf ../pod/log
 sudo rm -rf ../pod/media
 sudo rm -rf ../pod/static
-sudo rm -rf ../pod/node_modules/*
-sudo rm -rf ../pod/node_modules/.*
+sudo rm -rf ../pod/node_modules
+sudo rm -rf ../pod/db_migrations
 sudo rm ../pod/db.sqlite3
 sudo rm ../pod/yarn.lock
