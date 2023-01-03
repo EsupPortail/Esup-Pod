bac86d7ec41e284aa33ab4f1ee74a67d43de3867
ptitloup <nicolas.can@univ-lille.fr>
2023-01-03 10:02:31 +0100

    use log function to print data and use with to open markdown file

--- pod/main/management/commands/getsettings.py
+++ pod/main/management/commands/getsettings.py
@@ -1,3 +1,4 @@
+from typing import List
 from django.core.management.base import BaseCommand
 import re
 import os
@@ -24,26 +25,25 @@ class Command(BaseCommand):
             global_settings_list += settings_list
         global_settings_list = list(dict.fromkeys(global_settings_list))
         global_settings_list.sort()
-        print(20 * "-")
-        print(options["app_name"] + " :")
-        print("\n    - " + "\n    - ".join(global_settings_list))
-        print(20 * "-")
-        mk_file = open(os.path.join("pod", "custom", "ConfigurationPod.md"), "r")
+        self.print_log(options["app_name"], global_settings_list)
         mk_settings = []
-        for line in mk_file:
-            match = re.search(r'- (?P<set>\w+) (=|:)?.*', line)
-            if match:
-                settings = match.group('set')
-                if settings.isupper() and settings not in mk_settings:
-                    mk_settings.append(settings)
+        with open(os.path.join("pod", "custom", "ConfigurationPod.md"), "r") as  mk_file:
+            for line in mk_file:
+                match = re.search(r'- (?P<set>\w+) (=|:)?.*', line)
+                if match:
+                    settings = match.group('set')
+                    if settings.isupper() and settings not in mk_settings:
+                        mk_settings.append(settings)
         mk_settings.sort()
-        print("Configuration settings :")
-        print("\n    - " + "\n    - ".join(mk_settings))
+        self.print_log("Configuration setting", mk_settings)
         new_settings = list(set(global_settings_list) - set(mk_settings))
         new_settings.sort()
-        print(20 * "-")
-        print("New settings :")
-        print("\n    - " + "\n    - ".join(new_settings))
-        print(20 * "-")
+        self.print_log("New settings", new_settings)
         # raise CommandError('Poll "%s" does not exist' % poll_id)
         # self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
+
+    def print_log(self, title: str, data: List[str]) -> None:
+        print(20 * "-")
+        print(f"{title} :")
+        print("\n    - " + "\n    - ".join(data))
+        # print(20 * "-")
