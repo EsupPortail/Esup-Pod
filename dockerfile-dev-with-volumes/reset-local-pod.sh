d94cc870bb70dd98495d2cbdecf439b4a4815877
farid.aitkarra <farid.aitkarra@univ-lille.fr>
2023-01-11 10:50:44 +0100

    feature/conteneurisation : - ajout d'un fichier my-entrypoint.sh - maj de la documentation

--- dockerfile-dev-with-volumes/reset-local-pod.sh
+++ dockerfile-dev-with-volumes/reset-local-pod.sh
@@ -2,10 +2,10 @@
 #!/bin/bash
 set -xv
 docker-compose -f ../docker-compose-dev-with-volumes.yml -p esup-pod down -v
-sudo rm -rf ../pod/log
-sudo rm -rf ../pod/media
-sudo rm -rf ../pod/static
-sudo rm -rf ../pod/node_modules
-sudo rm -rf ../pod/db_migrations
-sudo rm ../pod/db.sqlite3
-sudo rm ../pod/yarn.lock
+sudo rm -rf ./pod/log
+sudo rm -rf ./pod/media
+sudo rm -rf ./pod/static
+sudo rm -rf ./pod/node_modules
+sudo rm -rf ./pod/db_migrations
+sudo rm ./pod/db.sqlite3
+sudo rm ./pod/yarn.lock
