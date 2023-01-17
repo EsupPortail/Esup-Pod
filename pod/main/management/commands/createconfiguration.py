658e9d208f3fdfa12009c903320ead2215a6521e
ptitloup <nicolas.can@univ-lille.fr>
2023-01-17 14:52:35 +0100

    fix flake8 in createconfiguration

--- pod/main/management/commands/createconfiguration.py
+++ pod/main/management/commands/createconfiguration.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
-import json, os
+import json
+import os
+
 
 class Command(BaseCommand):
     help = 'Export configuration to markdown'
@@ -19,50 +21,37 @@ class Command(BaseCommand):
             raise CommandError(
                 'Langage must be fr or en'
             )
-        
         filename = os.path.join("pod", "custom", "configuration.json")
         with open(filename, "r", encoding='utf-8') as json_file:
             self.data = json.load(json_file)
-
         # header, information, configuration_pod, configuration_apps
         output = ""
-
         output += "# %s \n" % self.data[0]["header"][self.language]
-
         output += self.get_information()
-
         output += self.get_configuration("pod")
-
         output += self.get_configuration("apps")
 
         md_filename = os.path.join("pod", "custom", "configuration_%s.md" % self.language)
-
-        open(md_filename, "w").close() # erase it
-
+        open(md_filename, "w").close()  # erase it
         with open(md_filename, 'w', encoding='utf-8') as f:
             f.write(output)
 
         self.stdout.write(self.style.SUCCESS('Successfully export configuration'))
 
-
     def get_information(self):
         msg = "\n"
         msg += "## %s \n" % self.data[0]["information"]["title"][self.language]
-
         for line in self.data[0]["information"]["description"][self.language]:
             if line != "":
                 msg += "\n%s <br/>" % line
             else:
                 msg += "\n"
-        
         msg += self.get_settings(self.data[0]["information"]["settings"])
-
         return msg
-    
+
     def get_configuration(self, app):
         msg = "\n"
         msg += "## %s \n" % self.data[0]["configuration_%s" % app]["title"][self.language]
-
         descs = self.data[0]["configuration_%s" % app]["description"]
         for key, desc in descs.items():
             msg += "\n### %s \n" % desc["title"][self.language]
@@ -75,7 +64,6 @@ class Command(BaseCommand):
 
         msg += self.get_settings(self.data[0]["configuration_%s" % app]["settings"])
         return msg
-
 
     def get_settings(self, settings):
         msg = "\n"
@@ -94,4 +82,3 @@ class Command(BaseCommand):
                 else:
                     msg += "\n"
         return msg
-
