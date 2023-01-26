bc79e311b4f54cb2a7e661bc438d058e682a44f2
ptitloup <nicolas.can@univ-lille.fr>
2023-01-26 16:39:01 +0100

    add compare setting command to check local configuration with gloabl configuration

---
+++ pod/main/management/commands/comparesettings.py
@@ -0,0 +1,52 @@
+from typing import List
+from django.core.management.base import BaseCommand
+
+# import re
+import os
+import fnmatch
+import importlib
+import json
+
+
+class Command(BaseCommand):
+    help = "Get all settings for the specified app and compare it to configuration file"
+
+    def handle(self, *args, **options):
+        mod = importlib.import_module(
+            ".".join(["pod", "custom", "settings_local"])
+        )
+        items = dir(mod)
+        settings_list = [
+            item
+            for item in items
+            if (not item.startswith("__") and item.isupper() and len(item) > 1)
+        ]
+        local_settings_list = list(dict.fromkeys(settings_list))
+        local_settings_list.sort()
+        self.print_log("Settings local", local_settings_list)
+        json_settings = self.get_all_settings()
+        new_settings = list(set(local_settings_list) - set(json_settings))
+        new_settings.sort()
+        self.print_log("Local setting not in global configuration file : ", new_settings)
+        # raise CommandError('Poll "%s" does not exist' % poll_id)
+        # self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
+
+    def print_log(self, title: str, data: List[str]) -> None:
+        print(20 * "-")
+        print(f"{title} :")
+        print("\n    - " + "\n    - ".join(data))
+        # print(20 * "-")
+    
+    def get_all_settings(self):
+        with open(os.path.join("pod", "main", "configuration.json"), "r") as json_file:
+            data = json.load(json_file)
+        json_settings = []
+        pod_settings = data[0]["configuration_pod"]["description"]
+        for keys in pod_settings.keys():
+            keys_settings = pod_settings[keys]["settings"]
+            json_settings += keys_settings.keys()
+        app_settings = data[0]["configuration_apps"]["description"]
+        for keys in app_settings.keys():
+            keys_settings = app_settings[keys]["settings"]
+            json_settings += keys_settings.keys()
+        return json_settings
