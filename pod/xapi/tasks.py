da029466baad52efba4f48f78d39fb458e1fd60a
ptitloup <nicolas.can@univ-lille.fr>
2023-02-02 15:28:04 +0100

    use celery to send statement - remove ralph malph which is not compatible with python 3.7

---
+++ pod/xapi/tasks.py
@@ -0,0 +1,27 @@
+# xapi/tasks.py
+from django.conf import settings
+from celery import shared_task
+import logging
+import requests
+from requests.auth import HTTPBasicAuth
+
+logger = logging.getLogger(__name__)
+
+XAPI_LRS_URL = getattr(settings, "XAPI_LRS_URL", "")
+XAPI_LRS_LOGIN = getattr(settings, "XAPI_LRS_LOGIN", "")
+XAPI_LRS_PWD = getattr(settings, "XAPI_LRS_PWD", "")
+
+
+@shared_task()
+def send_xapi_statement_task(statement):
+    """Sends the xapi statement to the specify LRS."""
+    x = requests.post(
+        XAPI_LRS_URL,
+        json=statement,
+        auth=HTTPBasicAuth(XAPI_LRS_LOGIN, XAPI_LRS_PWD)
+    )
+    if x.status_code == 200:
+        print(x.text)
+        logger.info('statement id : %s' % x.text)
+    else:
+        logger.error('Error during sending statement : %s' % x.text)
--- pod/xapi/views.py
+++ pod/xapi/views.py
@@ -3,14 +3,13 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.csrf import csrf_protect
 from django.conf import settings
 from django.core.exceptions import SuspiciousOperation
-# import requests
-# from requests.auth import HTTPBasicAuth
+from .tasks import send_xapi_statement_task
 import json
 import uuid
 
-XAPI_LRS_URL = getattr(settings, "XAPI_LRS_URL", "")
-XAPI_LRS_LOGIN = getattr(settings, "XAPI_LRS_LOGIN", "")
-XAPI_LRS_PWD = getattr(settings, "XAPI_LRS_PWD", "")
+# pip3 install ralph-malph==3.2.1
+# from ralph.models.xapi.fields.actors import AccountActorField
+# from ralph.models.xapi.video.statements import VideoPlayed
 
 XAPI_ANONYMIZE_ACTOR = getattr(settings, "XAPI_ANONYMIZE_ACTOR", True)
 
@@ -21,25 +20,36 @@ def statement(request, app: str = None):
     if request.body:
         body_unicode = request.body.decode('utf-8')
         body = json.loads(body_unicode)
+        """
+        actor = AccountActorField(
+            objectType = "Agent",
+            account={
+                "homePage": request.get_host(),
+                "name": get_actor_name(request)
+            },
+        )
+        """
         statement = {
-            "actor": get_actor(request),
+            "actor": {
+                "objectType": "Agent",
+                "account": {
+                    "homePage": request.get_host(),
+                    "name": get_actor_name(request)
+                }
+            },
             "id": str(uuid.uuid4())
         }
         for key, value in body.items():
             statement[key] = value
-        """
-        x = requests.post(
-            XAPI_LRS_URL,
-            json=statement,
-            auth=HTTPBasicAuth(XAPI_LRS_LOGIN, XAPI_LRS_PWD)
-        )
-        """
+        # send statement via celery
+        send_xapi_statement_task.delay(statement)
+
         json_object = json.dumps(statement, indent=2)
         return JsonResponse(json_object, safe=False)
     raise SuspiciousOperation("Invalid video id")
 
 
-def get_actor(request):
+def get_actor_name(request):
     if request.user.is_authenticated :
         if XAPI_ANONYMIZE_ACTOR:
             name = request.user.owner.hashkey
@@ -49,10 +59,4 @@ def get_actor(request):
         if not request.session or not request.session.session_key:
             request.session.save()
         name = request.session.session_key
-    return {
-        "account" : {
-            "homePage": request.get_host(),
-            "name": name,
-        },
-        "objectType": "Agent",
-    }
+    return name
