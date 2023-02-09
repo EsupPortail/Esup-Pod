918bd00b7dd61bf5e081a80e47e718bbb8a78fb1
ptitloup <nicolas.can@univ-lille.fr>
2023-02-09 11:29:32 +0100

    fix english typo and move xapi video js to a specific file

--- pod/xapi/tasks.py
+++ pod/xapi/tasks.py
@@ -14,7 +14,7 @@ XAPI_LRS_PWD = getattr(settings, "XAPI_LRS_PWD", "")
 
 @shared_task()
 def send_xapi_statement_task(statement):
-    """Sends the xapi statement to the specify LRS."""
+    """Sends the xapi statement to the specified LRS."""
     x = requests.post(
         XAPI_LRS_URL,
         json=statement,
@@ -22,6 +22,6 @@ def send_xapi_statement_task(statement):
     )
     if x.status_code == 200:
         print(x.text)
-        logger.info('statement id : %s' % x.text)
+        logger.info('statement id: %s' % x.text)
     else:
-        logger.error('Error during sending statement : %s' % x.text)
+        logger.error('Error during sending statement: %s' % x.text)
