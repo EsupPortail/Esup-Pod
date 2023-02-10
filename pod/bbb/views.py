3e5e1ff77fba9cee480e152cafa862950997b3ba
Olivier Bado <bado@unice.fr>
2023-02-10 17:40:41 +0100

    Update i18n files + Remove "__future__" imports, as Python 2 has ended since January 1, 2020

--- pod/bbb/views.py
+++ pod/bbb/views.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 from django.conf import settings
--- pod/recorder/views.py
+++ pod/recorder/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """Esup-pod recorder views."""
-from __future__ import unicode_literals
 import os
 import datetime
 import uuid
