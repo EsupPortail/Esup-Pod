599f7f61dbb910ca30fce157ba264fc10d3e35ac
Olivier Bado <bado@unice.fr>
2023-02-16 16:00:52 +0100

    Improve `clean_video_files` script (add `--type`and `--dry`options), now can delete videos files and userfolders.

--- pod/video/management/commands/clean_video_files.py
+++ pod/video/management/commands/clean_video_files.py
@@ -6,18 +6,69 @@ from django.conf import settings
 from os import listdir, remove
 from os.path import isfile, join
 
+USE_PODFILE = getattr(settings, "USE_PODFILE", False)
+if USE_PODFILE:
+    from pod.podfile.models import UserFolder
+
 VIDEOS_DIR = getattr(settings, "VIDEOS_DIR", "videos")
 
 
 class Command(BaseCommand):
     """Delete useless video files."""
 
-    help = "Delete useless video files (not associated with a video Object)"
+    help = "Delete useless files (video or userfolder not associated with a video Object)"
+
+    def add_arguments(self, parser):
+        """Allow arguments to be used with the command."""
+        parser.add_argument(
+            "--type",
+            default="video",
+            help="Type of objects to be deleted.",
+            choices=["userfolder", "video", "all"],
+            type=str)
+        parser.add_argument(
+            "--dry",
+            help="Simulate what would be deleted.",
+            action='store_true', default=False)
 
     def handle(self, *args, **options):
         """Handle the clean_video_files command call."""
+        clean_type = options["type"].lower()
+        self.nb_deleted = {}
+        if clean_type in ["userfolder", "all"]:
+            if USE_PODFILE:
+                self.nb_deleted["userfolder"] = self.clean_userFolders(options["dry"])
+
+        if clean_type in ["video", "all"]:
+            self.nb_deleted["video"] = self.clean_videos(options["dry"])
+
+        self.print_resume(clean_type, options["dry"])
+
+    def clean_userFolders(self, dry_run):
+        """Clean useless User folders."""
+        nb_deleted = 0
+
+        print("Start cleaning useless User folders, please wait...")
+        user_folders = UserFolder.objects.all()
+        for folder in user_folders:
+            if "-" in folder.name:
+                folder_data = folder.name.split("-")
+                vid_id = int(folder_data[0])
+                try:
+                    Video.objects.get(id=vid_id)
+                except Video.DoesNotExist:
+                    print("%s -- %s " % (folder, folder.get_all_files()))
+                    nb_deleted += 1
+                    if not dry_run:
+                        folder.delete()
+        print("Cleaning useless User folders done.")
+        return nb_deleted
+
+    def clean_videos(self, dry_run):
+        """Clean useless video files."""
         list_dir = listdir(join(settings.MEDIA_ROOT, VIDEOS_DIR))
         print("Start cleaning useless video files, please wait...")
+        nb_deleted = 0
         for video_dir in list_dir:
             mypath = join(settings.MEDIA_ROOT, VIDEOS_DIR, video_dir)
             onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
@@ -27,5 +78,28 @@ class Command(BaseCommand):
                 except Video.DoesNotExist:
                     vid_path = join(settings.MEDIA_ROOT, VIDEOS_DIR, video_dir, myfile)
                     print(vid_path)
-                    remove(vid_path)
-        print("Useless video files cleaning done. Have a nice day ;)")
+                    nb_deleted += 1
+                    if not dry_run:
+                        remove(vid_path)
+        print("Cleaning useless video files done.")
+        return nb_deleted
+
+    def print_resume(self, clean_type, dry_run):
+        """Print summary of deleted files."""
+        if clean_type in ["userfolder", "all"]:
+            if USE_PODFILE:
+                if dry_run:
+                    print("[DRY RUN] %i useless user folders would have been deleted." % (
+                        self.nb_deleted["userfolder"]))
+                else:
+                    print("%i useless video folders deleted." % (
+                        self.nb_deleted["userfolder"]))
+            else:
+                print("No need to clean User Folders, as USE_PODFILE = False...")
+        if clean_type in ["video", "all"]:
+            if dry_run:
+                print("[DRY RUN] %i useless video files would have been deleted." % (
+                    self.nb_deleted["video"]))
+            else:
+                print("%i useless video files deleted." % self.nb_deleted["video"])
+        print("Have a nice day ;)")
