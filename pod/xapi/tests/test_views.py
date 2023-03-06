52414888d1e21e2df6655a7b118bf144f51255d4
Ptitloup <nicolas.can@univ-lille.fr>
2023-03-06 16:19:48 +0100

    [DONE] create xapi app to manage and send statements (#748)
    * create xapi app to manage and send statements
    
    * create some class to represent statement, add app to specify type of statement
    
    * create template xapi video and call it in video template - send statement in post ajax in view - create some object to create statement
    
    * clean requirement - send xapi statement to LRS for played action
    
    * add resturn value and statement to json response
    
    * develop all video statement - send it to xapi views
    
    * use celery to send statement - remove ralph malph which is not compatible with python 3.7
    
    * remove unused code in models file
    
    * add example code to use ralph (later)
    
    * add xAPI configuration variable to configuration json file
    
    * add unit test view
    
    * improve test
    
    * fix dict for json response - unit test ok
    
    * use ralph malph lib to validate statement before send it to LRS
    
    * improve configuration for xapi app
    
    * fix english typo and move xapi video js to a specific file
    
    * fix typo on xapi views file
    
    * remove seeking listener event

---
+++ pod/xapi/tests/test_views.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.test import Client  # , override_settings
+from http import HTTPStatus
+import json
+
+
+class xapi_statement_TestView(TestCase):
+    def setUp(self):
+        """initialize the Django test client"""
+        self.client = Client()
+        print(" --->  SetUp of xapi_statement_TestView: OK!")
+
+    def test_xapi_statment_TestView_get_request(self):
+        bad_url = reverse("xapi:statement", kwargs={})
+        response = self.client.get(bad_url)
+        # need post request and video app parameter
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+
+        good_url = reverse("xapi:statement", kwargs={"app": "video"})
+        response = self.client.get(good_url)
+        # video app parameter ok but need post data
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+
+        response = self.client.post(
+            good_url,
+            json.dumps({}),
+            'json',
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)  # 200
+        self.assertEqual(response['Content-Type'], 'application/json')
+        response_unicode = response.content.decode('utf-8')
+        data = json.loads(response_unicode)
+        self.assertEqual(data["actor"]["objectType"], "Agent")
+        print(
+            " --->  test_xapi_statment_TestView_get_request ",
+            "of xapi_statement_TestView: OK!"
+        )
