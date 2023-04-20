6b12175e8300c3bb9c2fdcd892388591b7b4794f
Aymeric Jakobowski <106442526+AymericJak@users.noreply.github.com>
2023-04-20 16:00:19 +0200

    [DONE] Feature : Add 'My videos' button in header (#813)
    * Add 'My videos' button in header
    
    * Add staff verification to display buttons
    
    * Add templatetags to show video & meeting buttons in header
    
    * Implement unit tests for video & meeting buttons
    
    * Hide buttons in user menu

--- pod/main/context_processors.py
+++ pod/main/context_processors.py
@@ -80,6 +80,18 @@ USE_OPENCAST_STUDIO = getattr(django_settings, "USE_OPENCAST_STUDIO", False)
 
 USE_MEETING = getattr(django_settings, "USE_MEETING", False)
 
+RESTRICT_EDIT_VIDEO_ACCESS_TO_STAFF_ONLY = getattr(
+    django_settings,
+    "RESTRICT_EDIT_VIDEO_ACCESS_TO_STAFF_ONLY",
+    False
+)
+
+RESTRICT_EDIT_MEETING_ACCESS_TO_STAFF_ONLY = getattr(
+    django_settings,
+    "RESTRICT_EDIT_MEETING_ACCESS_TO_STAFF_ONLY",
+    False
+)
+
 
 def context_settings(request):
     """Return all context settings."""
@@ -133,6 +145,10 @@ def context_settings(request):
     new_settings["COOKIE_LEARN_MORE"] = COOKIE_LEARN_MORE
     new_settings["SHOW_EVENTS_ON_HOMEPAGE"] = SHOW_EVENTS_ON_HOMEPAGE
     new_settings["USE_MEETING"] = USE_MEETING
+    new_settings["RESTRICT_EDIT_VIDEO_ACCESS_TO_STAFF_ONLY"] = \
+        RESTRICT_EDIT_VIDEO_ACCESS_TO_STAFF_ONLY
+    new_settings["RESTRICT_EDIT_MEETING_ACCESS_TO_STAFF_ONLY"] = \
+        RESTRICT_EDIT_MEETING_ACCESS_TO_STAFF_ONLY
     return new_settings
 
 
--- pod/main/templatetags/navbar.py
+++ pod/main/templatetags/navbar.py
@@ -1,4 +1,27 @@
 from django.template import Library
-
 # from django.utils.safestring import mark_safe
 register = Library()
+
+
+@register.simple_tag(takes_context=True, name="show_video_buttons")
+def show_video_buttons(context):
+    request = context['request']
+    return (
+        not context["RESTRICT_EDIT_VIDEO_ACCESS_TO_STAFF_ONLY"]
+    ) or (
+        context["RESTRICT_EDIT_VIDEO_ACCESS_TO_STAFF_ONLY"]
+        and request.user.is_staff
+    )
+
+
+@register.simple_tag(takes_context=True, name="show_meeting_button")
+def show_meeting_button(context):
+    request = context['request']
+    return (
+        (
+            not context["RESTRICT_EDIT_MEETING_ACCESS_TO_STAFF_ONLY"]
+        ) or (
+            context["RESTRICT_EDIT_MEETING_ACCESS_TO_STAFF_ONLY"]
+            and request.user.is_staff
+        )
+    ) and context["USE_MEETING"]
