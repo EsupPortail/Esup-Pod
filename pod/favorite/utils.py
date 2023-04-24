3f22a16a7048341f14f4ee87b84b757f40e3c96e
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2023-04-24 14:32:13 +0200

    Add some util functions

---
+++ pod/favorite/utils.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import User
+from django.db.models import Max
+
+from .models import Favorite
+from pod.video.models import Video
+
+
+def user_has_favorite_video(user: User, video: Video) -> bool:
+    """
+    Know if user has the video in favorite.
+
+    Args:
+        user (:class:`django.contrib.auth.models.User`): The user entity
+        video (:class:`pod.video.models.Video`): The video entity
+
+    Returns:
+        bool: True if user has the video in favorite, False otherwise
+    """
+    return Favorite.objects.filter(owner=user, video=video).exists()
+
+
+def user_add_or_remove_favorite_video(user: User, video: Video):
+    """
+    Add or remove the video in favorite list of the user.
+
+    Args:
+        user (:class:`django.contrib.auth.models.User`): The user entity
+        video (:class:`pod.video.models.Video`): The video entity
+    """
+    if user_has_favorite_video(user, video):
+        Favorite.objects.filter(owner=user, video=video).delete()
+    else:
+        Favorite.objects.create(owner=user, video=video, rank=get_next_rank(user))
+
+
+def get_next_rank(user: User) -> int:
+    """
+    Get the next favorite rank for the user.
+
+    Args:
+        user (:class:`django.contrib.auth.models.User`): The user entity
+
+    Returns:
+        int: The next rank
+    """
+    last_rank = Favorite.objects.filter(owner=user).aggregate(Max('rank'))['rank__max']
+    return last_rank + 1 if last_rank is not None else 1
