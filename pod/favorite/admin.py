52a2b76fa9f2a2d72dbb638b6ae81907c9703b05
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2023-04-24 13:45:49 +0200

    Add favorite model in admin panel

--- pod/favorite/admin.py
+++ pod/favorite/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Favorite
 
-# Register your models here.
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    date_hierarchy = "date_added"
+    list_display = (
+        "id",
+        "video",
+        "owner",
+        "date_added",
+        "rank",
+    )
+    list_filter = ("id", "owner", "date_added")
+    search_fields = ("video__title", "owner__username")
