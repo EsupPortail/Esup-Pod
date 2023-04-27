28e8cbe11ce424eab93704ea740c713acb94035f
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2023-04-27 11:01:22 +0200

    Remove the refresh when we remove favorite in the card.

--- pod/video/static/js/video_favorites_card.js
+++ pod/video/static/js/video_favorites_card.js
@@ -1,23 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById("favorite-button-form");
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-
-        const formData = new FormData(form)
-
-        fetch(form.action, {
-            method: form.method,
-            body: formData
-        })
-            .then(response => response.text()) // On récupère le contenu HTML de la réponse
-            .then(data => {
-                const parser = new DOMParser();
-                const html = parser.parseFromString(data, 'text/html');
-                const button = document.querySelector("#star_btn > i")
-                button.remove();
+    const forms = document.getElementsByClassName("favorite-button-form-card");
+    console.log(forms)
+    for (let form of forms) {
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+    
+            const formData = new FormData(form)
+    
+            fetch(form.action, {
+                method: form.method,
+                body: formData
             })
-            .catch(error => {
-                alert("La suppression n'a pas pu se faire...")
-            });
-    });
+                .then(response => response.text()) // On récupère le contenu HTML de la réponse
+                .then(data => {
+                    const parser = new DOMParser();
+                    const html = parser.parseFromString(data, 'text/html');
+                    form.remove();
+                })
+                .catch(error => {
+                    alert("La suppression n'a pas pu se faire...")
+                });
+        });
+    }
 });
