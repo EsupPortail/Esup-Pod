fa5b5a3cf47d5b4bbd7471fb192f1f442a34c678
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2023-04-28 14:56:21 +0200

    Add drap and drop to save the reorganization

--- pod/favorite/static/js/favorite-reorganize.js
+++ pod/favorite/static/js/favorite-reorganize.js
@@ -1,3 +1,5 @@
+const exchangedValues = [];
+
 document.getElementById('reorganize-button').addEventListener('click', function (event) {
     if (this.id == 'reorganize-button') {
         event.preventDefault();
@@ -8,12 +10,57 @@ document.getElementById('reorganize-button').addEventListener('click', function 
         const spanElement = this.querySelector('span');
         iconElement.classList.replace('bi-arrows-move', 'bi-save');
         spanElement.textContent = gettext('Save');
+    } else if (this.id == 'save-button') {
+        alert(exchangedValues);
+        console.log(convert2DTableToJson(exchangedValues));
+        document.getElementById('json-data').value = convert2DTableToJson(exchangedValues);
     }
 });
+
+function onDragStart(event) {
+    event.dataTransfer.clearData();
+    event.dataTransfer.setData('text/plain', event.target.id); // TODO Mettre "text" à la place de "text/plain" pour IE 9 à 11
+    event.currentTarget.style.backgroundColor = "yellow";
+  }
+
+function onDragOver(event) {
+    event.preventDefault();
+}
+
+function onDrop(event) {
+    event.preventDefault();
+    const id = event.dataTransfer.getData('text');
+    const draggableElement = document.getElementById(id);
+    const dropzone = event.target;
+    const child1 = draggableElement.children[0];
+    const child2 = dropzone.children[0];
+    if (child1.id == child2.id) return;
+    const child1copy = child1.cloneNode(true);
+    const child2copy = child2.cloneNode(true);
+    draggableElement.appendChild(child2copy);
+    dropzone.appendChild(child1copy);
+    child1.remove();
+    child2.remove();
+    exchangedValues.push([child1.id, child2.id]);
+}
 
 /**
  * Activate the drap and drop in the page
  */
 function activateDragAndDrop(parent) {
-    const draggableElements = document.querySelectorAll('.draggable');
+    const draggableElements = document.querySelectorAll('.draggable-container');
+    draggableElements.forEach(draggableElement => {
+        draggableElement.setAttribute('draggable', true);
+        draggableElement.addEventListener('dragstart', onDragStart);
+        draggableElement.addEventListener('dragover', onDragOver);
+        draggableElement.addEventListener('drop', onDrop);
+    });
+}
+
+function convert2DTableToJson(table) {
+    const jsonObject = {};
+    for (let i = 0; i < table.length; i++) {
+        jsonObject[i] = table[i];
+    }
+    return JSON.stringify(jsonObject);
 }
