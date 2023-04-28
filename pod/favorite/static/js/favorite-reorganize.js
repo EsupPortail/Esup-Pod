65d2ff55e77ed6fba9c6ddceed1f7f5099eb6931
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2023-04-28 13:21:31 +0200

    Change a tag to button tag

--- pod/favorite/static/js/favorite-reorganize.js
+++ pod/favorite/static/js/favorite-reorganize.js
@@ -1,5 +1,19 @@
 document.getElementById('reorganize-button').addEventListener('click', function (event) {
-    event.preventDefault();
-    alert("It works !")
-    this.id = "save-button";
+    if (this.id == 'reorganize-button') {
+        event.preventDefault();
+        activateDragAndDrop();
+        this.id = 'save-button';
+        this.title = gettext('Save your reorganization');
+        const iconElement = this.querySelector('i');
+        const spanElement = this.querySelector('span');
+        iconElement.classList.replace('bi-arrows-move', 'bi-save');
+        spanElement.textContent = gettext('Save');
+    }
 });
+
+/**
+ * Activate the drap and drop in the page
+ */
+function activateDragAndDrop() {
+    // TODO A completer
+}
