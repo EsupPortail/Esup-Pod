ed90652e11bc413e14c09a077e3a4cdf97effe02
Sebastien Montel <montel6@univ-lorraine.fr>
2023-05-09 11:15:27 +0200

    admin - videos as draft

--- pod/video/admin.py
+++ pod/video/admin.py
@@ -229,7 +229,7 @@ class VideoAdmin(admin.ModelAdmin):
         form = super(VideoAdmin, self).get_form(request, obj, **kwargs)
         return form
 
-    actions = ["encode_video", "transcript_video","draft_video"]
+    actions = ["encode_video", "transcript_video", "draft_video"]
 
     def draft_video(self, request, queryset):
         for item in queryset:
