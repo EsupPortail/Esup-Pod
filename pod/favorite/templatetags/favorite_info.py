5d35ba4bb5870893da6938d3bb373a8bdea12b8f
Aymeric Jakobowski <aymericjakpro@gmail.com>
2023-05-11 12:42:18 +0200

    Check if user is authenticated

--- pod/favorite/templatetags/favorite_info.py
+++ pod/favorite/templatetags/favorite_info.py
@@ -10,6 +10,8 @@ register = Library()
 @register.simple_tag(takes_context=True, name="is_favorite")
 def is_favorite(context, video: Video):
     request = context["request"]
+    if not request.user.is_authenticated:
+        return False
     return user_has_favorite_video(request.user, video)
 
 
