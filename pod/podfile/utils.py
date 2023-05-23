a36ebbc1de46bdc4cf7080c41edbe57c5403abb4
thinhantran <trannhanutc@gmail.com>
2023-05-23 16:36:42 +0200

    add fct in podfile/utils

--- pod/podfile/utils.py
+++ pod/podfile/utils.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 
 def update_shared_user(request, action):
+    """Update shared user for a folder based on the specified action."""
     foldid = request.GET.get("foldid", 0)
     userid = request.GET.get("userid", 0)
     if foldid == 0 or userid == 0:
