b565af45ce0e0349160ece8a2ba4863445b3ec69
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2023-05-30 12:11:37 +0200

    Add admin.py

--- pod/playlist/admin.py
+++ pod/playlist/admin.py
@@ -1,3 +1,20 @@
+"""Esup-Pod playlist admin"""
 from django.contrib import admin
 
-# Register your models here.
+from .models import Playlist, PlaylistContent
+
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    """Playlist admin page."""
+
+    date_hierarchy = "date_updated"
+    list_display = (
+        "id",
+        "name",
+    )
+
+@admin.register(PlaylistContent)
+class PlaylistContentAdmin(admin.ModelAdmin):
+    """PlaylistContent admin page."""
+
+    ...
