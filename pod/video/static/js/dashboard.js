43881671d4b452074d3f76c9a1ca4186e4e375b3
pampletousse <sylvain.loppin@grenet.fr>
2023-05-31 10:12:10 +0200

    Change form to dynamic modelForm append async, clean code, rename create_with_fields form function

--- pod/video/static/js/dashboard.js
+++ pod/video/static/js/dashboard.js
@@ -1,4 +1,12 @@
+var dashboardFormContainer = document.getElementById("dashboardFormContainer");
+var bulkUpdateActionSelect = document.getElementById("bulkUpdateActionSelect");
 var confirmBulkUpdateBtn = document.getElementById("confirmBulkUpdateBtn");
+
+// Add event listener on change Action Select
+bulkUpdateActionSelect.addEventListener("change", function() {
+    let action = bulkUpdateActionSelect.value;
+    appendDynamicForm(action);
+});
 
 function bulk_update() {
   // Async POST request to bulk update
@@ -21,6 +29,31 @@ function bulk_update() {
     })
     .catch((error) => {
       console.log(error);
+    });
+}
+
+function appendDynamicForm(action){
+    // Async request to get specific field to batch update
+    fetch(urlDashboardForm, {
+    method: 'POST',
+    headers: {
+      "X-CSRFToken": Cookies.get("csrftoken"),
+      "X-Requested-With": "XMLHttpRequest",
+    },
+    dataType: "html",
+    cache: "no-store",
+    body: JSON.stringify({action:action}),
+    })
+    .then((response) => response.text())
+    .then((data) => {
+        let parser = new DOMParser();
+        let html = parser.parseFromString(data, "text/html");
+        dashboardFormContainer.innerHTML = data;
+    })
+    .catch((error) => {
+        document.getElementById("videos_list").innerHTML = gettext(
+            "An Error occurred while processing."
+      );
     });
 }
 
