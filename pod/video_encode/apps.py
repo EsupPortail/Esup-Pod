bce2a9fe06f7bdf2bd2d937c8a2e04a420cee8ab
thinhantran <trannhanutc@gmail.com>
2023-05-31 09:43:10 +0200

    create app video_encode

---
+++ pod/video_encode/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VideoEncodeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pod.video_encode'
---
+++ pod/video_encode/models.py
@@ -0,0 +1,4 @@
+"""Models for Esup-Pod video_encode."""
+from django.db import models
+
+# Create your models here.
