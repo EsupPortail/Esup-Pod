a00536d8eafde1278f7225e956265ca860c52489
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2023-05-31 14:56:37 +0200

    Add personal playlists page

--- pod/playlist/views.py
+++ pod/playlist/views.py
@@ -1,17 +1,19 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from django.shortcuts import render
 
-from .models import Playlist
+from .utils import get_playlist_list_for_user
 
 @login_required(redirect_field_name="referrer")
 def playlist_list(request):
     """Render my playlists page."""
+    playlists = get_playlist_list_for_user(request.user)
     return render(
         request,
         "playlist/playlists.html",
         {
             "page_title": _("Playlists"),
-            "playlists": ['lol', 'lol2'],
+            "playlists": playlists,
         }
     )
