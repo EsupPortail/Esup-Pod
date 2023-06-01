3bfc11bbe67dd290257a2bdf9cbacae285df0baa
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2023-06-01 10:27:11 +0200

    Add template tag to check if the user can edit or remove a playlist or a video in a playlist

---
+++ pod/playlist/templatetags/playlist_buttons.py
@@ -0,0 +1,12 @@
+from django.template import Library
+
+from ..models import Playlist
+
+register = Library()
+
+@register.simple_tag(takes_context=True, name="user_can_edit_or_remove")
+def user_can_edit_or_remove(context: dict, playlist: Playlist) -> bool:
+    request = context["request"]
+    if not request.user.is_authenticated:
+        return False
+    return request.user == playlist.owner or request.user.is_staff
