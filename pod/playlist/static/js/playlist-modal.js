8c496fa5c75a83824438756cb4a437065307dc1e
Aymeric Jakobowski <aymericjakpro@gmail.com>
2023-06-02 11:27:56 +0200

    Add playlist link in user menu

--- pod/playlist/static/js/playlist-modal.js
+++ pod/playlist/static/js/playlist-modal.js
@@ -11,28 +11,30 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function preventRefreshButton(button) {
-  button.addEventListener("click", function (e) {
-    e.preventDefault();
-    const url = this.getAttribute("href");
-    fetch(url, {
-      method: "GET"
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.text();
-        } else {
-          throw new Error("Network response was not ok.");
-        }
+  if (button) {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      const url = this.getAttribute("href");
+      fetch(url, {
+        method: "GET"
       })
-      .then((data) => {
-        const parser = new DOMParser();
-        const html = parser.parseFromString(data, "text/html");
-        const updatedButton = html.getElementById(button.id);
-        preventRefreshButton(updatedButton)
-        button.replaceWith(updatedButton);
-      })
-      .catch(error => {
-        console.error("Error : ", error)
-      });
-  });
+        .then((response) => {
+          if (response.ok) {
+            return response.text();
+          } else {
+            throw new Error("Network response was not ok.");
+          }
+        })
+        .then((data) => {
+          const parser = new DOMParser();
+          const html = parser.parseFromString(data, "text/html");
+          const updatedButton = html.getElementById(button.id);
+          preventRefreshButton(updatedButton)
+          button.replaceWith(updatedButton);
+        })
+        .catch(error => {
+          console.error("Error : ", error)
+        });
+    });
+  }
 }
