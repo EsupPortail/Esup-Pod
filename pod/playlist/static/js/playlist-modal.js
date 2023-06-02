badefe743af43d8d99b134d8e05b880a0326d215
Aymeric Jakobowski <aymericjakpro@gmail.com>
2023-06-02 11:05:24 +0200

    Fix playlist-modal JS

--- pod/playlist/static/js/playlist-modal.js
+++ pod/playlist/static/js/playlist-modal.js
@@ -1,33 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const playlists = document.getElementById("playlist-list").children;
-    const buttons = [];
-    for (let playlist of playlists) {
-      buttons.push(playlist.querySelector(".action-btn"));
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const playlists = document.getElementById("playlist-list").children;
+  const buttons = [];
+  for (let playlist of playlists) {
+    buttons.push(playlist.querySelector(".action-btn"));
+  }
 
-    for (let button of buttons) {
-      button.addEventListener("click", function(e) {
-        e.preventDefault();
-        const url = this.getAttribute("href");
-        fetch(url, {
-          method: "GET"
-        })
-        .then((response) => {
-          if (response.ok) {
-            return response.text();
-          } else {
-            throw new Error("Network response was not ok.");
-          }
-        })
-        .then((data) => {
-          const parser = new DOMParser();
-          const html = parser.parseFromString(data, "text/html");
-          const updatedButton = html.getElementById(button.id);
-          button.replaceWith(updatedButton);
-        })
-        .catch(error => {
-          console.error("Error : ", error)
-        })
-    });
+  for (let button of buttons) {
+    preventRefreshButton(button);
   }
 });
+
+function preventRefreshButton(button) {
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
+    const url = this.getAttribute("href");
+    fetch(url, {
+      method: "GET"
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error("Network response was not ok.");
+        }
+      })
+      .then((data) => {
+        const parser = new DOMParser();
+        const html = parser.parseFromString(data, "text/html");
+        const updatedButton = html.getElementById(button.id);
+        preventRefreshButton(updatedButton)
+        button.replaceWith(updatedButton);
+      })
+      .catch(error => {
+        console.error("Error : ", error)
+      });
+  });
+}
