71ff5ffbddb187a6c7d7bf738ad4fc3eece36b79
thinhantran <trannhanutc@gmail.com>
2023-06-15 15:34:27 +0200

    create line graphs for videos

--- pod/video/static/js/video_stats_view2.js
+++ pod/video/static/js/video_stats_view2.js
@@ -1,54 +1,75 @@
-$(() => {
+$(function () {
   let data_url = window.location.href;
-  
-  // Tạo biểu đồ
-  let ctx = document.getElementById("myChart").getContext("2d");
-  let myChart = new Chart(ctx, {
-    type: "line",
+
+  // Khởi tạo mảng dữ liệu cho biểu đồ
+  let chartData = [];
+
+  // Lấy dữ liệu từ URL
+  $.ajax({
+    url: data_url,
+    method: "POST",
+    dataType: "json",
     data: {
-      labels: [
-        gettext("During the day"),
-        gettext("During the month"),
-        gettext("During the year"),
-        gettext("Total from creation"),
-      ],
-      datasets: [
-        {
-          label: gettext("View"),
-          backgroundColor: "rgba(255, 99, 132, 0.2)",
-          borderColor: "rgba(255, 99, 132, 1)",
-          borderWidth: 1,
-          data: [],
-        },
-        {
-          label: gettext("Favorite"),
-          backgroundColor: "rgba(54, 162, 235, 0.2)",
-          borderColor: "rgba(54, 162, 235, 1)",
-          borderWidth: 1,
-          data: [],
-        },
-      ],
+      csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
     },
-    options: {
-      responsive: true,
-      scales: {
-        x: { display: true },
-        y: { beginAtZero: true },
-      },
+    success: function (data) {
+      // Tạo dữ liệu cho biểu đồ từ dữ liệu trả về
+      for (let i = 0; i < data.length; i++) {
+        let rowData = data[i];
+        chartData.push(rowData);
+      }
+
+      // Khởi tạo biểu đồ bằng Chart.js
+      var ctx = document.getElementById("myChart").getContext("2d");
+      var day = chartData.map((row) => row.day);
+      var month = chartData.map((row) => row.month);
+      var year = chartData.map((row) => row.year);
+      var since_created = chartData.map((row) => row.since_created);
+      var fav_day = chartData.map((row) => row.fav_day);
+      var fav_month = chartData.map((row) => row.fav_month);
+      var fav_year = chartData.map((row) => row.fav_year);
+      var fav_since_created = chartData.map((row) => row.fav_since_created);
+      var myChart = new Chart(ctx, {
+        type: "line",
+        data: {
+          // labels: chartData.map((row) => row.title),
+          labels: [
+            gettext("During the day"),
+            gettext("During the month"),
+            gettext("During the year"),
+            gettext("Total from creation"),
+          ],
+          datasets: [
+            {
+              label: gettext("View"),
+              backgroundColor: "#DC143C",
+              borderColor: "#DC143C",
+              borderWidth: 1,
+              data: [day[0], month[0], year[0], since_created[0]],
+              tension: 0.5,
+            },
+            {
+              label: gettext("Favorite"),
+              backgroundColor: "#1F7C85",
+              borderColor: "#1F7C85",
+              borderWidth: 1,
+              data: [fav_day[0], fav_month[0], fav_year[0], fav_since_created[0]],
+              tension: 0.5,
+            },
+            // Thêm các dataset khác tương tự
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    },
+    error: function (error) {
+      console.log(error);
     },
   });
-  
-  // Lấy dữ liệu từ URL và cập nhật biểu đồ
-  fetch(data_url)
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((item) => {
-      alert("day " + item.day);
-
-        myChart.data.datasets[0].data.push(item.day);
-        myChart.data.datasets[1].data.push(item.fav_since_created);
-      });
-      myChart.update();
-    })
-    .catch((error) => console.error(error));
 });
