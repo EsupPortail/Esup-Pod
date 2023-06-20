aa94f017133aedbd476fa31c68b704900cb1ef81
Aymeric Jakobowski <aymericjakpro@gmail.com>
2023-06-20 14:31:39 +0200

    Add counters under videos

--- pod/playlist/templatetags/playlist_stats.py
+++ pod/playlist/templatetags/playlist_stats.py
@@ -1,6 +1,8 @@
 from django.template import Library
 
-from pod.playlist.utils import get_number_video_added_in_specific_playlist, get_total_favorites_video
+from pod.playlist.utils import get_number_video_added_in_specific_playlist
+from pod.playlist.utils import get_total_favorites_video as total_favorites_utils
+from pod.playlist.utils import get_count_video_added_in_playlist as total_additions_playlist_utils
 from pod.video.models import Video
 
 from ..models import Playlist
@@ -15,12 +17,14 @@ def get_number_favorites(context: dict) -> int:
     favorites_playlist = Playlist.objects.get(name="Favorites", owner=user)
     return get_number_video_added_in_specific_playlist(favorites_playlist)
 
+
 @register.simple_tag(name="get_total_favorites_video")
 def get_total_favorites_video(video: Video) -> int:
     """Get the number of times a video has been added in favorites."""
-    return get_total_favorites_video(video)
+    return total_favorites_utils(video)
+
 
 @register.simple_tag(name="get_count_video_added_in_playlist")
 def get_count_video_added_in_playlist(video: Video) -> int:
     """Get the number of times a video has been added in a playlist."""
-    return get_total_favorites_video(video)
+    return total_additions_playlist_utils(video)
