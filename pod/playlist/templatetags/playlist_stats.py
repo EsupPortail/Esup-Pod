cb3aeca8b4f522cd88905c5b4735c53dee62776d
Aymeric Jakobowski <aymericjakpro@gmail.com>
2023-06-20 09:47:22 +0200

    Infinite loader & templatetag

--- pod/playlist/templatetags/playlist_stats.py
+++ pod/playlist/templatetags/playlist_stats.py
@@ -1,6 +1,7 @@
 from django.template import Library
 
-from pod.playlist.utils import get_number_video_added_in_specific_playlist
+from pod.playlist.utils import get_number_video_added_in_specific_playlist, get_total_favorites_video
+from pod.video.models import Video
 
 from ..models import Playlist
 
@@ -13,3 +14,13 @@ def get_number_favorites(context: dict) -> int:
     user = context["request"].user
     favorites_playlist = Playlist.objects.get(name="Favorites", owner=user)
     return get_number_video_added_in_specific_playlist(favorites_playlist)
+
+@register.simple_tag(name="get_total_favorites_video")
+def get_total_favorites_video(video: Video) -> int:
+    """Get the number of times a video has been added in favorites."""
+    return get_total_favorites_video(video)
+
+@register.simple_tag(name="get_count_video_added_in_playlist")
+def get_count_video_added_in_playlist(video: Video) -> int:
+    """Get the number of times a video has been added in a playlist."""
+    return get_total_favorites_video(video)
