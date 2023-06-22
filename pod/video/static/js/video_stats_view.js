ddb82ad729dda408c196dae7d1cdf265f49fb03f
thinhantran <trannhanutc@gmail.com>
2023-06-22 16:47:43 +0200

    update the translation

--- pod/video/static/js/video_stats_view.js
+++ pod/video/static/js/video_stats_view.js
@@ -1,150 +1,154 @@
 $(function () {
-  var yearChart;
-  var startYear;
-  var endYear;
-  let y = [];
-  let yearlyviews = [];
+  var dateChart;
+  var startDate;
+  var endDate;
+  let d = [];
+  let dailyviews = [];
+  let dailyfavo = [];
   let today = new Date();
   let data_url = window.location.href;
 
-  let startYearDefault = today.getFullYear() - 5;
-  $("#start-year").val(startYearDefault); // set year input value to current year - 5
-  document.querySelector("#start-year").max = today.getFullYear();
-  $("#end-year").val(today.getFullYear()); // set year input value to current year
-  document.querySelector("#end-year").max = today.getFullYear();
+  let startDateDefault = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000).toISOString().split("T")[0];
+  $("#start-date").val(startDateDefault); // set date input value to today - 7 days
+  document.querySelector("#start-date").max = today.toISOString().split("T")[0];
+  $("#end-date").val(today.toISOString().split("T")[0]); // set date input value to today
+  document.querySelector("#end-date").max = today.toISOString().split("T")[0];
 
-  if (startYear === undefined && endYear === undefined) {
-      startYear = startYearDefault;
-      endYear = today.getFullYear();
-      updateChart();
+
+  if (startDate === undefined && endDate === undefined) {
+    startDate = startDateDefault;
+    endDate = today.toISOString().split("T")[0];
+    updateChart();
   }
 
-  $("#start-year").change(function () {
-      startYear = parseInt($(this).val());
-      document.querySelector("#end-year").min = startYear;
-      updateChart();
+  $("#start-date").change(function () {
+    startDate = $(this).val();
+    document.querySelector("#end-date").min = new Date(startDate).toISOString().split("T")[0];
+    updateChart();
   });
 
-  $("#end-year").change(function () {
-      endYear = parseInt($(this).val());
-      document.querySelector("#start-year").max = endYear;
-      updateChart();
+  $("#end-date").change(function () {
+    endDate = $(this).val();
+    document.querySelector("#start-date").max = new Date(endDate).toISOString().split("T")[0];
+    updateChart();
   });
 
-  function fetchData(year) {
-      return new Promise(function (resolve, reject) {
-          $.ajax({
-              url: data_url,
-              method: "POST",
-              dataType: "json",
-              data: {
-                  csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
-                  periode: year,
-              },
-              success: function (data) {
-                  var year = data.map((row) => row.all_year);
-                  var all_view = data.map((row) => row.all_view_year);
-                  var year_favo = data.map((row) => row.all_fav_year);
-                  y.push(year[0]);
-                  yearlyviews.push(all_view[0]);
-                  yearlyfav.push(year_favo[0]);
-                  resolve();
-              },
-              error: function (error) {
-                  reject(error);
-              },
-          });
+  function fetchData(data) {
+    return new Promise(function (resolve, reject) {
+      $.ajax({
+        url: data_url,
+        method: "POST",
+        dataType: "json",
+        data: {
+          csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
+          periode: data,
+        },
+        success: function (data) {
+          var date = data.map((row) => row.date);
+          var day = data.map((row) => row.day);
+          var fav_day = data.map((row) => row.fav_day);
+          d.push(date[0]);
+          dailyviews.push(day[0]);
+          dailyfavo.push(fav_day[0]);
+          resolve();
+        },
+        error: function (error) {
+          reject(error);
+        },
       });
+    });
   }
 
   function updateChart() {
-      y = [];
-      yearlyviews = [];
-      yearlyfav = [];
-      let startYearInt = parseInt(startYear);
-      let endYearInt = parseInt(endYear);
-      let requests = [];
-      while (startYearInt <= endYearInt ) {
-          requests.push(fetchData(startYearInt));
-          startYearInt++;
-      }
+    d = [];
+    dailyviews = [];
+    dailyfavo = [];
+    let currentDate = new Date(startDate);
+    let targetDate = new Date(endDate);
 
-      Promise.all(requests)
-          .then(function () {
-              let sortedData = y
-                  .map((item, index) => {
-                      return {
-                          year: item,
-                          views: yearlyviews[index],
-                          favorite : yearlyfav[index],
-                      };
-                  })
-                  .sort((a, b) => a.year - b.year);
-              year = sortedData.map((item) => item.year);
-              yearlyviews = sortedData.map((item) => item.views);
-              yearlyfavo = sortedData.map((item) => item.favorite);
+    let requests = [];
+    while (currentDate <= targetDate) {
+      let data = currentDate.toISOString().split("T")[0];
+      requests.push(fetchData(data));
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
 
-              var ctx = document.getElementById("yearChart").getContext("2d");
-              if (yearChart) {
-                  yearChart.destroy();
-              }
-              yearChart = new Chart(ctx, {
-                  type: "line",
-                  data: {
-                      labels: year,
-                      datasets: [
-                          {
-                            label: gettext("View"),
-                            animations: {
-                                y: {
-                                    duration: 2000,
-                                    delay: 500,
-                                },
-                            },
-                            backgroundColor: "#DC143C",
-                            borderColor: "#DC143C",
-                            borderWidth: 2,
-                            data: yearlyviews,
-                            tension: 0.5,
-                          },
-                          {
-                            label: gettext("Favorite"),
-                            backgroundColor: "#1F7C85",
-                            borderColor: "#1F7C85",
-                            borderWidth: 2,
-                            data: yearlyfavo,
-                            tension: 0.5,
-                          },
-                      ],
-                  },
-                  options: {
-                      animations: {
-                          y: {
-                              easing: "easeInOutElastic",
-                              from: (ctx) => {
-                                  if (ctx.type === "data") {
-                                      if (ctx.mode === "default" && !ctx.dropped) {
-                                          ctx.dropped = true;
-                                          return 0;
-                                      }
-                                  }
-                              },
-                          },
-                      },
-                  },
-              });
+    Promise.all(requests)
+      .then(function () {
+        let sortedData = d
+          .map((item, index) => {
+            return {
+              date: item,
+              views: dailyviews[index],
+              favo: dailyfavo[index],
+            };
           })
-          .catch(function (error) {
-              console.log(error);
-          });
+          .sort((a, b) => new Date(a.date) - new Date(b.date));
+        daily = sortedData.map((item) => item.date);
+        dailyviews = sortedData.map((item) => item.views);
+        dailyfavo = sortedData.map((item) => item.favo);
+
+        var ctx = document.getElementById("dateChart").getContext("2d");
+        if (dateChart) {
+          dateChart.destroy();
+        }
+        dateChart = new Chart(ctx, {
+          type: "line",
+          data: {
+            labels: daily,
+            datasets: [
+              {
+                label: gettext("Views"),
+                animations: {
+                  y: {
+                    duration: 2000,
+                    delay: 500,
+                  },
+                },
+                backgroundColor: "#DC143C",
+                borderColor: "#DC143C",
+                borderWidth: 2,
+                data: dailyviews,
+                tension: 0.5,
+              },
+              {
+                label: gettext("Favorites"),
+                backgroundColor: "#1F7C85",
+                borderColor: "#1F7C85",
+                borderWidth: 2,
+                data: dailyfavo,
+                tension: 0.5,
+              },
+            ],
+          },
+          options: {
+            animations: {
+              y: {
+                easing: "easeInOutElastic",
+                from: (ctx) => {
+                  if (ctx.type === "data") {
+                    if (ctx.mode === "default" && !ctx.dropped) {
+                      ctx.dropped = true;
+                      return 0;
+                    }
+                  }
+                },
+              },
+            },
+          },
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   // Function to export data in CSV format
   function exportToCSV() {
     let csvContent = "data:text/csv;charset=utf-8,";
     csvContent += "Date,Views,Favorites\n";
-    for (let i = 0; i < year.length; i++) {
-      let row = year[i] + "," + yearlyviews[i] + "," + yearlyfav[i] + "\n";
+    for (let i = 0; i < daily.length; i++) {
+      let row = daily[i] + "," + dailyviews[i] + "," + dailyfavo[i] + "\n";
       csvContent += row;
     }
     var encodedUri = encodeURI(csvContent);
