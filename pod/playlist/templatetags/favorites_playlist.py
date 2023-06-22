be47a2de4e02071772ef4aa35ffaaef03aa1fcee
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2023-06-22 11:28:41 +0200

    Update playlist player

--- pod/playlist/templatetags/favorites_playlist.py
+++ pod/playlist/templatetags/favorites_playlist.py
@@ -2,7 +2,12 @@ from django.template import Library
 from pod.playlist.models import Playlist
 from django.utils.translation import ugettext_lazy as _
 
-from pod.playlist.utils import check_video_in_playlist, get_favorite_playlist_for_user
+from pod.playlist.utils import (
+    check_video_in_playlist,
+    get_favorite_playlist_for_user,
+    get_playlist_list_for_user,
+    get_playlists_for_additional_owner,
+)
 from pod.video.models import Video
 
 
@@ -24,7 +29,7 @@ def is_favorite(user, video: Video) -> bool:
         video (:class:`pod.video.models.Video`): The video entity to check
 
     Returns:
-        bool: True if the user has the video as favorite, False otherwise
+        bool: `True` if the user has the video as favorite, `False` otherwise
     """
     return check_video_in_playlist(get_favorite_playlist_for_user(user), video)
 
@@ -44,3 +49,24 @@ def get_playlist_name(playlist: Playlist) -> str:
         return _("Favorites")
     else:
         return playlist.name
+
+@register.simple_tag(takes_context=True, name="can_see_favorite_video")
+def can_see_favorite_video(context: dict, video: Video) -> bool:
+    """
+    Template tag to check if the user can see a favorite video.
+
+    Args:
+        context (dict): The template context dictionary
+        video (:class:`pod.video.models.Video`): The video entity to check
+
+    Returns:
+        bool: `True` if the user can, `False` otherwise
+    """
+    request = context["request"]
+    if not request.user.is_authenticated:
+        return False
+    is_password_protected = video.password is not None and video.password != ""
+    if is_password_protected:
+        return video in (get_playlist_list_for_user(request.user) | get_playlists_for_additional_owner(request.user))
+    else:
+        return not video.is_draft;
