e4a5ba7ba2d2c32b1981eb8f592c620898d91b94
Aymeric Jakobowski <aymericjakpro@gmail.com>
2023-06-22 11:47:08 +0200

    Use bulk create / update

--- pod/playlist/apps.py
+++ pod/playlist/apps.py
@@ -19,20 +19,22 @@ class PlaylistConfig(AppConfig):
         post_migrate.connect(self.send_previous_data, sender=self)
 
     def save_previous_data(self, sender, **kwargs):
-        """Save previous data from favorites table."""
+        """Save previous data from various database tables."""
         self.save_playlists()
         if len(PLAYLIST_INFORMATIONS) > 0:
             print("PLAYLIST_INFORMATIONS saved %s playlists" % len(PLAYLIST_INFORMATIONS))
 
         self.save_playlist_content()
         if len(PLAYLIST_CONTENTS) > 0:
-            print("PLAYLIST_CONTENTS saved %s videos in playlists" % len(PLAYLIST_CONTENTS))
+            print("PLAYLIST_CONTENTS saved %s videos in playlists" %
+                  len(PLAYLIST_CONTENTS))
 
         self.save_favorites()
         if len(FAVORITES_DATA) > 0:
             print("FAVORITES_DATA saved for %s persons" % len(FAVORITES_DATA))
 
     def save_favorites(self):
+        """Save previous data from favorites table."""
         try:
             with connection.cursor() as c:
                 c.execute(
@@ -50,42 +52,47 @@ class PlaylistConfig(AppConfig):
         except Exception as e:
             print(e)
 
-    def save_playlists(self):
+    def execute_query(self, query, mapping_dict):
+        """
+        Execute the given query and populate the mapping dictionary with the results.
+
+        Args:
+            query (str): The given query to execute
+            mapping_dict (dict): The dictionary.
+        """
         try:
             with connection.cursor() as c:
-                c.execute(
-                    """
-                    SELECT id, title, description, visible, owner_id
-                    FROM playlist_playlist
-                    ORDER BY id
-                    """
-                )
+                c.execute(query)
                 results = c.fetchall()
                 for res in results:
                     id = res[0]
-                    PLAYLIST_INFORMATIONS[id] = [res[i] for i in range(1, len(res))]
+                    mapping_dict[id] = [res[i] for i in range(1, len(res))]
         except Exception as e:
             print(e)
+            pass
+
+    def save_playlists(self):
+        """Save previous data from playlists table."""
+        self.execute_query(
+            """
+            SELECT id, title, description, visible, owner_id
+            FROM playlist_playlist
+            ORDER BY id
+            """, PLAYLIST_INFORMATIONS
+        )
 
     def save_playlist_content(self):
-        try:
-            with connection.cursor() as c:
-                c.execute(
-                    """
-                    SELECT id, position, playlist_id, video_id
-                    FROM playlist_playlistelement
-                    ORDER BY id
-                    """
-                )
-                results = c.fetchall()
-                for res in results:
-                    id = res[0]
-                    PLAYLIST_CONTENTS[id] = [res[i] for i in range(1, len(res))]
-        except Exception as e:
-            print(e)
+        """Save previous data from playlistelement table."""
+        self.execute_query(
+            """
+            SELECT id, position, playlist_id, video_id
+            FROM playlist_playlistelement
+            ORDER BY id
+            """, PLAYLIST_CONTENTS
+        )
 
     def send_previous_data(self, sender, **kwargs):
-        """Send previous data from favorites table."""
+        """Send previous data from various database tables."""
         print("Sending datas")
 
         if len(PLAYLIST_INFORMATIONS) > 0:
@@ -97,6 +104,7 @@ class PlaylistConfig(AppConfig):
         self.create_new_favorites()
 
     def create_new_favorites(self):
+        """Create favorites records from existing datas or create favorites playlist."""
         from pod.playlist.models import Playlist, PlaylistContent
         from django.utils.translation import gettext_lazy as _
         from django.contrib.auth.models import User
@@ -137,9 +145,12 @@ class PlaylistConfig(AppConfig):
         print("create_new_favorites --> OK")
 
     def update_playlists(self):
-        from pod.playlist.models import Playlist, PlaylistContent
+        """Update previous playlist table."""
+        from pod.playlist.models import Playlist
         from django.utils.translation import gettext_lazy as _
-        from django.contrib.auth.models import User
+
+        playlists_to_update = []
+
         for playlist_id, playlist_datas in PLAYLIST_INFORMATIONS.items():
             title, description, visible, owner_id = playlist_datas
             visibility = "private" if visible == 0 else "public"
@@ -155,20 +166,33 @@ class PlaylistConfig(AppConfig):
             playlist.owner_id = owner_id
             playlist.autoplay = True
             playlist.editable = True
-            playlist.save()
 
+            playlists_to_update.append(playlist)
+
+        Playlist.objects.bulk_update(playlists_to_update, fields=[
+            "name",
+            "description",
+            "visibility",
+            "owner_id",
+            "autoplay",
+            "editable"
+        ])
         print("update_playlists --> OK")
 
     def add_playlists_contents(self):
-        from pod.playlist.models import Playlist, PlaylistContent
+        """Add playlist content record from existing datas"""
+        from pod.playlist.models import PlaylistContent
         from django.utils.translation import gettext_lazy as _
-        from django.contrib.auth.models import User
+        content_list_to_bulk = []
         for content_datas in PLAYLIST_CONTENTS.values():
             position, playlist_id, video_id = content_datas
 
-            PlaylistContent.objects.create(
-                rank=position,
-                playlist_id=playlist_id,
-                video_id=video_id
+            content_list_to_bulk.append(
+                PlaylistContent(
+                    rank=position,
+                    playlist_id=playlist_id,
+                    video_id=video_id
+                )
             )
+        PlaylistContent.objects.bulk_create(content_list_to_bulk, batch_size=1000)
         print("add_playlists_contents --> OK")
