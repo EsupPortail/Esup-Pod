911d5c55d256c338665534ad7d34cee3161d18a2
Ptitloup <nicolas.can@univ-lille.fr>
2023-06-27 15:10:19 +0200

    [DONE] Ptitloup/improve cache request (#895)
    * add key prefix for default cache
    
    * add debug toolbar in pod project, show it if DEBUG is set to True
    
    * add queue in xapi celery command
    
    * add include requirements - remove ES V7 from conteneur requirement and put it in dockerfile, ES7 will be set by default in next futur
    
    * add debug toolbar only if installed
    
    * refactor get available video and put it in context_processor to get it in template
    
    * fix django_site table and bs constructor, add unit test to get available video
    
    * remove get_available_video call outside function
    
    * replace get_videos_list by get_available_video
    
    * fix unit test for test stat view
    
    * change docker doc to deal with redis db
    
    * remove unused line and refactor videos list

--- pod/video/context_processors.py
+++ pod/video/context_processors.py
@@ -1,9 +1,64 @@
 from django.conf import settings as django_settings
 
+from pod.video.models import Channel
+from pod.video.models import Theme
+from pod.video.models import Type
+from pod.video.models import Discipline
+from pod.video.models import Video
+
+from django.db.models import Count, Sum
+from django.db.models import Prefetch
+from django.db.models import Q
+from django.db.models import Exists
+from django.db.models import OuterRef
+
+from datetime import timedelta
+from django.contrib.sites.shortcuts import get_current_site
+from pod.main.models import AdditionalChannelTab
+from pod.video_encode_transcript.models import EncodingVideo
+from pod.video_encode_transcript.models import PlaylistVideo
+from pod.video_encode_transcript.models import EncodingAudio
+
 CHUNK_SIZE = getattr(django_settings, "CHUNK_SIZE", 100000)
 HIDE_USER_FILTER = getattr(django_settings, "HIDE_USER_FILTER", False)
 OEMBED = getattr(django_settings, "OEMBED", False)
 USE_STATS_VIEW = getattr(django_settings, "USE_STATS_VIEW", False)
+
+__AVAILABLE_VIDEO_FILTER__ = {
+    'encoding_in_progress': False,
+    'is_draft': False,
+    'sites': 1,
+}
+
+
+def get_available_videos(request=None):
+    """Get all videos available."""
+    __AVAILABLE_VIDEO_FILTER__["sites"] = get_current_site(request)
+    vids = Video.objects.filter(**__AVAILABLE_VIDEO_FILTER__).defer(
+        "video", "slug", "owner", "additional_owners", "description"
+    ).filter(
+        Q(Exists(
+            EncodingVideo.objects.filter(
+                video=OuterRef('pk'),
+                encoding_format="video/mp4"
+            )
+        ))
+        | Q(Exists(
+            PlaylistVideo.objects.filter(
+                video=OuterRef('pk'),
+                name="playlist",
+                encoding_format="application/x-mpegURL"
+            )
+        ))
+        | Q(Exists(
+            EncodingAudio.objects.filter(
+                video=OuterRef('pk'),
+                name="audio",
+                encoding_format="video/mp4"
+            )
+        ))
+    ).distinct()
+    return vids
 
 
 def context_video_settings(request):
@@ -13,3 +68,92 @@ def context_video_settings(request):
     new_settings["OEMBED"] = OEMBED
     new_settings["USE_STATS_VIEW"] = USE_STATS_VIEW
     return new_settings
+
+
+def context_navbar(request):
+    channels = (
+        Channel.objects.filter(
+            visible=True,
+            video__is_draft=False,
+            add_channels_tab=None,
+            site=get_current_site(request),
+        )
+        .distinct()
+        .annotate(video_count=Count("video", distinct=True))
+        .prefetch_related(
+            Prefetch(
+                "themes",
+                queryset=Theme.objects.filter(
+                    parentId=None, channel__site=get_current_site(request)
+                )
+                .distinct()
+                .annotate(video_count=Count("video", distinct=True)),
+            )
+        )
+    )
+
+    add_channels_tab = AdditionalChannelTab.objects.all().prefetch_related(
+        Prefetch(
+            "channel_set",
+            queryset=Channel.objects.filter(site=get_current_site(request))
+            .distinct()
+            .annotate(video_count=Count("video", distinct=True)),
+        )
+    )
+
+    all_channels = (
+        Channel.objects.all()
+        .filter(site=get_current_site(request))
+        .distinct()
+        .annotate(video_count=Count("video", distinct=True))
+        .prefetch_related(
+            Prefetch(
+                "themes",
+                queryset=Theme.objects.filter(
+                    channel__site=get_current_site(request)
+                )
+                .distinct()
+                .annotate(video_count=Count("video", distinct=True)),
+            )
+        )
+    )
+
+    types = (
+        Type.objects.filter(
+            sites=get_current_site(request),
+            video__is_draft=False,
+            video__sites=get_current_site(request),
+        )
+        .distinct()
+        .annotate(video_count=Count("video", distinct=True))
+    )
+
+    disciplines = (
+        Discipline.objects.filter(
+            site=get_current_site(request),
+            video__is_draft=False,
+            video__sites=get_current_site(request),
+        )
+        .distinct()
+        .annotate(video_count=Count("video", distinct=True))
+    )
+
+    list_videos = get_available_videos(request)
+    VIDEOS_COUNT = list_videos.count()
+    VIDEOS_DURATION = (
+        str(timedelta(seconds=list_videos.aggregate(
+            Sum("duration")
+        )["duration__sum"]))
+        if list_videos.aggregate(Sum("duration"))["duration__sum"]
+        else 0
+    )
+
+    return {
+        "ALL_CHANNELS": all_channels,
+        "ADD_CHANNELS_TAB": add_channels_tab,
+        "CHANNELS": channels,
+        "TYPES": types,
+        "DISCIPLINES": disciplines,
+        "VIDEOS_COUNT": VIDEOS_COUNT,
+        "VIDEOS_DURATION": VIDEOS_DURATION,
+    }
