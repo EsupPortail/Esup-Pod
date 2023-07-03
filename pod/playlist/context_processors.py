f265053c94361ee475e1ee55122fe2cd11abfd21
Aymeric Jakobowski <aymericjakpro@gmail.com>
2023-07-03 13:48:10 +0200

    Remove Favorites application

--- pod/playlist/context_processors.py
+++ pod/playlist/context_processors.py
@@ -1,6 +1,7 @@
 from django.conf import settings as django_settings
 
 USE_PLAYLIST = getattr(django_settings, "USE_PLAYLIST", True)
+USE_FAVORITES = getattr(django_settings, "USE_FAVORITES", True)
 DEFAULT_PLAYLIST_THUMBNAIL = getattr(
     django_settings,
     "DEFAULT_PLAYLIST_THUMBNAIL",
@@ -12,5 +13,6 @@ def context_settings(request):
     """Return all context settings for playlist app"""
     new_settings = {}
     new_settings["USE_PLAYLIST"] = USE_PLAYLIST
+    new_settings["USE_FAVORITES"] = USE_FAVORITES
     new_settings["DEFAULT_PLAYLIST_THUMBNAIL"] = DEFAULT_PLAYLIST_THUMBNAIL
     return new_settings
