fa5cf0a582d91f402651783e245dcf4dee7f5d21
Aymeric Jakobowski <106442526+AymericJak@users.noreply.github.com>
2023-08-30 16:41:46 +0200

    [DONE] Fix links for accessibility (#924)
    * Change header & footer titles
    
    * Change logo button in video player
    
    * Change some translations
    
    * Change LINK_PLAYER setting and link of logo in player
    
    * Change title for buttons
    
    * Remove redundant button
    
    * Change files location
    
    * Add LINK_PLAYER_NAME parameter
    
    * Fix some translations
    
    * Fix
    
    * Last fix

---
+++ pod/video/static/js/videojs-logo-controlbar.js
@@ -0,0 +1,64 @@
+(function () {
+  "use strict";
+  var videojs = null;
+  if (typeof window.videojs === "undefined" && typeof require === "function") {
+    videojs = require("video.js");
+  } else {
+    videojs = window.videojs;
+  }
+
+  (function (window, videojs) {
+    var videoJsLogo,
+      defaults = {
+        ui: true,
+      };
+
+    /*
+     * Logo menu link
+     */
+    var MenuLink = videojs.getComponent("ClickableComponent");
+
+    var LogoMenuLink = videojs.extend(MenuLink, {
+      constructor: function (player, options) {
+        var aElement = document.createElement("a");
+        console.log(options.link);
+        if (options.link && options.link !== "") {
+          aElement.href = options.link;
+        } else {
+          aElement.href = "/"
+        }
+        aElement.target = "_blank";
+        aElement.setAttribute("aria-label", options.linktitle);
+        aElement.setAttribute("title", options.linktitle);
+        aElement.classList.add("vjs-logo-button", "vjs-control")
+        aElement.setAttribute(
+          "style",
+          "background-image: url(" +
+            options.imgsrc +
+            "); background-repeat: no-repeat; background-position: center; cursor: pointer;",
+        );
+        MenuLink.call(this, player, { el: aElement });
+      },
+    });
+
+    /**
+     * Initialize the plugin.
+     */
+    videoJsLogo = function (options) {
+      var settings = videojs.mergeOptions(defaults, options),
+        player = this;
+      player.ready(function () {
+        if (settings.ui) {
+          var menuLink = new LogoMenuLink(player, settings);
+          player.controlBar.logo = player.controlBar.el_.appendChild(menuLink.el());
+          player.controlBar.logo.dispose = function () {
+            this.parentNode.removeChild(this);
+          };
+        }
+      });
+    };
+
+    // Register the plugin
+    videojs.registerPlugin("videoJsLogo", videoJsLogo);
+  })(window, videojs);
+})();
