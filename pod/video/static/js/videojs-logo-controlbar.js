74b20c223a49aa5e00b25e8d6914bb9ecded9323
Ptitloup <nicolas.can@univ-lille.fr>
2023-09-07 17:06:45 +0200

    workon videojs 8 plugin - ok for logo, quality selector, p2p, remove seek plugin, ok for note and playlist

--- pod/video/static/js/videojs-logo-controlbar.js
+++ pod/video/static/js/videojs-logo-controlbar.js
@@ -21,7 +21,6 @@
     class LogoMenuLink extends MenuLink {
       constructor(player, options) {
         var aElement = document.createElement("a");
-        console.log(options.link);
         if (options.link && options.link !== "") {
           aElement.href = options.link;
         } else {
