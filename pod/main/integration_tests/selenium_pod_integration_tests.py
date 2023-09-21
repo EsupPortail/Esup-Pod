05ef9e2218be13d4959badc21e9b42e4404faf17
Ptitloup <nicolas.can@univ-lille.fr>
2023-09-21 14:07:37 +0200

    try : pa11y no error

--- pod/main/integration_tests/selenium_pod_integration_tests.py
+++ pod/main/integration_tests/selenium_pod_integration_tests.py
@@ -6,21 +6,28 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 
 
 class PodSeleniumTests(StaticLiveServerTestCase):
+    """Tests the integration of Pod application with Selenium from side files"""
     fixtures = ["initial_data.json"]
 
     @classmethod
     def setUpClass(cls):
+        """Create the driver for all selenium tests."""
         super().setUpClass()
         cls.selenium = WebDriver()
         cls.selenium.implicitly_wait(10)
 
     @classmethod
     def tearDownClass(cls):
+        """Close the driver used."""
         cls.selenium.quit()
         super().tearDownClass()
 
-    def test_home(self):
+    def Test_selenium_suites(self):
+        """Run a set of Selenium Test Suites from Side files"""
         print(self.live_server_url)
         self.selenium.get(f"{self.live_server_url}/")
         print(self.selenium.title)
         assert "Welcome" in self.selenium.page_source
+
+    def run_suite(self, suite_name):
+        pass
