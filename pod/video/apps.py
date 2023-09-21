58712958869344a46b5daf49c98ae8334b09f30a
Aymeric Jakobowski <aymericjakpro@gmail.com>
2023-09-21 15:01:38 +0200

    Improve migration functions

--- pod/video/apps.py
+++ pod/video/apps.py
@@ -62,10 +62,13 @@ def update_video_passwords(sender, **kwargs):
     """Encrypt all video passwords."""
     from pod.video.models import Video
     from django.contrib.auth.hashers import make_password
-    for video in Video.objects.all():
-        if video.password and not video.password.startswith(('pbkdf2', 'sha256$')):
-            video.password = make_password(video.password, hasher="pbkdf2_sha256")
-            video.save()
+    from django.db.models import Q, F
+    # Filter insecure protected videos
+    videos_to_update = Video.objects.filter(
+        Q(password__isnull=False) & ~Q(password__startswith=('pbkdf2', 'sha256$')))
+    for video in videos_to_update:
+        video.password = make_password(video.password, hasher='pbkdf2_sha256')
+        video.save()
 
 
 class VideoConfig(AppConfig):
