6abd834d7020a7929c95fa334fd7b66542a227f3
LoicBonavent <loic.bonavent@umontpellier.fr>
2023-09-22 15:22:12 +0200

    Add recording_ready_url URL

--- pod/meeting/urls.py
+++ pod/meeting/urls.py
@@ -38,6 +38,16 @@ if not views.MEETING_DISABLE_RECORD:
             views.delete_internal_recording,
             name="delete_internal_recording",
         ),
+        path(
+            "internal_recording/<slug:meeting_id>/<slug:recording_id>/",
+            views.internal_recording,
+            name="internal_recording",
+        ),
+        path(
+            "recording_ready_url/",
+            views.recording_ready_url,
+            name="recording_ready_url"
+        ),
     ]
 
 urlpatterns += [
