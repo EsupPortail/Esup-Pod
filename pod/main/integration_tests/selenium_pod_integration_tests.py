ed79b40be8dba7f09a99bc7bd9fa3b6b4ce3ebb0
Ptitloup <nicolas.can@univ-lille.fr>
2023-09-25 11:19:56 +0200

    add directory to store side selenium. rename test selenium suite and work on parse side directory

--- pod/main/integration_tests/selenium_pod_integration_tests.py
+++ pod/main/integration_tests/selenium_pod_integration_tests.py
@@ -1,11 +1,14 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
 
+import glob
+import json
+
 # python manage.py test -v 3 --settings=pod.main.test_settings \
 # pod.main.integration_tests.selenium_pod_integration_tests
 
 
-class PodSeleniumTests(StaticLiveServerTestCase):
+class PodSeleniumTest(StaticLiveServerTestCase):
     """Tests the integration of Pod application with Selenium from side files"""
     fixtures = ["initial_data.json"]
 
@@ -22,12 +25,22 @@ class PodSeleniumTests(StaticLiveServerTestCase):
         cls.selenium.quit()
         super().tearDownClass()
 
-    def Test_selenium_suites(self):
+    def test_selenium_suites(self):
         """Run a set of Selenium Test Suites from Side files"""
+        print("\n\n - test_selenium_suites - ")
+        sides = glob.glob(r"sides/*.side")
+        print(sides)
+        for side in sides:
+            print(side)
+            with open('./sides/%s' % side) as json_file:
+                data = json.load(json_file)
+                print(data)
+
         print(self.live_server_url)
         self.selenium.get(f"{self.live_server_url}/")
         print(self.selenium.title)
         assert "Welcome" in self.selenium.page_source
+        print("\n\n - test_selenium_suites - ")
 
     def run_suite(self, suite_name):
         pass
