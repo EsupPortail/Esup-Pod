c40cccd19613763b0c70164fb1d89adb83fb35bd
Olivier Bado <bado@unice.fr>
2023-09-27 15:30:51 +0200

    Now if a core flatpage is missing, it will be created on Pod migration

--- pod/main/apps.py
+++ pod/main/apps.py
@@ -1,13 +1,79 @@
+"""Esup-pod Main applications."""
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ObjectDoesNotExist
 
 import json
 
 
+def create_missing_pages(sender, **kwargs):
+    """Create missing flat pages from several fixtures."""
+    print("---> Creating missing flat pages...")
+
+    with open("./pod/main/fixtures/initial_data.json", encoding="utf-8") as data_file:
+        json_data = json.loads(data_file.read())
+        print("--> Search missing initial pages...")
+        create_pages_and_links(json_data)
+
+    with open("./pod/main/fixtures/accessibility.json", encoding="utf-8") as data_file:
+        json_data = json.loads(data_file.read())
+        print("--> Search missing accessibility pages...")
+        create_pages_and_links(json_data)
+
+
+def create_pages_and_links(json_data):
+    """Create missing flat pages and associated footer links from one fixture."""
+    from pod.main.models import LinkFooter
+    from django.contrib.flatpages.models import FlatPage
+
+    created_pages = []
+    created_link = 0
+    for fixture in json_data:
+        if fixture["model"] == "flatpages.flatpage":
+            url = fixture["fields"]["url"]
+            page, page_created = FlatPage.objects.get_or_create(url=url)
+            if page_created:
+                print("-> New page created at “%s”." % url)
+                created_pages.append(fixture["fields"]["title_en"])
+                update_object_fields(page, fixture["fields"])
+
+        elif fixture["model"] == "main.linkfooter":
+            title = fixture["fields"]["title_en"]
+            link, link_created = LinkFooter.objects.get_or_create(title_en=title)
+            if link_created:
+                print("-> New Footer link created for “%s”." % title)
+                created_link = created_link + 1
+                update_object_fields(link, fixture["fields"], title)
+
+    if created_link == 0:
+        print("--> No missing link found.")
+    if len(created_pages) == 0:
+        print("--> No missing page found.")
+
+
+def update_object_fields(obj, fields, title=None):
+    """Update all fields values on object obj."""
+    from django.contrib.sites.models import Site
+    from django.contrib.flatpages.models import FlatPage
+
+    for field, value in fields.items():
+        if field == "sites":
+            obj.sites.add(Site.objects.get_current())
+        elif field == "page":
+            try:
+                obj.page = FlatPage.objects.get(title_en=title)
+            except ObjectDoesNotExist:
+                print("--> [[ ERROR ]] NO page with title_en=“%s”" % title)
+        else:
+            # print("--> Set ATTR [%s]" % (field))
+            setattr(obj, field, value)
+    obj.save()
+
+
 def create_missing_conf(sender, **kwargs):
+    """Create missing configurations from initial_data.json."""
     from pod.main.models import Configuration
-    from django.core.exceptions import ObjectDoesNotExist
 
     print("---> Creating missing configurations...")
     json_data = []
@@ -44,13 +110,17 @@ def create_missing_conf(sender, **kwargs):
                     description_en=description_en,
                 )
     if updated_count == 0:
-        print("--> No missing configurations found, all is up to date !")
+        print("--> No missing configurations found, all is up to date!")
 
 
 class MainConfig(AppConfig):
+    """Esup-pod Main configurations class."""
+
     name = "pod.main"
     default_auto_field = "django.db.models.BigAutoField"
     verbose_name = _("Main configurations")
 
     def ready(self):
+        """Run code when Django starts."""
         post_migrate.connect(create_missing_conf, sender=self)
+        post_migrate.connect(create_missing_pages, sender=self)
