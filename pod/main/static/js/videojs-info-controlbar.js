0425494f86109c8141e0efe1b0ac309d158f99e8
Olivier Bado <bado@unice.fr>
2023-11-07 11:53:34 +0100

    Fix the videojs-info

--- pod/main/static/js/videojs-info-controlbar.js
+++ pod/main/static/js/videojs-info-controlbar.js
@@ -1,3 +1,72 @@
+// Esup-Pod videojs-info-controlbar
+
+// Read-only globals defined in video-script.html
+/*
+  global player
+*/
+
+// Global vars
+const hdres = 1920/1080;
+let isPlaying = false, wasPlaying = false;
+
+/**
+ * Show or Hide the Pod video information panel (Called by video-iframe)
+ */
+function show_info_video() {
+  const hasStarted = player.el().classList.contains('vjs-has-started');
+  const isOverlayed = document.getElementById("div-video-wrapper").classList.contains('overlay');
+  const isInfoOverlayed = isOverlayed && document.getElementById("div-video-wrapper").classList.contains('info');
+  const cls = 'overlay info';
+  if(!isOverlayed) { // No overlay is displayed
+    wasPlaying = isPlaying;
+    player.pause();
+    document.getElementById("div-video-wrapper").className = cls;
+    resizeInfo();
+  } else if(!isInfoOverlayed) { // One overlay is displayed but not Info
+    document.getElementById("div-video-wrapper").className = cls;
+    resizeInfo();
+  } else { // Info overlay is displayed => close
+    document.getElementById("div-video-wrapper").className = (hasStarted ? '' : 'vjs-not-started');
+    if(wasPlaying) player.play();
+  }
+}
+
+/**
+ * Resize responsively the Pod video Information panel
+ */
+function resizeInfo() {
+  const ihead = document.querySelector('#info-video-wrapper > .iframe-header')
+    , ph = player.el().offsetHeight
+    , pb = parseInt(player.el().style.top) + ph - 30
+    , pw = ph*hdres;  //$('#podvideoplayer .vjs-poster').width()
+  document.getElementById("info-video").style.top = ihead.offsetHeight+'px';
+  // console.log('MTop: '+player.el().style.top+'\nleft: '+player.el().offsetLeft+'\nwidth: '+player.el().offsetWidth+'\nheight: '+player.el().offsetHeight+' /// '+pb)
+  document.getElementById("info-video-wrapper").cssText = "top:"+player.el().style.top+", height:"+(ph - 30)+"px, left: '50%', 'margin-left':"+ -(pw/2)+"px, width:"+ pw+"px";
+  document.getElementById("overlay-footer").style.top = pb +'px';
+}
+
+/**
+ * Resize responsively the video.js player (Called by video-iframe.html)
+ */
+function resizeVideoJs(){
+  const width = document.getElementById(player.id()).parentElement.offsetWidth
+    , height = document.getElementById(player.id()).parentElement.offsetHeight
+    , ratio = width/height;
+  let margintop = 0;
+  if(ratio<hdres) {
+    player.width(width);
+    player.height(width / hdres);
+    margintop = parseInt((height - player.height())/2);
+    document.getElementById("podvideoplayer").style.top = margintop+"px";
+  } else {
+    player.height(height);
+    player.width(height*hdres);
+    document.getElementById("podvideoplayer").style.top = "0";
+  }
+  resizeInfo();
+}
+
+
 (function () {
   "use strict";
   var videojs = null;
@@ -20,11 +89,11 @@
 
     class InfoMenuButton extends MenuButton {
       constructor(player, options) {
-        options.label = "Info";
-        MenuButton.call(this, player, options);
-        this.el().setAttribute("aria-label", "Info");
+        options.label = gettext("Info");
+        super(player, options);
+        this.el().setAttribute("aria-label", options.label);
         videojs.dom.addClass(this.el(), "vjs-info-button");
-        this.controlText("Information");
+        this.controlText(gettext("Information"));
       }
     }
     InfoMenuButton.prototype.handleClick = function (event) {
--- pod/video/static/js/ajax-display-channels.js
+++ pod/video/static/js/ajax-display-channels.js
@@ -1,3 +1,10 @@
+// Read-only globals defined in base.html
+/*
+  global GET_CHANNELS_FOR_SPECIFIC_CHANNEL_TAB_REQUEST_URL, GET_CHANNEL_TAGS_REQUEST_URL,
+  GET_THEMES_FOR_SPECIFIC_CHANNEL_REQUEST_URL
+*/
+
+
 /**
  * Set attributes of an HTML element with a two-dimensional array.
  *
