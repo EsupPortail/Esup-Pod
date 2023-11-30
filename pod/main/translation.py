8330743f34767ab47a01003a7911e375ac3bfe82
fanfounet <franck.charneau@gmail.com>
2023-11-30 11:35:47 +0100

    add translation field name of AdditionalChannelTab

--- pod/main/translation.py
+++ pod/main/translation.py
@@ -2,6 +2,7 @@ from modeltranslation.translator import translator, TranslationOptions
 from django.contrib.flatpages.models import FlatPage
 from pod.main.models import LinkFooter
 from pod.main.models import Configuration
+from pod.main.models import AdditionalChannelTab
 
 
 class FlatPageTranslationOptions(TranslationOptions):
@@ -18,7 +19,10 @@ class LinkFooterTranslationOptions(TranslationOptions):
 class ConfigurationTranslationOptions(TranslationOptions):
     fields = ("description",)
 
+class AdditionalChannelTabTranslationOptions(TranslationOptions):
+    fields = ("name",)
 
 translator.register(FlatPage, FlatPageTranslationOptions)
 translator.register(LinkFooter, LinkFooterTranslationOptions)
 translator.register(Configuration, ConfigurationTranslationOptions)
+translator.register(AdditionalChannelTab, AdditionalChannelTabTranslationOptions)
