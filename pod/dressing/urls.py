32d2005c828f50edf63e49edc6ca0723e9a7ed43
Valentin Sabatier <valentin.sabatier@hotmail.fr>
2023-12-05 10:59:00 +0100

    Use path instead url

--- pod/dressing/urls.py
+++ pod/dressing/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.urls import path
 from pod.dressing.views import video_dressing, my_dressings, dressing_edit
 from pod.dressing.views import dressing_create, dressing_delete
 
 app_name = "dressing"
 
 urlpatterns = [
-    url(r"^my/$", my_dressings, name="my_dressings"),
-    url(r"^edit/(?P<dressing_id>[\-\d\w]+)/$", dressing_edit, name="dressing_edit"),
-    url(r"^new/$", dressing_create, name="dressing_create"),
-    url(r"^delete/(?P<dressing_id>[\-\d\w]+)/$", dressing_delete, name="dressing_delete"),
-    url(r"^(?P<slug>[\-\d\w]+)/$", video_dressing, name="video_dressing"),
+    path("my/", my_dressings, name="my_dressings"),
+    path("edit/<slug:dressing_id>/", dressing_edit, name="dressing_edit"),
+    path("new/", dressing_create, name="dressing_create"),
+    path("delete/<slug:dressing_id>/", dressing_delete, name="dressing_delete"),
+    path("<slug:slug>/", video_dressing, name="video_dressing"),
 ]
