929e01876bead60b537081f1e2083e4e5ba4a34a
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2023-12-14 16:33:50 +0100

    Add connection to an other peer

--- pod/peer_to_peer/static/peer_to_peer/js/p2p-script.js
+++ pod/peer_to_peer/static/peer_to_peer/js/p2p-script.js
@@ -12,20 +12,39 @@ let peer; // TODO Update this ?
 
 let firstTime = false;
 
+let connectedId = "";
 
-function startConnect(options) {
+
+async function startConnect(options) {
     console.log('[p2p-script.js] startConnect()');
     let uuid = crypto.randomUUID();
     let peerId = `${videoSlug}_ID_${uuid}`;
     peer = new Peer(peerId, options);
     console.log("Peer:", peer);
 
-    if (false) { // TODO Delete this test | Set true for sender
-        let idToConnect = '47858-rabbit-and-others-100bd4a6-9077-440f-8cb3-21e25a096881';
-        let conn = peer.connect(idToConnect);
-        conn.on('open', function () {
-            conn.send('Hi!');
-        });
+    const idList = await getIds();
+    if (idList.length <= 1) {
+        // TODO Make CDN live function and call this here
+    } else {
+        let conn;
+        for (let i = 0; i++; i < idList.length) {
+            if (idList[i] != videoSlug) {
+                connectedId = idList[i];
+                try {
+                    conn = peer.connect(connectedId);
+                    break;
+                } catch {
+                    // TODO Remove connectedId from caches with request
+                }
+            }
+        }
+        if (!conn) {
+            // TODO Make CDN live function and call this here if the all peer don't exists
+        } else {
+            conn.on('open', function() {
+                conn.send('Connected successfully');
+            });
+        }
     }
 
     peer.on('connection', function (conn) {
@@ -45,7 +64,7 @@ async function getIds() {
     let postData = {
         'url': videoSlug,
     };
-    console.log('[p2p-script.js] postData:', postData);
+    // console.log('[p2p-script.js] postData:', postData);
     // TODO Update the URL
     await fetch('http://localhost:9090/peer-to-peer/get-ids/', {
         method: 'POST',
@@ -56,7 +75,6 @@ async function getIds() {
     }).then(response => response.json())
         .then(response => {
             idList = [].concat(response);
-            console.log(idList);
         })
         .catch(err => console.error(err));
     return idList;
@@ -69,8 +87,8 @@ async function storeUrlsId() {
     for (let i = 0; i < urlList.length; i++) {
         postData[urlList[i]] = 1;
     }
-    console.log('[p2p-script.js] postData:', postData);
-    console.log('[p2p-script.js] JSON.stringify(postData):', JSON.stringify(postData));
+    // console.log('[p2p-script.js] postData:', postData);
+    // console.log('[p2p-script.js] JSON.stringify(postData):', JSON.stringify(postData));
     // TODO Change the URL.
     fetch(`http://localhost:9090/peer-to-peer/store/${peer.id}/`, {
         method: 'POST',
@@ -82,12 +100,12 @@ async function storeUrlsId() {
         body: JSON.stringify(postData),
     }).then(function (response) {
         fetch_status = response.status;
-        console.log("JSON", response);
+        // console.log("JSON", response);
         return response.json();
     }).then(function (json) {
-        console.log("FETCH-STATUS:", fetch_status);
+        // console.log("FETCH-STATUS:", fetch_status);
         if (fetch_status == 200) {
-            console.log(json);
+            // console.log(json);
         }
     }).catch(function (error) {
         console.log(error);
@@ -99,12 +117,12 @@ async function storeUrlsId() {
 player.on('xhr-hooks-ready', () => {
     console.log('[p2p-script.js] xhr-hooks-ready event');
     const playerOnResponseHook = (request, error, response) => {
-        console.log('[p2p-script.js] Inside playerOnResponseHook');
+        // console.log('[p2p-script.js] Inside playerOnResponseHook');
         // console.log('[p2p-script.js] content_type:', content_type);
-        console.log('[p2p-script.js] request: ', request);
-        console.log('[p2p-script.js] response: ', response);
+        // console.log('[p2p-script.js] request: ', request);
+        // console.log('[p2p-script.js] response: ', response);
         // if (content_type.includes(response.headers['content-type'])) {
-        console.log('[p2p-script.js] Condition met');
+        // console.log('[p2p-script.js] Condition met');
         urlList.push(peer.id);
         coreCache[`${response.url}_ID_${peer.id}`] = response;
         if (urlList.length > 10) {
@@ -127,14 +145,14 @@ player.on('xhr-hooks-ready', () => {
 
     console.log('[p2p-script.js] player.tech().vhs.xhr:', player.tech().vhs.xhr);
     player.tech().vhs.xhr = function(urlC, callback) {
-        console.log("URLC CALLLLLLLL", urlC, callback);
+        // console.log("URLC CALLLLLLLL", urlC, callback);
         let url = '';
-        console.log('[p2p-script.js] player.tech().vhs.xhr');
+        // console.log('[p2p-script.js] player.tech().vhs.xhr');
         if (typeof urlC === 'object') {
             url = urlC.uri;
-            console.log('[p2p-script.js] url:', url);
-            console.log('[p2p-script.js] urlC:', urlC);
-            console.log('[p2p-script.js] urlC.responseType:', urlC.responseType);
+            // console.log('[p2p-script.js] url:', url);
+            // console.log('[p2p-script.js] urlC:', urlC);
+            // console.log('[p2p-script.js] urlC.responseType:', urlC.responseType);
             if (urlC.responseType === 'arraybuffer') {
                 firstTime = true;
                 if (nbLog < 10) {
@@ -143,7 +161,7 @@ player.on('xhr-hooks-ready', () => {
                 }
                 let ids = []; // TODO Go the search ID in the server
                 if (ids.length > 0) {
-                    console.log('youpi');
+                    // console.log('youpi');
                 }
                 videojs.Vhs.xhr.onResponse(playerOnResponseHook);
                 return videojs.Vhs.xhr(urlC, callback);
