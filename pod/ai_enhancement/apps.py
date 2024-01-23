07e0087726e99340bafc4a05daaaddcd9c470085
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2024-01-23 14:56:35 +0100

    Create the AI Enhancement application

---
+++ pod/ai_enhancement/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class IaEnhancementConfig(AppConfig):
+    name = 'pod.ai_enhancement'
+    default_auto_field = 'django.db.models.BigAutoField'
+    verbose_name = 'Artificial Intelligence Enhancement'
