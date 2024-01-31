7ffe181b2494f1b53e95849ba7bc78fc6bd5e74f
Valentin Sabatier <valentin.sabatier@hotmail.fr>
2024-01-31 10:00:21 +0100

    fix

--- pod/dressing/tests/test_views.py
+++ pod/dressing/tests/test_views.py
@@ -126,7 +126,9 @@ class DressingDeleteViewTest(TestCase):
 class DressingCreateViewTest(TestCase):
     """Dressing create page test case."""
 
-    def setUp(self):
+    fixtures = ["initial_data.json"]
+
+    def setUp(self) -> None:
         self.user = User.objects.create_user(
             username="user", password="password", is_staff=1
         )
