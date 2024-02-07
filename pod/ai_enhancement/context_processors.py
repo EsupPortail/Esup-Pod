af83403462d92c5baa7c40252eba710cffbbd64f
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2024-02-07 12:47:25 +0100

    Create the AI_ENRICHMENT_CLIENT_ID & AI_ENRICHMENT_SECRET settings

--- pod/ai_enhancement/context_processors.py
+++ pod/ai_enhancement/context_processors.py
@@ -2,6 +2,8 @@ from django.conf import settings as django_settings
 
 AI_ENRICHMENT_DIR = getattr(django_settings, "AI_ENRICHMENT_DIR", "ai-enrichments")
 USE_AI_ENRICHMENT = getattr(django_settings, "USE_AI_ENRICHMENT", True)
+AI_ENRICHMENT_CLIENT_ID = getattr(django_settings, "AI_ENRICHMENT_CLIENT_ID", "mocked_id")
+AI_ENRICHMENT_CLIENT_SECRET = getattr(django_settings, "AI_ENRICHMENT_CLIENT_SECRET", "mocked_secret")
 
 
 def context_settings(request):
@@ -9,5 +11,7 @@ def context_settings(request):
     new_settings = {
         "AI_ENRICHMENT_DIR": AI_ENRICHMENT_DIR,
         "USE_AI_ENRICHMENT": USE_AI_ENRICHMENT,
+        "AI_ENRICHMENT_CLIENT_ID": AI_ENRICHMENT_CLIENT_ID,
+        "AI_ENRICHMENT_CLIENT_SECRET": AI_ENRICHMENT_CLIENT_SECRET,
     }
     return new_settings
