63391e4af85d12d1ba7b2290cd45905b6ad11331
Aymeric Jakobowski <aymericjakpro@gmail.com>
2024-02-08 13:59:20 +0100

    Add urls

--- pod/quiz/urls.py
+++ pod/quiz/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 
+from pod.quiz.views import to_do
+
 app_name = "quiz"
 
-urlpatterns = []
+urlpatterns = [
+    path("/<slug:video_slug>/", to_do, name="video_quiz"),  # TODO
+    path("add/<slug:video_slug>/", to_do, name="add_quiz"),  # TODO
+    path("edit/<slug:video_slug>/", to_do, name="edit_quiz"),  # TODO
+    path("remove/<slug:video_slug>/", to_do, name="remove_quiz"),  # TODO
+    path("<slug:video_slug>/statistics/", to_do, name="statistics_quiz"),  # TODO
+]
--- pod/quiz/views.py
+++ pod/quiz/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def to_do(request):  # TODO remove this view
+    return "To do..."
