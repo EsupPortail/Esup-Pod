f956891ae60c5685011b401aeff69daeee01b589
Aymeric Jakobowski <aymericjakpro@gmail.com>
2024-02-27 14:50:21 +0100

    Improve quiz form

--- pod/quiz/forms.py
+++ pod/quiz/forms.py
@@ -1,84 +1,6 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
-
-from pod.quiz.models import Question, Quiz, UniqueChoiceQuestion
-
-
-# class QuestionForm(forms.ModelForm):
-# QUESTION_TYPES = [
-#     (
-#         _("Redaction"),
-#         [
-#             ("short_answer", _("Short Answer")),
-#             ("long_answer", _("Long Answer")),
-#         ],
-#     ),
-#     (
-#         _("Choice"),
-#         [
-#             ("unique_choice", _("Unique Choice")),
-#             ("multiple_choice", _("Multiple Choice")),
-#         ],
-#     ),
-# ]
-
-#     type = forms.ChoiceField(
-#         choices=QUESTION_TYPES,
-#         initial="unique_choice",
-#         widget=forms.Select(),
-#         label=_("Question Type"),
-#     )
-
-#     class Meta:
-#         model = Question
-#         fields = ["title", "explanation", "start_timestamp", "end_timestamp", "type"]
-
-
-# class ChoiceForm(forms.Form):
-#     proposition = forms.CharField(label=_("Proposition"), max_length=255)
-#     is_correct = forms.BooleanField(label=_("Is correct"), required=False)
-
-
-# class UniqueChoiceForm(QuestionForm):
-#     choices = forms.JSONField(
-#         label=_("Choices"),
-#         widget=forms.HiddenInput(),
-#     )
-
-#     ChoiceFormSet = forms.formset_factory(ChoiceForm, extra=2)
-
-#     class Meta:
-#         model = UniqueChoiceQuestion
-#         fields = QuestionForm.Meta.fields + ["choices"]
-
-
-# class ChoiceFormSet(forms.formset_factory(ChoiceForm, extra=2, can_delete=True)):
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         choices_dict = {}
-#         for form in self.forms:
-#             proposition = form.cleaned_data.get("proposition")
-#             is_correct = form.cleaned_data.get("is_correct")
-#             if proposition and is_correct:
-#                 choices_dict[proposition] = is_correct
-#         # if len(choices_dict) < 2:
-#         #     raise forms.ValidationError(_("There must be at least 2 choices."))
-#         # if sum(choices_dict.values()) != 1:
-#         #     raise forms.ValidationError(_("There must be only one correct answer."))
-#         return cleaned_data
-
-
-# class QuizForm(forms.ModelForm):
-#     QuestionFormSet = forms.formset_factory(UniqueChoiceForm, extra=2)
-
-#     def __init__(self, *args, **kwargs):
-#         self.video = kwargs.pop("video")
-#         self.user = kwargs.pop("user")
-#         super(QuizForm, self).__init__(*args, **kwargs)
-
-#     class Meta:
-#         model = Quiz
-#         fields = ["connected_user_only", "activated_statistics", "show_correct_answers"]
+from pod.main.forms_utils import add_placeholder_and_asterisk
 
 
 class QuestionForm(forms.Form):
@@ -116,8 +38,19 @@ class UniqueChoiceForm(forms.Form):
 
 class QuizForm(forms.Form):
     connected_user_only = forms.BooleanField(
-        label=_("Connected User Only"), required=False)
-    activated_statistics = forms.BooleanField(
-        label=_("Activated Statistics"), required=False)
+        label=_("Connected User Only"),
+        required=False,
+        widget=forms.CheckboxInput(),
+        help_text=_("Only the connected users can answer to this quiz.")
+    )
     show_correct_answers = forms.BooleanField(
-        label=_("Show Correct Answers"), required=False)
+        label=_("Show Correct Answers"),
+        required=False,
+        widget=forms.CheckboxInput(),
+        help_text=_("The correction page can be reach.")
+    )
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Init method."""
+        super(QuizForm, self).__init__(*args, **kwargs)
+        self.fields = add_placeholder_and_asterisk(self.fields)
--- pod/quiz/views.py
+++ pod/quiz/views.py
@@ -2,48 +2,28 @@ from django.forms import formset_factory
 from django.shortcuts import get_object_or_404, render
 from django.utils.translation import gettext_lazy as _
 
-from pod.quiz.forms import QuestionForm, QuizForm, UniqueChoiceForm
+from pod.quiz.forms import QuestionForm, QuizForm
 from pod.video.models import Video
-
-# Create your views here.
 
 
 def to_do(request):  # TODO remove this view
     return "To do..."
 
 
-# def create_quiz(request, video_slug: str):
-#     video = get_object_or_404(Video, slug=video_slug)
-#     quiz_form = QuizForm(request.POST, video=video, user=request.user)
-#     question_form = UniqueChoiceForm()
-#     question_formset = quiz_form.QuestionFormSet()
-
-#     return render(
-#         request,
-#         "create_edit_quiz.html",
-#         {
-#             "page_title": _("Create a new quiz"),
-#             "quiz_form": quiz_form,
-#             "video": video,
-#             "question_form": question_form,
-#             "question_formset": question_formset,
-#             'choice_formset': question_form.ChoiceFormSet()
-#         },
-#     )
-
-
 def create_quiz(request, video_slug: str):
+    video = get_object_or_404(Video, slug=video_slug)
     QuestionFormSet = formset_factory(QuestionForm, extra=2)
     if request.method == 'POST':
         quiz_form = QuizForm(request.POST)
         question_formset = QuestionFormSet(request.POST, prefix='questions')
 
     else:
-        print("ici")
         quiz_form = QuizForm()
         question_formset = QuestionFormSet(prefix='questions')
 
     return render(request, 'quiz/create_edit_quiz.html', {
+        "page_title": _(f"Quiz edition for the video: {video.title}"),
         'quiz_form': quiz_form,
         'question_formset': question_formset,
+        'video': video,
     })
