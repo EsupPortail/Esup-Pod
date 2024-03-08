de2dc3fa78f421d8d150477bf9ab7b442a2ec863
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2024-03-08 13:30:11 +0100

    Rename user_can_enrich_video by user_can_enhance_video

--- pod/ai_enhancement/templatetags/ai_enhancement_template_tags.py
+++ pod/ai_enhancement/templatetags/ai_enhancement_template_tags.py
@@ -17,8 +17,8 @@ USE_AI_ENHANCEMENT = getattr(settings, "USE_AI_ENHANCEMENT", True)
 register = Library()
 
 
-@register.simple_tag(takes_context=True, name="user_can_enrich_video")
-def user_can_enrich_video(context: dict, video: Video) -> bool:
+@register.simple_tag(takes_context=True, name="user_can_enhance_video")
+def user_can_enhance_video(context: dict, video: Video) -> bool:
     """
     Template tag used to check if the user can enrich a specific video.
 
@@ -57,7 +57,7 @@ def enhancement_is_ready(context: dict, video: Video) -> bool:
     request = context["request"]
     if not request.user.is_authenticated:
         return False
-    return eir(video) and USE_AI_ENHANCEMENT and user_can_enrich_video(context, video)
+    return eir(video) and USE_AI_ENHANCEMENT and user_can_enhance_video(context, video)
 
 
 @register.simple_tag(takes_context=True, name="enhancement_is_already_asked")
@@ -75,4 +75,4 @@ def enhancement_is_already_asked(context: dict, video: Video) -> bool:
     request = context["request"]
     if not request.user.is_authenticated:
         return False
-    return eia(video) and USE_AI_ENHANCEMENT and user_can_enrich_video(context, video) and not eir(video)
+    return eia(video) and USE_AI_ENHANCEMENT and user_can_enhance_video(context, video) and not eir(video)
