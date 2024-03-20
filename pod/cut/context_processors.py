a7758ea54a0fbdbefddbe4b50ea0dc460e0dcd11
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2024-03-20 09:03:04 +0100

    Add USE_CUT setting in context processors file.

---
+++ pod/cut/context_processors.py
@@ -0,0 +1,11 @@
+from django.conf import settings as django_settings
+
+USE_CUT = getattr(django_settings, "USE_CUT", True)
+
+
+def context_settings(request):
+    """Return all context settings for cut app"""
+    new_settings = {
+        "USE_CUT": USE_CUT,
+    }
+    return new_settings
