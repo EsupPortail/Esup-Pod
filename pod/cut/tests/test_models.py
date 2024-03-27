acefc7b4817b081fbdcb3340b4fa6cb62fd528f4
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2024-03-27 10:09:41 +0100

    Add test for verify_time

--- pod/cut/tests/test_models.py
+++ pod/cut/tests/test_models.py
@@ -11,16 +11,23 @@ from ..models import CutVideo
 
 
 class CutVideoModelTestCase(TestCase):
+    """
+    Test case for the CutVideo model.
+    """
+
     fixtures = [
         "initial_data.json",
     ]
 
     def setUp(self):
+        """
+        Set up the test with a video and a cut.
+        """
         owner = User.objects.create(username="test")
-        videotype = Type.objects.create(title="others")
+        video_type = Type.objects.create(title="others")
         video = Video.objects.create(
             title="video",
-            type=videotype,
+            type=video_type,
             owner=owner,
             video="test.mp4",
             duration=20,
@@ -28,6 +35,9 @@ class CutVideoModelTestCase(TestCase):
         CutVideo.objects.create(video=video, start="00:00:00", end="00:00:20")
 
     def test_bad_time(self):
+        """
+        Test the creation with bad time values.
+        """
         video = Video.objects.get(id=1)
         cut = CutVideo()
         cut.duration = video.duration_in_time
@@ -38,5 +48,17 @@ class CutVideoModelTestCase(TestCase):
         cut.start = "00:00:12"
         cut.end = "00:00:08"
         self.assertRaises(ValidationError, cut.clean)
-
         print(" ---> test_bad_time: OK ! --- CutVideoModel")
+
+    def test_verify_time__value_error(self):
+        """
+        Test verify_time method with bad values.
+        """
+        video = Video.objects.get(id=1)
+        cut = CutVideo()
+        cut.duration = video.duration_in_time
+        cut.video = video
+        cut.start = "bad_value"
+        cut.end = "bad_value"
+        self.assertFalse(cut.verify_time())
+        print(" ---> test_verify_time_value_error: OK ! --- CutVideoModel")
