b07be23233577cdb8dd21be735efaa298f7f1bf8
Aymeric Jakobowski <aymericjakpro@gmail.com>
2024-03-27 11:26:09 +0100

    Fix video access tokens.

--- pod/cut/tests/test_models.py
+++ pod/cut/tests/test_models.py
@@ -1,6 +1,4 @@
-"""
-Unit tests for cut models
-"""
+"""Unit tests for cut models."""
 
 from django.test import TestCase
 from django.core.exceptions import ValidationError
@@ -11,18 +9,14 @@ from ..models import CutVideo
 
 
 class CutVideoModelTestCase(TestCase):
-    """
-    Test case for the CutVideo model.
-    """
+    """Test case for the CutVideo model."""
 
     fixtures = [
         "initial_data.json",
     ]
 
     def setUp(self):
-        """
-        Set up the test with a video and a cut.
-        """
+        """Set up the test with a video and a cut."""
         owner = User.objects.create(username="test")
         video_type = Type.objects.create(title="others")
         video = Video.objects.create(
@@ -35,9 +29,7 @@ class CutVideoModelTestCase(TestCase):
         CutVideo.objects.create(video=video, start="00:00:00", end="00:00:20")
 
     def test_bad_time(self):
-        """
-        Test the creation with bad time values.
-        """
+        """Test the creation with bad time values."""
         video = Video.objects.get(id=1)
         cut = CutVideo()
         cut.duration = video.duration_in_time
@@ -51,9 +43,7 @@ class CutVideoModelTestCase(TestCase):
         print(" ---> test_bad_time: OK ! --- CutVideoModel")
 
     def test_verify_time__value_error(self):
-        """
-        Test verify_time method with bad values.
-        """
+        """Test verify_time method with bad values."""
         video = Video.objects.get(id=1)
         cut = CutVideo()
         cut.duration = video.duration_in_time
