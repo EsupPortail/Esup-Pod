a586a0e22ef05268a9da7d12d0c3e6a96770fac9
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2024-04-09 13:48:42 +0200

    :white_check_mark: Add unit tests for the model

--- pod/ai_enhancement/tests/test_models.py
+++ pod/ai_enhancement/tests/test_models.py
@@ -25,9 +25,20 @@ class AIEnrichmentModelTest(TestCase):
             owner=self.owner,
             type=Type.objects.create(title="Test Type"),
         )
-        self.ai_enhancement = AIEnhancement.objects.create(video=self.video)
+        self.ai_enhancement = AIEnhancement.objects.create(video=self.video, ai_enhancement_id_in_aristote="test_id")
 
     def test_create_ai_enhancement(self):
         """Test the model creation."""
         self.assertEqual(self.ai_enhancement.video, self.video)
+        self.assertEqual(self.ai_enhancement.ai_enhancement_id_in_aristote, "test_id")
         print(" --->  test_create_ai_enhancement ok")
+
+    def test_str(self):
+        """Test the string representation."""
+        self.assertEqual(str(self.ai_enhancement), f"{self.video.title} - test_id")
+        print(" --->  test_str ok")
+
+    def test_sites(self):
+        """Test the sites property."""
+        self.assertEqual(self.ai_enhancement.sites, self.video.sites)
+        print(" --->  test_sites ok")
