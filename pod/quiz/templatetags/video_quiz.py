a256fecd4ee8290211a514e26f044342ce877be9
Aymeric Jakobowski <aymericjakpro@gmail.com>
2024-05-30 15:28:26 +0200

    :lipstick: Improve question infos when submitting

--- pod/quiz/templatetags/video_quiz.py
+++ pod/quiz/templatetags/video_quiz.py
@@ -67,4 +67,4 @@ def get_question_color(is_submitted: bool, score: int = None) -> str:
         elif score <= 0.75:
             return "warning"
         return "success"
-    return ""
+    return "dark"
