8ddec6b714607cadcbd8f372012ee75850a65ea9
Charneau Franck <franck.charneau@univ-lr.fr>
2024-06-23 17:54:17 +0200

    add views unit tests

---
+++ pod/speaker/tests/test_views.py
@@ -0,0 +1,91 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from pod.speaker.models import Speaker, Job
+
+
+class SpeakerViewsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.superuser = User.objects.create_superuser(
+            username='admin', password='admin', email='admin@example.com'
+        )
+        self.user = User.objects.create_user(
+            username='user', password='user'
+        )
+        self.speaker = Speaker.objects.create(firstname='John', lastname='Doe')
+        self.job = Job.objects.create(title='Engineer', speaker=self.speaker)
+
+    def test_speaker_management_superuser_get(self):
+        self.client.login(username='admin', password='admin')
+        response = self.client.get(reverse('speaker:speaker_management'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'speaker/speakers_management.html')
+
+    def test_speaker_management_non_superuser_get(self):
+        self.client.login(username='user', password='user')
+        response = self.client.get(reverse('speaker:speaker_management'))
+        self.assertEqual(response.status_code, 403)
+
+    def test_add_speaker(self):
+        self.client.login(username='admin', password='admin')
+        data = {
+            'action': 'add',
+            'firstname': 'Jane',
+            'lastname': 'Smith',
+            'jobs[]': ['Teacher', 'Researcher'],
+        }
+        url = reverse('speaker:speaker_management')
+        response = self.client.post(url, data)
+        self.assertRedirects(response, reverse('speaker:speaker_management'))
+        self.assertTrue(Speaker.objects.filter(firstname='Jane', lastname='Smith').exists())
+        speaker = Speaker.objects.get(firstname='Jane', lastname='Smith')
+        self.assertEqual(speaker.job_set.count(), 2)
+
+    def test_delete_speaker(self):
+        self.client.login(username='admin', password='admin')
+        data = {
+            'action': 'delete',
+            'speakerid': self.speaker.id,
+        }
+        response = self.client.post(reverse('speaker:speaker_management'), data)
+        self.assertRedirects(response, reverse('speaker:speaker_management'))
+        self.assertFalse(Speaker.objects.filter(id=self.speaker.id).exists())
+
+    def test_edit_speaker(self):
+        self.client.login(username='admin', password='admin')
+        data = {
+            'action': 'edit',
+            'speakerid': self.speaker.id,
+            'firstname': 'Johnny',
+            'lastname': 'Doe',
+            'jobIds[]': [self.job.id],
+            'jobs[]': ['Senior Engineer'],
+        }
+        response = self.client.post(reverse('speaker:speaker_management'), data)
+        self.assertRedirects(response, reverse('speaker:speaker_management'))
+        speaker = Speaker.objects.get(id=self.speaker.id)
+        self.assertEqual(speaker.firstname, 'Johnny')
+        self.assertEqual(speaker.job_set.count(), 1)
+        self.assertEqual(speaker.job_set.first().title, 'Senior Engineer')
+
+    def test_get_speaker(self):
+        self.client.login(username='admin', password='admin')
+        response = self.client.get(reverse('speaker:get_speaker', args=[self.speaker.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            'speaker': {
+                'id': self.speaker.id,
+                'firstname': self.speaker.firstname,
+                'lastname': self.speaker.lastname,
+                'jobs': [{'id': self.job.id, 'title': self.job.title}],
+            }
+        })
+
+    def test_get_jobs(self):
+        self.client.login(username='admin', password='admin')
+        response = self.client.get(reverse('speaker:get_jobs', args=[self.speaker.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            'jobs': [{'id': self.job.id, 'title': self.job.title}]
+        })
