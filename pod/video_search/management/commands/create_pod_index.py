4cbb1ab9295e97f4ed2e19679931d9f4d6f264e6
Ptitloup <nicolas.can@univ-lille.fr>
2024-07-10 16:33:16 +0200

    Update create_pod_index.py
    fix pep8 compliant to remove unused import and remove unused globals

--- pod/video_search/management/commands/create_pod_index.py
+++ pod/video_search/management/commands/create_pod_index.py
@@ -3,12 +3,10 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from pod.video_search.utils import create_index_es, delete_index_es
-from elasticsearch import Elasticsearch, exceptions
+from elasticsearch import exceptions
 import logging
 
 logger = logging.getLogger(__name__)
-
-ES_URL = getattr(settings, "ES_URL", ["http://elasticsearch.localhost:9200/"])
 
 
 class Command(BaseCommand):
