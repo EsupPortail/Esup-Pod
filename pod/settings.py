33f1d51521b33d9ad0143939e02602075c59401e
Loïc Bonavent <loic.bonavent@umontpellier.fr>
2025-04-08 15:59:27 +0200

    Manage flake8

--- pod/settings.py
+++ pod/settings.py
@@ -10,14 +10,13 @@ import importlib.util
 
 # DEPRECATIONS HACKS
 import django
-from django.utils.translation import gettext
 from urllib.parse import quote
 
 # Needed for django-cas-client==1.5.3
 django.utils.http.urlquote = quote
 
 # As a comment until a new version of django-chunked-upload is released on pypi
-# See requirements.txt (django-chunked-upload==2.0.0 replaced by 
+# See requirements.txt (django-chunked-upload==2.0.0 replaced by
 # git+https://github.com/juliomalegria/django-chunked-upload.git@master)
 # django.utils.translation.ugettext = gettext
 
