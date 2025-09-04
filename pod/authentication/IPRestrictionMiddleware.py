1d12877885cd79089648f49270e8cf71374df893
Olivier Bado-Faustin <12731381+Badatos@users.noreply.github.com>
2025-09-04 17:43:03 +0200

    Add a new "ALLOWED_SUPERUSER_IPS" parameter to set a list of IPs and/or ranges from which 'superuser' status is allowed. + remove links from translated strings in claim_record and video_element
    TODO : Unit tests

---
+++ pod/authentication/IPRestrictionMiddleware.py
@@ -0,0 +1,46 @@
+"""Esup-Pod IP Restriction middleware."""
+
+import ipaddress
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
+ALLOWED_SUPERUSER_IPS = getattr(settings, "ALLOWED_SUPERUSER_IPS", [])
+
+
+def ip_in_allowed_range(ip) -> bool:
+    """Make sure the IP is one of the authorized ones."""
+    try:
+        ip_obj = ipaddress.ip_address(ip)
+    except ValueError:
+        return False
+
+    for allowed in ALLOWED_SUPERUSER_IPS:
+        try:
+            if '/' in allowed:
+                net = ipaddress.ip_network(allowed, strict=False)
+                if ip_obj in net:
+                    return True
+            else:
+                if ip_obj == ipaddress.ip_address(allowed):
+                    return True
+        except ValueError:
+            continue
+    return False
+
+
+class IPRestrictionMiddleware:
+    def __init__(self, get_response) -> None:
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip = request.META.get('REMOTE_ADDR')
+        user = request.user
+
+        if user.is_authenticated and user.is_superuser:
+            if not ip_in_allowed_range(ip):
+                user.is_superuser = False
+                user.last_name = _(
+                    "%(last_name)s (Restricted - IP %(ip)s not allowed)"
+                ) % {"last_name": user.last_name, "ip": ip}
+
+        return self.get_response(request)
