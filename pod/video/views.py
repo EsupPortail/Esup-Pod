2e21a55c5987f23ea37b0e7c784da7f959a7580e
Loïc B. <56730254+LoicBonavent@users.noreply.github.com>
2025-09-08 11:25:26 +0200

    Fix v4.0.0 bugs (#1341)
    Fix some bugs for v4.0.0:
    
    - Aristote: Fix a bug when notify an user
    - Live: Change display and truncate chars to 55
    - Recorder: Set max_length to 200 for file attribute, in RecordingFileTreatment
    - Migrate BBB recordings: Modifications to be able to retrieve BBB records from an infrastructure using access tokens
    - Search: Display the right number of results, and add keyword for cursus/main_lang
    
    Change for Github:
    
    - Manage dev_v4 branch

--- pod/video/views.py
+++ pod/video/views.py
@@ -358,7 +358,7 @@ def paginator(videos_list, page):
 def channel(request, slug_c, slug_t=None):
     channel = get_object_or_404(Channel, slug=slug_c, site=get_current_site(request))
     videos_list = get_available_videos().filter(channel=channel)
-    videos_list = sort_videos_list(videos_list, "date_added", "on")
+    videos_list = sort_videos_list(videos_list, "date_added")
     channel.video_count = videos_list.count()
 
     theme = None
