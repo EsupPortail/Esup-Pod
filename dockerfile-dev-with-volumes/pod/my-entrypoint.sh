1a6333689c5c531ae7e27b0f5e519d200a6833b5
Éloi Rivard <eloi@yaal.coop>
2025-09-11 17:04:56 +0200

    feat: allow multiple pod instances to be ran
    - docker initialization marker is the creation of a .initialized file
      instead of detecting if the database has been created or not
    - make createDb file cleaning is error-proof and can be executed
      multiple times in parallel
    - collectstatic logs are quieter
    - the default documentation advise to rely on the POD_INSTANCE
      environment var to discriminate ES_INDEX, caches and redis session
      prefix
    - docker pod entrypoints rely on the POD_INSTANCE en var to detect the
      right .initialized file
    - docker pod entrypoints rely on the POD_PORT en var to run the app on
      dedicated port
    - the 'Site' object is not statically initialized with
      initial_data.json, but dynamically in main/apps.py
    - the celery queues use the POD_INSTANCE var as keyprefixes
    - the default database is named after POD_INSTANCE if available

--- dockerfile-dev-with-volumes/pod/my-entrypoint.sh
+++ dockerfile-dev-with-volumes/pod/my-entrypoint.sh
@@ -1,28 +1,31 @@
 #!/bin/sh
-echo "Launching commands into pod-dev"
+INSTANCE="${POD_INSTANCE:-pod}"
+PORT="${POD_PORT:-8000}"
+echo "Launching commands into ${INSTANCE}-dev"
 mkdir -p pod/node_modules
 mkdir -p pod/db_migrations && touch pod/db_migrations/__init__.py
 ln -fs /tmp/node_modules/* pod/node_modules
 # Mise en route
 # Base de données SQLite intégrée
-BDD_FILE=/usr/src/app/pod/db.sqlite3
-if test ! -f "$BDD_FILE"; then
-    echo "$BDD_FILE does not exist."
+INIT_FILE="/usr/src/app/pod/.${INSTANCE}.initialized"
+if test ! -f "$INIT_FILE"; then
+    echo "$INIT_FILE does not exist."
     python3 manage.py create_pod_index
-    curl -XGET "elasticsearch.localhost:9200/pod/_search"
+    curl -XGET "elasticsearch.localhost:9200/${INSTANCE}/_search"
     # Deployez les fichiers statiques
-    python3 manage.py collectstatic --no-input --clear
+    python3 manage.py collectstatic --no-input --clear --verbosity 0
     # Lancez le script présent à la racine afin de créer les fichiers de migration, puis de les lancer pour créer la base de données SQLite intégrée.
     make createDB
     # SuperUtilisateur
     # Il faut créer un premier utilisateur qui aura tous les pouvoirs sur votre instance.
     python3 manage.py createsuperuser --noinput
+    touch "$INIT_FILE"
 else
-    echo "$BDD_FILE exist."
+    echo "$INIT_FILE exist."
 fi
 # Serveur de développement
 # Le serveur de développement permet de tester vos futures modifications facilement.
 # N'hésitez pas à lancer le serveur de développement pour vérifier vos modifications au fur et à mesure.
 # À ce niveau, vous devriez avoir le site en français et en anglais et voir l'ensemble de la page d'accueil.
-python3 manage.py runserver 0.0.0.0:8000 --insecure
+python3 manage.py runserver "0.0.0.0:${PORT}" --insecure
 sleep infinity
