2eec241ada3a463e678ca61ba60974baed8e97cc
Giorgio UTZERI <gioutzeri@gmail.com>
2025-11-27 14:17:20 +0100

    feat: expose swagger and redoc documentation endpoints

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 # Correction: import depuis 'router' (singulier) et non 'routers'
 from config.router import router
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
     path("api/auth/", include("rest_framework.urls")), # Login browsable API
+
+    # --- AJOUT ROUTES SWAGGER ---
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
