b4aaae8e54fba29ba9e3875c3b3e2994e6a89a7e
Benjamin-etu_Lille <serebenjamin.tr@gmail.com>
2025-11-28 11:59:06 +0100

    feat:add logger config in setting/dev.py

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -1,4 +1,5 @@
 from .base import *
+import os  # Bonne pratique : expliciter l'import si on l'utilise ici, même si base l'importe déjà
 
 # Surcharge spécifique pour le développement
 DEBUG = True
@@ -6,3 +7,39 @@ DEBUG = True
 # Tu pourras ajouter ici des outils de debug (Django Debug Toolbar, etc.)
 # INSTALLED_APPS += ["debug_toolbar"]
 # MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {asctime} {name} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+        },
+    },
+    "loggers": {
+        # Logger par défaut pour Django
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "propagate": False,
+        },
+        # Votre logger spécifique au projet "pod"
+        "pod": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
