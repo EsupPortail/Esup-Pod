b2da464bcc14eef0f9c2a16368fc19ffb0e630c6
Benjamin-etu_Lille <serebenjamin.tr@gmail.com>
2025-12-10 08:32:32 +0100

    fix:manage.py

--- manage.py
+++ manage.py
@@ -1,39 +1,27 @@
 #!/usr/bin/env python
-"""Esup-Pod management program."""
-
-# This file is part of Esup-Pod.
-#
-# Esup-Pod is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Esup-Pod is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with Esup-Pod. If not, see <https://www.gnu.org/licenses/>.
-
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pod.settings")
+def main():
+    """Run administrative tasks."""
+    
+    base_path = Path(__file__).resolve().parent
+    sys.path.append(str(base_path / "src"))
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        # try:
-        #    import django
-        # except ImportError:
-        #    raise ImportError(
-        #        "Couldn't import Django. Are you sure it's installed and "
-        #        "available on your PYTHONPATH environment variable? Did you "
-        #        "forget to activate a virtual environment?"
-        #    )
-        raise
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    
     execute_from_command_line(sys.argv)
+
+if __name__ == "__main__":
+    main()
