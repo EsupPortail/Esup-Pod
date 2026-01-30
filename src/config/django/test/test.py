32b8b55fa4edaee70a69024de2d2fd16f58cff8d
Giorgio UTZERI <gioutzeri@gmail.com>
2026-01-30 14:56:42 +0100

    docs: add docstring to local test config

--- src/config/django/test/test.py
+++ src/config/django/test/test.py
@@ -1,3 +1,13 @@
+"""
+Local testing configuration.
+
+Lightweight setup for running tests locally without Docker.
+Uses an in-memory SQLite database by default for speed and allows all hosts.
+
+Usage:
+    export DJANGO_SETTINGS_MODULE=config.django.test.test
+    pytest
+"""
 import os
 
 from config.django.test.init_env import *  # noqa: F401, F403
