325365940d855d72be50a15d9a6dfc15aa5af17e
Giorgio UTZERI <gioutzeri@gmail.com>
2026-01-30 15:00:17 +0100

    docs: add docstring to settings init

--- src/config/settings/__init__.py
+++ src/config/settings/__init__.py
@@ -1,2 +1,8 @@
+"""
+Settings package initialization.
+
+Aggregates specialized configuration modules for Authentication and Swagger
+to make them easily accessible.
+"""
 from .authentication import *  # noqa: F401, F403
 from .swagger import *  # noqa: F401, F403
