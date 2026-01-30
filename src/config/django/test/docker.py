80c1f9317204106e576d3ab216f7f3d2fc6bb0e5
Giorgio UTZERI <gioutzeri@gmail.com>
2026-01-30 14:54:52 +0100

    docs: add docstring to docker test config

--- src/config/django/test/docker.py
+++ src/config/django/test/docker.py
@@ -1,3 +1,14 @@
+"""
+Docker-based testing configuration.
+
+Optimized settings for running tests within a Docker container.
+Enhances execution speed by using MD5 password hashing and synchronous Celery tasks.
+Configures a dedicated MySQL test database and captures emails in memory.
+
+Usage:
+    export DJANGO_SETTINGS_MODULE=config.django.test.docker
+    python manage.py test
+"""
 from config.django.test.init_env import *  # noqa: F401, F403 # isort:skip
 from config.django.dev.docker import *  # noqa: F401, F403
 from config.env import env
